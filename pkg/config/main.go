570856e6541c0648e54edcf2a50d1110473b2d12
Olblak <me@olblak.com>
2020-03-03 08:33:32 +0100

    Use Viper for handling configuration

--- pkg/config/main.go
+++ pkg/config/main.go
@@ -2,11 +2,9 @@ package config
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
+	"strings"
 
-	"github.com/BurntSushi/toml"
-	"gopkg.in/yaml.v3"
+	"github.com/spf13/viper"
 )
 
 // Config hold our cli configuration
@@ -38,43 +36,31 @@ type Target struct {
 	Repository interface{}
 }
 
-// ReadTomlFile read settings from a toml file
-func (config *Config) ReadTomlFile(cfgFile string) {
-
-	if _, err := toml.DecodeFile(cfgFile, &config); err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-// ReadYamlFile read settings from a yaml file
-func (config *Config) ReadYamlFile(cfgFile string) {
-	file, err := os.Open(cfgFile)
-	defer file.Close()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	data, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-}
-
 // ReadFile is just a abstraction in front of ReadYamlFile or ReadTomlFile
 func (config *Config) ReadFile(cfgFile string) {
 
-	config.ReadYamlFile(cfgFile)
+	v := viper.New()
 
+	v.SetEnvPrefix("updatecli")
+	v.AutomaticEnv()
+	v.SetConfigName("updateCli")        // name of config file (without extension)
+	v.SetConfigType("yaml")             // REQUIRED if the config file does not have the extension in the name
+	v.AddConfigPath("$HOME/.updateCli") // call multiple times to add many search paths
+	v.AddConfigPath(".")                // optionally look for config in the working directory
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println("Config file not found")
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	err := v.Unmarshal(&config)
+	if err != nil {
+		fmt.Printf("unable to decode into struct, %v\n", err)
+	}
 }
 
 // Check is a function to test that some settings are correctly present
