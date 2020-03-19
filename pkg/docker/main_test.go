17e0224dfd037fbcda17ad30a09a623d68ed6b5b
Olblak <me@olblak.com>
2020-03-19 18:30:05 +0100

    Add docker parameter validation & unit tests

--- pkg/docker/main_test.go
+++ pkg/docker/main_test.go
@@ -2,6 +2,52 @@ package docker
 
 import "testing"
 
+func TestParameters(t *testing.T) {
+	d := &Docker{
+		Image: "nginx",
+	}
+
+	ok, _ := d.Check()
+
+	// Test if current setting is valid
+	if ok != true {
+		t.Errorf("Minimum valid configuration provided! Expect %v, got %v", true, ok)
+	}
+
+	// Test if we correctly return library images if dockerhub namespace is not specified
+	got := d.Image
+	expected := "library/nginx"
+	if got != expected {
+		t.Errorf("Image is configured without namespace! expected %v, got %v", expected, got)
+	}
+
+	// Test if we correctly return the default docker hub url if not defined
+	expected = "hub.docker.com"
+	got = d.URL
+	if got != expected {
+		t.Errorf("URL is not configured! expected value %v, got %v", expected, got)
+	}
+
+	// Test if we correctly return the default docker hub url if not defined
+	expected = "latest"
+	got = d.Tag
+	if got != expected {
+		t.Errorf("Tag is not configured! expected %v, got %v", expected, got)
+	}
+}
+
+func TestCheck(t *testing.T) {
+
+	// Test if image is not defined
+	d := &Docker{}
+
+	expected := false
+	got, _ := d.Check()
+	if got != false {
+		t.Errorf("Image is not configured! expected %v, got %v", expected, got)
+	}
+}
+
 func TestIsPublished(t *testing.T) {
 	// Test if existing image tag return true
 	d := &Docker{
