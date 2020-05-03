19547c5a63c3d20131d2fc89883ed075939b7735
Olblak <me@olblak.com>
2020-05-03 16:14:40 +0200

    Add diff subcommand

---
+++ cmd/diff.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	diffCmd = &cobra.Command{
+		Use:   "diff",
+		Short: "diff will execute updateCli and check if target update are needed",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("\n%s\n\n", strings.ToTitle("Apply"))
+
+			e.Options.File = cfgFile
+			e.Options.ValuesFile = valuesFile
+
+			e.Options.Target.Commit = false
+			e.Options.Target.Push = false
+			e.Options.Target.Clean = false
+
+			run(
+				"apply",
+			)
+		},
+	}
+)
+
+func init() {
+	diffCmd.Flags().StringVarP(&cfgFile, "config", "c", "./updateCli.yaml", "config file (default is ./updateCli.yaml)")
+	diffCmd.Flags().StringVarP(&valuesFile, "values", "v", "", "values file use for templating (required {.tpl,.tmpl} config)")
+}
