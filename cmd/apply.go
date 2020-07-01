f1b74fa3b1f95ba4a80b60d293634fbc719d7643
Olivier Vernin <olivier@vernin.me>
2020-07-01 13:53:26 +0200

    Fix apply parameters

--- cmd/apply.go
+++ cmd/apply.go
@@ -36,7 +36,7 @@ func init() {
 	applyCmd.Flags().StringVarP(&cfgFile, "config", "c", "./updateCli.yaml", "config file (default is ./updateCli.yaml)")
 	applyCmd.Flags().StringVarP(&valuesFile, "values", "v", "", "values file use for templating (required {.tpl,.tmpl} config)")
 
-	showCmd.Flags().BoolVarP(&applyCommit, "commit", "", true, "Commit")
-	showCmd.Flags().BoolVarP(&applyPush, "push", "", true, "Push changes")
-	showCmd.Flags().BoolVarP(&applyClean, "clean", "", true, "clean working directory")
+	applyCmd.Flags().BoolVarP(&applyCommit, "commit", "", true, "Commit")
+	applyCmd.Flags().BoolVarP(&applyPush, "push", "", true, "Push changes")
+	applyCmd.Flags().BoolVarP(&applyClean, "clean", "", true, "clean working directory")
 }
