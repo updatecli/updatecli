eb304347bbe06d40ab3bd5552b031b675af52cd3
Olivier Vernin <olivier@vernin.me>
2020-07-01 11:34:55 +0200

    Add support for array in Target yaml key

--- pkg/yaml/main.go
+++ pkg/yaml/main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -25,42 +27,177 @@ func (y *Yaml) GetFile() string {
 	return y.File
 }
 
+// isPositionKey checks if key use the array position format
+func isPositionKey(key string) bool {
+	matched, err := regexp.MatchString("^[[:alnum:]]*[[[:digit:]]*]$", key)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	return matched
+}
+
+func getPositionKeyValue(k string) (key string, position int, err error) {
+	if isPositionKey(k) {
+		re := regexp.MustCompile(`^([[:alnum:]]*)\[[[:digit:]]*\]$`)
+		keys := re.FindStringSubmatch(k)
+		key = keys[1]
+
+		re = regexp.MustCompile(`^[[:alnum:]]*\[([[:digit:]]*)\]$`)
+
+		positions := re.FindStringSubmatch(k)
+
+		fmt.Println(positions)
+		position, err = strconv.Atoi(positions[1])
+
+		if err != nil {
+			fmt.Println(err)
+			return "", -1, err
+		}
+
+	} else {
+		if strings.ContainsAny(k, "_?:,[]{}#&*!|>`\"%") {
+			key = ""
+			position = -1
+			err = fmt.Errorf("Error: key '%s' cannot contains yaml special characters", k)
+
+		} else {
+			key = k
+			position = -1
+			err = nil
+		}
+	}
+
+	return key, position, err
+
+}
+
 // replace parses a yaml object looking for a specific key which needs to be updated and replace it if needed.
 func replace(entry *yaml.Node, keys []string, version string, columnRef int) (found bool, oldVersion string, column int) {
+	/*
+		In yaml.MappingNodes, child nodes represent yaml keys when index is even, and yaml values when index is odd
+	*/
 
 	valueFound := false
 	column = columnRef
-	nextLevel := false
-	for _, content := range entry.Content {
-		if content.Column < column {
-			break
-		}
-		if nextLevel {
-			column = content.Column
-			nextLevel = false
+
+	key, position, err := getPositionKeyValue(keys[0])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// If document start with a sequence and we are looking for an array position
+	if entry.Kind == yaml.DocumentNode &&
+		entry.Content[0].Kind == yaml.SequenceNode &&
+		isPositionKey(keys[0]) {
+
+		if entry.Content[0].Content[0].Kind == yaml.MappingNode {
+			if len(keys) > 1 {
+				positionIndex := 0
+
+				if (position*2)-1 >= 0 {
+					positionIndex = (position * 2) - 1
+				}
+
+				if len(entry.Content[0].Content) < positionIndex {
+					return false, "", 0
+				}
+
+				keys = keys[1:]
+
+				column = entry.Content[0].Content[positionIndex].Column
+
+				valueFound, oldVersion, column = replace(entry.Content[0].Content[positionIndex],
+					keys,
+					version,
+					entry.Content[0].Content[positionIndex].Column)
+			} else {
+
+				if len(entry.Content[0].Content) < position {
+					return false, "", 0
+				}
+
+				valueFound = true
+				oldVersion = entry.Content[0].Content[position].Value
+				column = entry.Content[0].Content[position].Column
+			}
+
+		} else if entry.Content[0].Content[0].Kind == yaml.ScalarNode && len(keys) == 1 {
+
+			if len(entry.Content[0].Content) < position {
+				return false, "", 0
+			}
+
+			oldVersion = entry.Content[0].Content[position].Value
+			valueFound = true
+			column = entry.Content[0].Content[position].Column
+
+		} else {
+			return false, "", 0
 		}
 
-		if content.Value == keys[0] && (content.Column == columnRef) {
-			column = content.Column
-			nextLevel = true
+		return valueFound, oldVersion, column
+
+	}
+
+	for index, content := range entry.Content {
+		// In yaml.MappingNodes, child nodes represent yaml keys when index is even and yaml values when index is odd
+		if index%2 == 0 && content.Value == key && (content.Column == columnRef) {
 
 			if len(keys) > 1 {
 				keys = keys[1:]
-			} else if len(keys) == 1 {
-				valueFound = true
-				continue
+				column = entry.Content[index+1].Column
+
+				if entry.Content[index+1].Kind == yaml.SequenceNode && entry.Content[index+1].Content[0].Kind == yaml.MappingNode {
+					positionIndex := 0
+
+					if (position*2)-1 >= 0 {
+						positionIndex = (position * 2) - 1
+					}
+
+					if positionIndex > len(entry.Content[index+1].Content) {
+						return false, "", 0
+					}
+
+					valueFound, oldVersion, column = replace(entry.Content[index+1].Content[positionIndex],
+						keys,
+						version,
+						entry.Content[index+1].Content[positionIndex].Column)
+				}
+
+				key, position, err = getPositionKeyValue(keys[0])
+				if err != nil {
+					fmt.Println(err)
+				}
+			} else {
+				if entry.Content[index+1].Kind == yaml.ScalarNode {
+					column = entry.Content[index+1].Column
+
+					oldVersion = entry.Content[index+1].Value
+					entry.Content[index+1].SetString(version)
+					valueFound = true
+					break
+
+				} else if entry.Content[index+1].Kind == yaml.SequenceNode {
+
+					if len(entry.Content[index+1].Content) < position {
+						return false, "", 0
+					}
+
+					oldVersion = entry.Content[index+1].Content[position].Value
+					valueFound = true
+					column = entry.Content[index+1].Content[position].Column
+
+					break
+				}
 			}
+			continue
 		}
-
-		if content.Kind == yaml.ScalarNode && valueFound == true {
-			column = content.Column
-
-			oldVersion = content.Value
-			content.SetString(version)
-
-			break
-		} else if content.Kind == yaml.MappingNode {
+		if content.Kind == yaml.MappingNode {
 			valueFound, oldVersion, column = replace(content, keys, version, column)
+		}
+		if content.Column < column {
+			break
 		}
 	}
 	return valueFound, oldVersion, column
--- pkg/yaml/main_test.go
+++ pkg/yaml/main_test.go
@@ -1,6 +1,7 @@
 package yaml
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 
@@ -16,9 +17,158 @@ database:
     repository: postgresql
     tag: 10
 image2: none
+image3:
+- name: a
+  version: 1.0
+- name: b
+  version: 1.5
+image4:
+- c
+- d
+- f
 `
+var data2 = `
+- image:
+    repository: nginx
+    tag: 1.17
+- database:
+    image:
+      repository: postgresql
+      tag: 10
+- image2: none
+- image3:
+  - name: a
+    version: 1.0
+  - name: b
+    version: 1.5
+- image4:
+  - c
+  - d
+  - f
+- 5
+  `
 
 var source = "1.17"
+
+func TestGetPositionKeyValue(t *testing.T) {
+	type dataSet struct {
+		key              string
+		expectedKey      string
+		expectedPosition int
+		expectedError    error
+	}
+
+	dataset := []dataSet{
+		{
+			key:              "image",
+			expectedKey:      "image",
+			expectedPosition: -1,
+			expectedError:    nil,
+		},
+		{
+			key:              "image[0]",
+			expectedKey:      "image",
+			expectedPosition: 0,
+			expectedError:    nil,
+		},
+		{
+			key:              "[0]image",
+			expectedKey:      "",
+			expectedPosition: -1,
+			expectedError:    errors.New("Error: key '[0]image' cannot contains yaml special characters"),
+		},
+		{
+			key:              "image[x]",
+			expectedKey:      "",
+			expectedPosition: -1,
+			expectedError:    fmt.Errorf("Error: key 'image[x]' cannot contains yaml special characters"),
+		},
+		{
+			key:              "im[0]age",
+			expectedKey:      "",
+			expectedPosition: -1,
+			expectedError:    fmt.Errorf("Error: key 'im[0]age' cannot contains yaml special characters"),
+		},
+		{
+			key:              "#image",
+			expectedKey:      "",
+			expectedPosition: -1,
+			expectedError:    fmt.Errorf("Error: key '#image' cannot contains yaml special characters"),
+		},
+	}
+
+	for _, d := range dataset {
+		gotKey, gotPosition, gotError := getPositionKeyValue(d.key)
+		if gotError == nil && d.expectedError == nil {
+			if gotKey != d.expectedKey || gotPosition != d.expectedPosition {
+				t.Errorf("Returned getPositionKeyValue is wrong for key '%s'"+
+					"key expected %s, got %s "+
+					"position expected %d, got %d "+
+					d.key,
+					d.expectedKey, gotKey,
+					d.expectedPosition, gotPosition)
+			}
+
+		} else if gotError != nil && d.expectedError != nil {
+			if gotKey != d.expectedKey || gotPosition != d.expectedPosition || gotError.Error() != d.expectedError.Error() {
+				t.Errorf("Returned getPositionKeyValue value is wrong for 'key' %s"+
+					"key expected %s, got %s "+
+					"position expected %d, got %d "+
+					"Error expected %v, got %v ",
+					d.key,
+					d.expectedKey, gotKey,
+					d.expectedPosition, gotPosition,
+					d.expectedError, gotError)
+			}
+		} else {
+			t.Errorf("Returned getPositionKeyValue value is wrong for 'key' %s"+
+				"key expected %s, got %s "+
+				"position expected %d, got %d "+
+				"Error expected %v, got %v ",
+				d.key,
+				d.expectedKey, gotKey,
+				d.expectedPosition, gotPosition,
+				d.expectedError, gotError)
+		}
+	}
+}
+
+func TestIsPositionKey(t *testing.T) {
+	type dataSet struct {
+		key      string
+		expected bool
+	}
+
+	dataset := []dataSet{
+		{
+			key:      "image",
+			expected: false,
+		},
+		{
+			key:      "image[0]",
+			expected: true,
+		},
+		{
+			key:      "[0]image",
+			expected: false,
+		},
+		{
+			key:      "image[x]",
+			expected: false,
+		},
+		{
+			key:      "im[0]age",
+			expected: false,
+		},
+	}
+
+	for _, d := range dataset {
+		got := isPositionKey(d.key)
+		if got != d.expected {
+			t.Errorf("isPositionKey is wrong for key %s! expected %v, got %v", d.key, d.expected, got)
+		}
+	}
+}
 
 func TestReplace(t *testing.T) {
 
@@ -27,6 +177,8 @@ func TestReplace(t *testing.T) {
 		expectedOldVersion string
 		expectedValueFound bool
 	}
+
+	//https://github.com/go-yaml/yaml/issues/599
 
 	dataset := []dataSet{
 		{
@@ -44,6 +196,49 @@ func TestReplace(t *testing.T) {
 			expectedOldVersion: "none",
 			expectedValueFound: true,
 		},
+		{
+			key:                []string{"image3[0]", "version"},
+			expectedOldVersion: "1.0",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"image3[1]", "version"},
+			expectedOldVersion: "1.5",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"image4[0]"},
+			expectedOldVersion: "c",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"image4[1]"},
+			expectedOldVersion: "d",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"image4[10]"},
+			expectedOldVersion: "",
+			expectedValueFound: false,
+		},
+	}
+
+	dataset2 := []dataSet{
+		{
+			key:                []string{"[0]", "image", "tag"},
+			expectedOldVersion: "1.17",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"[1]", "database", "image", "tag"},
+			expectedOldVersion: "10",
+			expectedValueFound: true,
+		},
+		{
+			key:                []string{"[5]"},
+			expectedOldVersion: "5",
+			expectedValueFound: true,
+		},
 	}
 
 	out := yaml.Node{}
@@ -55,6 +250,24 @@ func TestReplace(t *testing.T) {
 
 	for _, d := range dataset {
 		valueFound, oldVersion, _ := replace(&out, d.key, source, 1)
+
+		if valueFound != d.expectedValueFound {
+			t.Errorf("Value not found for key %v! expected %v, got %v", d.key, d.expectedValueFound, valueFound)
+		}
+
+		if oldVersion != d.expectedOldVersion {
+			t.Errorf("Old Version mismatch for key %v! expected %v, got %v", d.key, d.expectedOldVersion, oldVersion)
+		}
+	}
+
+	out2 := yaml.Node{}
+	err = yaml.Unmarshal([]byte(data2), &out2)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, d := range dataset2 {
+		valueFound, oldVersion, _ := replace(&out2, d.key, source, 1)
 
 		if valueFound != d.expectedValueFound {
 			t.Errorf("Value not found for key %v! expected %v, got %v", d.key, d.expectedValueFound, valueFound)
