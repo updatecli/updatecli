ead91b25889e49739903a1d36d6a6103b97e909a
Olivier Vernin <olivier@vernin.me>
2020-07-24 21:58:33 +0200

    Add newline at the end of file
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/engine/source/replacer_test.go
+++ pkg/engine/source/replacer_test.go
@@ -1,37 +1,36 @@
 package source
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 )
 
 func TestReplacersUnmarshall(t *testing.T) {
 	dataSet := Replacers{
 		{
 			From: "a",
-			To: "b",
+			To:   "b",
 		},
 		{
 			From: "c",
-			To: "d",
+			To:   "d",
 		},
 		{
 			From: "e",
-			To: "f",
+			To:   "f",
 		},
 	}
-	expected := []string{ "a","b","c","d","e","f"}
+	expected := []string{"a", "b", "c", "d", "e", "f"}
 
 	got := dataSet.Unmarshal()
 
 	// Testing that we correctly get a slice of string
-	if ! reflect.DeepEqual(expected, got) {
+	if !reflect.DeepEqual(expected, got) {
 		t.Errorf("List of replacers is incorrect, expected '%v', got '%v'", expected, got)
 	}
 
-
 	// Testing that order matter
-	expected = []string{ "a","c","b","d","e","d"}
+	expected = []string{"a", "c", "b", "d", "e", "d"}
 
 	if reflect.DeepEqual(expected, got) {
 		t.Errorf("List of replacers is incorrect, expected '%v', got '%v'", expected, got)
