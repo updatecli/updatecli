c5fbdeb171581a60450a9fc2b255456b6de40d5c
Olblak <2360224+olblak@users.noreply.github.com>
2020-07-24 22:01:37 +0200

    Add replace rules in source definition (#82)
    * Add replace rules in source definition
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add missing file
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Fix Replacer field name in test
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add newline at the end of file
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

---
+++ pkg/engine/source/replacer.go
@@ -0,0 +1,20 @@
+package source
+
+// Replacer is struct used to feed strings.Replacer
+type Replacer struct {
+	From string
+	To   string
+}
+
+// Replacers is an array of Replacer
+type Replacers []Replacer
+
+// Unmarshal read a struct of Replacers then return a slice of string
+func (replacers Replacers) Unmarshal() (result []string) {
+
+	for _, r := range replacers {
+		result = append(result, r.From)
+		result = append(result, r.To)
+	}
+	return result
+}
---
+++ pkg/engine/source/replacer_test.go
@@ -0,0 +1,38 @@
+package source
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReplacersUnmarshall(t *testing.T) {
+	dataSet := Replacers{
+		{
+			From: "a",
+			To:   "b",
+		},
+		{
+			From: "c",
+			To:   "d",
+		},
+		{
+			From: "e",
+			To:   "f",
+		},
+	}
+	expected := []string{"a", "b", "c", "d", "e", "f"}
+
+	got := dataSet.Unmarshal()
+
+	// Testing that we correctly get a slice of string
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("List of replacers is incorrect, expected '%v', got '%v'", expected, got)
+	}
+
+	// Testing that order matter
+	expected = []string{"a", "c", "b", "d", "e", "d"}
+
+	if reflect.DeepEqual(expected, got) {
+		t.Errorf("List of replacers is incorrect, expected '%v', got '%v'", expected, got)
+	}
+}
