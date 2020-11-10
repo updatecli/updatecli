ba2515d57fa6ababeac0c3ea70bc6c3dcfd28425
Olivier Vernin <olivier@vernin.me>
2020-11-10 11:42:28 +0100

    Correctly handle yaml condition return
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/plugins/yaml/condition.go
+++ pkg/plugins/yaml/condition.go
@@ -38,11 +38,9 @@ func (y *Yaml) Condition(source string) (bool, error) {
 		return false, fmt.Errorf("Something weird happened while trying to set working directory")
 	}
 
-	exist := false
-
 	data, err := y.ReadFile()
 	if err != nil {
-		return exist, err
+		return false, err
 	}
 
 	var out yaml.Node
@@ -50,17 +48,17 @@ func (y *Yaml) Condition(source string) (bool, error) {
 	err = yaml.Unmarshal(data, &out)
 
 	if err != nil {
-		return exist, fmt.Errorf("cannot unmarshal data: %v", err)
+		return false, fmt.Errorf("cannot unmarshal data: %v", err)
 	}
 
 	valueFound, oldVersion, _ := replace(&out, strings.Split(y.Key, "."), y.Value, 1)
 
 	if valueFound && oldVersion == y.Value {
-		exist = true
 		fmt.Printf("\u2714 Key '%s', from file '%v', is correctly set to %s'\n",
 			y.Key,
 			filepath.Join(y.Path, y.File),
 			y.Value)
+		return true, nil
 
 	} else if valueFound && oldVersion != y.Value {
 
@@ -74,11 +72,9 @@ func (y *Yaml) Condition(source string) (bool, error) {
 		fmt.Printf("\u2717 cannot find key '%s' from file '%s'\n",
 			y.Key,
 			filepath.Join(y.Path, y.File))
-
-		return exist, nil
 	}
 
-	return exist, nil
+	return false, nil
 }
 
 // ConditionFromSCM checks if a key exists in a yaml file
@@ -86,11 +82,9 @@ func (y *Yaml) ConditionFromSCM(source string, scm scm.Scm) (bool, error) {
 
 	y.Path = scm.GetDirectory()
 
-	exist := false
-
 	data, err := y.ReadFile()
 	if err != nil {
-		return exist, err
+		return false, err
 	}
 
 	var out yaml.Node
@@ -98,17 +92,17 @@ func (y *Yaml) ConditionFromSCM(source string, scm scm.Scm) (bool, error) {
 	err = yaml.Unmarshal(data, &out)
 
 	if err != nil {
-		return exist, fmt.Errorf("cannot unmarshal data: %v", err)
+		return false, fmt.Errorf("cannot unmarshal data: %v", err)
 	}
 
 	valueFound, oldVersion, _ := replace(&out, strings.Split(y.Key, "."), y.Value, 1)
 
 	if valueFound && oldVersion == y.Value {
-		exist = true
 		fmt.Printf("\u2714 Key '%s', from file '%v', is correctly set to %s'\n",
 			y.Key,
 			filepath.Join(y.Path, y.File),
 			y.Value)
+		return true, nil
 
 	} else if valueFound && oldVersion != y.Value {
 		fmt.Printf("\u2717 Key '%s', from file '%v', is incorrectly set to %s and should be %s'\n",
@@ -121,11 +115,7 @@ func (y *Yaml) ConditionFromSCM(source string, scm scm.Scm) (bool, error) {
 		fmt.Printf("\u2717 cannot find key '%s' from file '%s'\n",
 			y.Key,
 			filepath.Join(y.Path, y.File))
-
-		return exist, nil
 	}
 
-	exist = true
-
-	return exist, nil
+	return false, nil
 }
