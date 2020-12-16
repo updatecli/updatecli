f52c8fe40b65ff55b7fc997ddf12e19991f9da4e
Olivier Vernin <olivier@vernin.me>
2020-12-16 20:09:41 +0100

    Add dockerfile ConditionFromSCM
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/plugins/docker/dockerfile/condition.go
+++ pkg/plugins/docker/dockerfile/condition.go
@@ -3,6 +3,7 @@ package dockerfile
 import (
 	"bytes"
 	"fmt"
+	"path"
 
 	"github.com/olblak/updateCli/pkg/core/helpers"
 	"github.com/olblak/updateCli/pkg/core/scm"
@@ -12,6 +13,7 @@ import (
 
 // Condition test if the Dockerfile contains the correct key/value
 func (d *Dockerfile) Condition(version string) (bool, error) {
+
 	raw, err := helpers.ReadFile(d.File)
 	if err != nil {
 		return false, err
@@ -51,6 +53,40 @@ func (d *Dockerfile) Condition(version string) (bool, error) {
 
 // ConditionFromSCM run based on a file from SCM
 func (d *Dockerfile) ConditionFromSCM(version string, scm scm.Scm) (bool, error) {
-	//
+
+	raw, err := helpers.ReadFile(path.Join(scm.GetDirectory(), d.File))
+
+	if err != nil {
+		return false, err
+	}
+
+	if len(d.Value) == 0 {
+		d.Value = version
+	}
+
+	data, err := parser.Parse(bytes.NewReader(raw))
+
+	if err != nil {
+		return false, err
+	}
+
+	found, err := d.search(data.AST)
+
+	if err != nil {
+		return false, err
+	}
+
+	if found {
+		fmt.Printf("\u2714 Instruction '%s' from Dockerfile '%s', is correctly set to '%s' \n",
+			d.Instruction,
+			d.File,
+			d.Value)
+		return true, nil
+	}
+	fmt.Printf("\u2717 Instruction '%s' from Dockerfile '%s', is incorrectly set to '%s' \n",
+		d.Instruction,
+		d.File,
+		d.Value)
+
 	return false, nil
 }
