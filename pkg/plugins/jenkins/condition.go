b10163b8fe0eb55124958fffa035ab80a19a7918
Olivier Vernin <olivier@vernin.me>
2021-01-29 11:03:31 +0100

    Set default values for jenkins release
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/plugins/jenkins/condition.go
+++ pkg/plugins/jenkins/condition.go
@@ -11,24 +11,17 @@ import (
 // match a valid release type
 func (j *Jenkins) Condition(source string) (bool, error) {
 
-	version := source
 	versionExist := false
 	validReleaseType := false
 
 	// Override source input if version specified by parameter
-	if len(j.Version) > 0 {
-		version = j.Version
+	if len(j.Version) == 0 {
+		j.Version = source
 	}
 
-	releaseType, err := ReleaseType(version)
+	releaseType, err := ReleaseType(j.Version)
 	if err != nil {
 		return false, err
-	}
-
-	// Init release type based on version if not defined
-	if len(j.Release) == 0 {
-		j.Release = releaseType
-
 	}
 
 	err = j.Validate()
@@ -39,37 +32,35 @@ func (j *Jenkins) Condition(source string) (bool, error) {
 
 	validReleaseType = true
 
-	if len(j.Release) > 0 {
-		if strings.Compare(releaseType, j.Release) != 0 {
-			return false, fmt.Errorf(
-				"Wrong Release Type '%s' detected : Jenkins version '%s' is a '%s' release",
-				j.Release, version, releaseType)
-		}
+	if strings.Compare(releaseType, j.Release) != 0 {
+		return false, fmt.Errorf(
+			"Wrong Release Type '%s' detected : Jenkins version '%s' is a '%s' release",
+			j.Release, j.Version, releaseType)
 	}
 
-	if len(version) > 0 {
+	if len(j.Version) > 0 {
 		_, versions, err := GetVersions()
 
 		if err != nil {
 			return false, err
 		}
 		for _, v := range versions {
-			if strings.Compare(v, version) == 0 {
+			if strings.Compare(v, j.Version) == 0 {
 				versionExist = true
 				break
 			}
 		}
 	}
 	if !versionExist {
-		fmt.Printf("\u2717 Version '%v' doesn't exist\n", version)
+		fmt.Printf("\u2717 Version '%v' doesn't exist\n", j.Version)
 		return false, nil
 
 	} else if !validReleaseType {
-		fmt.Printf("\u2717 Wrong Release Type: %v for version %v\n", releaseType, version)
+		fmt.Printf("\u2717 Wrong Release Type: %v for version %v\n", releaseType, j.Version)
 		return false, nil
 	}
 
-	fmt.Printf("\u2714 %s release version '%s' available\n", releaseType, version)
+	fmt.Printf("\u2714 %s release version '%s' available\n", releaseType, j.Version)
 
 	return true, nil
 }
