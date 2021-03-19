eb740dec48affc00d85f7d6d5ed42fcde3fb48a8
Olivier Vernin <olivier@vernin.me>
2021-03-19 13:50:11 +0100

    [GITHUB] Use github tags when no github release found
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

---
+++ pkg/plugins/github/source_test.go
@@ -0,0 +1,32 @@
+package github
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestGetTags(t *testing.T) {
+
+	g := Github{
+		Owner:      "updatecli",
+		Repository: "updatecli",
+		Token:      os.Getenv("GITHUB_TOKEN"),
+		Directory:  filepath.Join(os.TempDir(), "tests", "updatecli"),
+	}
+
+	_, err := g.Clone()
+	if err != nil {
+		t.Errorf("Something went wrong when running git clone: %q", err)
+	}
+
+	tags, err := g.SearchTags()
+
+	if err != nil {
+		t.Errorf("Something went wrong when retrieving tags: %q", err)
+	}
+
+	if len(tags) == 0 {
+		t.Errorf("No tags founded: %q", tags)
+	}
+}
