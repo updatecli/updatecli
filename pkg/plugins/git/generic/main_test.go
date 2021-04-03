a9ec5240bc9555959e944f8017146035c5ff8e2c
Olivier Vernin <olivier@vernin.me>
2021-04-03 09:42:48 +0200

    Add git function to return list of tags
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/plugins/git/generic/main_test.go
+++ pkg/plugins/git/generic/main_test.go
@@ -1,6 +1,10 @@
 package generic
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestSanitizeBranchName(t *testing.T) {
 	type dataSet struct {
@@ -49,4 +53,34 @@ func TestSanitizeBranchName(t *testing.T) {
 			t.Errorf("Branch name isn't correctly got %s, expected %s", got, d.expected)
 		}
 	}
+}
+
+// Test that we can correctly retrieve a list of tags from a remote git repository
+// and that it's correctly ordered, starting with the oldest tag
+func TestTagsIntegration(t *testing.T) {
+
+	workingDir := filepath.Join(os.TempDir(), "tests", "updatecli")
+	err := Clone("", "", "https://github.com/updatecli/updatecli.git", workingDir)
+	if err != nil {
+		t.Errorf("Don't expect error: %q", err)
+	}
+	defer os.RemoveAll(workingDir)
+
+	tags, err := Tags(workingDir)
+	if err != nil {
+		t.Errorf("Don't expect error: %q", err)
+	}
+
+	expectedTag := "untagged-902d9ce264ba6334c5d0"
+	found := false
+
+	// Test that the first tag from array is also the oldest one
+	if tags[0] == expectedTag {
+		found = true
+	}
+
+	if !found {
+		t.Errorf("Expected tag %q to be found in %q", expectedTag, tags)
+	}
+	os.Remove(workingDir)
 }
