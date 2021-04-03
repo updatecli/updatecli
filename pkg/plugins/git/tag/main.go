c62f174d64f994f5175abc96cbe9184e7f16f1aa
Olivier Vernin <olivier@vernin.me>
2021-04-03 09:51:19 +0200

    Add function comment for package git/tag
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- pkg/plugins/git/tag/main.go
+++ pkg/plugins/git/tag/main.go
@@ -10,6 +10,7 @@ type Tag struct {
 	VersionFilter version.Filter // //VersionFilter provides parameters to specify version pattern and its type like regex, semver, or just latest.
 }
 
+// Validate tests that tag struct is correctly configured
 func (t *Tag) Validate() error {
 	err := t.VersionFilter.Validate()
 	if err != nil {
