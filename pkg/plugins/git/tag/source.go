7a3f95ec2d1685b941523bf9aa9126fbaf600b7d
Olivier Vernin <olivier@vernin.me>
2021-04-03 09:46:44 +0200

    Add gitTag source
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

---
+++ pkg/plugins/git/tag/source.go
@@ -0,0 +1,41 @@
+package tag
+
+import (
+	"fmt"
+
+	"github.com/olblak/updateCli/pkg/plugins/git/generic"
+	"github.com/sirupsen/logrus"
+)
+
+// Source return the latest git tag based on create time
+func (t *Tag) Source(workingDir string) (string, error) {
+
+	if len(t.Path) == 0 && len(workingDir) > 0 {
+		t.Path = workingDir
+	}
+
+	t.Validate()
+
+	tags, err := generic.Tags(workingDir)
+
+	if err != nil {
+		logrus.Errorln(err)
+		return "", err
+	}
+
+	value, err := t.VersionFilter.Search(tags)
+	if err != nil {
+		return "", err
+	}
+
+	if len(value) == 0 {
+		logrus.Infof("\u2717 No Git Tag found matching pattern %q", t.VersionFilter.Pattern)
+		return value, fmt.Errorf("no Git tag found matching pattern %q", t.VersionFilter.Pattern)
+	} else if len(value) > 0 {
+		logrus.Infof("\u2714 Git Tag %q found, matching pattern %q", value, t.VersionFilter.Pattern)
+	} else {
+		logrus.Errorf("Something unexpected happened in gitTag source")
+	}
+
+	return value, nil
+}
