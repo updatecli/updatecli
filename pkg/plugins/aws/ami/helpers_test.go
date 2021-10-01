e316f453790a1452ab9ec39f6779cd6169c02706
Olivier Vernin <olivier@vernin.me>
2021-10-01 19:13:15 +0200

    Add a new resource to manipulate AWS AMI (#265)
    * Init aws resource
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Implement AWS/AMI resource logic
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Switch ami filter to a more generic approach
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add aws/ami condition
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add aws/ami source
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Stop search for ami if not filter provided
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add region information
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add simple aws_ami example
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Fix implicit memory aliasing
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Refactor aws/ami to use mocked testing
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Update aws-ami example
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Update comment
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Remove duplicated code
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Update pkg/plugins/aws/ami/condition.go
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>
    
    * Update pkg/plugins/aws/ami/condition.go
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>
    
    * Fix indent
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add notify message
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Improve aws/ami error message
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Improve source output
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Move filter into a separated file
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Don't export unneeded function
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Move spec into a specific struct
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add test case
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Don't set default image-id if no source provided
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Fix ami condition test
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Refactor function
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Improve error handling and test case
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Configure aws api credential
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Update example
    
    Signed-off-by: Olivier Vernin <olivier@vernin.me>
    
    * Add the ability to sort AMI found
    
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ pkg/plugins/aws/ami/helpers_test.go
@@ -0,0 +1,27 @@
+package ami
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetLatestAmiID(t *testing.T) {
+
+	for id, d := range dataset {
+		got, err := d.ami.getLatestAmiID(
+			mockDescribeImagesOutput{
+				Resp: d.mockedResponse})
+		if err != nil {
+			t.Errorf("Unexpected error: %q",
+				err)
+		}
+
+		if strings.Compare(got, d.expectedGetAMI) != 0 {
+
+			t.Errorf("[%d] Wrong AMI ID returned:\nExpected Result:\t\t%q\nGot:\t\t\t\t\t%q",
+				id,
+				d.expectedSource,
+				got)
+		}
+	}
+}
---
+++ pkg/plugins/aws/ami/spec.go
@@ -0,0 +1,74 @@
+package ami
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	ErrWrongSortByValue error = errors.New("wrong value for key 'sortBy'")
+)
+
+// Spec contains updatecli configuration provided by users
+type Spec struct {
+	AccessKey string  // AWs access key
+	SecretKey string  // AWS secret key
+	Filters   Filters // AMI ID filters
+	Region    string  // AWS region to use when searching AMI
+	Endpoint  string  // Endpoint to use when searching AMI
+	DryRun    bool
+	SortBy    string // Specify the order of AMI-ID that will be used to retrieve the last element
+}
+
+// String return Spec information as a string
+func (s *Spec) String() (output string) {
+	output = output + "Region:\t" + s.Region
+	output = output + "\nEndpoint:\t" + s.Endpoint
+	output = output + fmt.Sprintf("\nFilters:\n  %s",
+		strings.ReplaceAll(s.Filters.String(), "\n", "\n  "))
+	return output
+}
+
+func getSortByAcceptedValues() []string {
+	return []string{
+		"creationdateasc",
+		"creationdatedesc",
+	}
+}
+
+// Validate ensure that configuration inject are correct
+func (s *Spec) Validate() (errs []error) {
+	if len(s.Region) == 0 {
+		logrus.Printf("No region specified, falling back to %s\n", "us-east-1")
+		s.Region = "us-east-1"
+	}
+
+	if len(s.Endpoint) == 0 {
+		s.Endpoint = fmt.Sprintf("https://ec2.%s.amazonaws.com", s.Region)
+	}
+
+	if len(s.Filters) == 0 {
+		errs = append(errs, ErrNoFilter)
+	}
+
+	if len(s.SortBy) > 0 {
+		found := false
+		for _, acceptedValue := range getSortByAcceptedValues() {
+			if strings.Compare(strings.ToLower(s.SortBy), strings.ToLower(acceptedValue)) == 0 {
+				found = true
+				// Ensure we use lowercase,
+				s.SortBy = strings.ToLower(s.SortBy)
+				break
+			}
+		}
+		if !found {
+			logrus.Printf("Invalid sortBy value %q", s.SortBy)
+			logrus.Printf("Accepted values: %v", getSortByAcceptedValues())
+			errs = append(errs, ErrWrongSortByValue)
+		}
+	}
+	return errs
+}
