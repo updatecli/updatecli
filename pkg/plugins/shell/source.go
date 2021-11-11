12976501374efaa833e98aed4a680ad097757249
Damien Duportal <damien.duportal@gmail.com>
2021-11-11 17:44:32 +0100

    fix(shell) correct logging output (#354)
    - Fixes #325 by printing newlines (using better block output with separators)
    - Centralized log method to avoid discrepancy between cases
    - Only prints error's output as Info when the command fails. Otherwise you need the "debug" flag.
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/shell/source.go
+++ pkg/plugins/shell/source.go
@@ -1,31 +1,16 @@
 package shell
 
-import (
-	"github.com/sirupsen/logrus"
-	"github.com/updatecli/updatecli/pkg/core/result"
-)
-
 // Source returns the stdout of the shell command if its exit code is 0
 // otherwise an error is returned with the content of stderr
 func (s *Shell) Source(workingDir string) (string, error) {
-	cmdResult, err := s.executor.ExecuteCommand(command{
+	s.executeCommand(command{
 		Cmd: s.spec.Command,
 		Dir: workingDir,
 	})
 
-	if err != nil {
-		return "", err
+	if s.result.ExitCode != 0 {
+		return "", &ExecutionFailedError{}
 	}
 
-	if cmdResult.ExitCode != 0 {
-		return "", &executionFailedError{
-			Command: s.spec.Command,
-			ErrCode: cmdResult.ExitCode,
-			Stdout:  cmdResult.Stdout,
-			Stderr:  cmdResult.Stderr}
-	}
-
-	logrus.Infof("%v The shell 🐚 command %q ran successfully and retrieved the following source value: %q", result.SUCCESS, s.spec.Command, cmdResult.Stdout)
-
-	return cmdResult.Stdout, nil
+	return s.result.Stdout, nil
 }
