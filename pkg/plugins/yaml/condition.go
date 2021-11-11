aba41b51d62525bddf3c0f2c0f43cd96d58ef2c3
Damien Duportal <damien.duportal@gmail.com>
2021-11-11 19:45:10 +0100

    fix(yaml) allow condition without a source input value
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/yaml/condition.go
+++ pkg/plugins/yaml/condition.go
@@ -2,85 +2,73 @@ package yaml
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
 	"github.com/sirupsen/logrus"
-
+	"github.com/updatecli/updatecli/pkg/core/result"
 	"github.com/updatecli/updatecli/pkg/core/scm"
 	"gopkg.in/yaml.v3"
 )
 
 // Condition checks if a key exists in a yaml file
 func (y *Yaml) Condition(source string) (bool, error) {
-	// Start by retrieving the specified file's content
-	if err := y.Read(); err != nil {
-		return false, err
-	}
-	data := y.CurrentContent
-
-	out := yaml.Node{}
-
-	err := yaml.Unmarshal([]byte(data), &out)
-
-	if err != nil {
-		return false, fmt.Errorf("cannot unmarshal data: %v", err)
-	}
-
-	valueFound, oldVersion, _ := replace(&out, strings.Split(y.Spec.Key, "."), y.Spec.Value, 1)
-
-	if valueFound && oldVersion == y.Spec.Value {
-		logrus.Infof("\u2714 Key '%s', from file '%v', is correctly set to %s'",
-			y.Spec.Key,
-			y.Spec.File,
-			y.Spec.Value)
-		return true, nil
-	} else if valueFound && oldVersion != y.Spec.Value {
-		logrus.Infof("\u2717 Key '%s', from file '%v', is incorrectly set to %s and should be %s'",
-			y.Spec.Key,
-			y.Spec.File,
-			oldVersion,
-			y.Spec.Value)
-	} else {
-		logrus.Infof("\u2717 cannot find key '%s' from file '%s'",
-			y.Spec.Key,
-			y.Spec.File)
-	}
-
-	return false, nil
+	return y.checkYamlCondition(source)
 }
 
 // ConditionFromSCM checks if a key exists in a yaml file
 func (y *Yaml) ConditionFromSCM(source string, scm scm.Scm) (bool, error) {
+	if !filepath.IsAbs(y.Spec.File) {
+		y.Spec.File = filepath.Join(scm.GetDirectory(), y.Spec.File)
+	}
+	return y.checkYamlCondition(source)
+}
+
+func (y *Yaml) checkYamlCondition(source string) (bool, error) {
 	// Start by retrieving the specified file's content
 	if err := y.Read(); err != nil {
 		return false, err
 	}
-	data := y.CurrentContent
-
 	out := yaml.Node{}
 
-	err := yaml.Unmarshal([]byte(data), &out)
+	err := yaml.Unmarshal([]byte(y.CurrentContent), &out)
 
 	if err != nil {
 		return false, fmt.Errorf("cannot unmarshal data: %v", err)
 	}
 
-	valueFound, oldVersion, _ := replace(&out, strings.Split(y.Spec.Key, "."), y.Spec.Value, 1)
+	// If a source is provided, then the key 'Value' cannot be specified
+	valueToCheck := y.Spec.Value
 
-	if valueFound && oldVersion == y.Spec.Value {
-		logrus.Infof("\u2714 Key '%s', from file '%v', is correctly set to %s'",
+	if len(source) > 0 {
+		if len(y.Spec.Value) > 0 {
+			validationError := fmt.Errorf("Validation error in condition of type 'yaml': the attributes `sourceID` and `spec.value` are mutually exclusive")
+			logrus.Errorf(validationError.Error())
+			return false, validationError
+		}
+
+		valueToCheck = source
+	}
+
+	valueFound, oldVersion, _ := replace(&out, strings.Split(y.Spec.Key, "."), valueToCheck, 1)
+
+	if valueFound && oldVersion == valueToCheck {
+		logrus.Infof("%s Key %q, in YAML file %q, is correctly set to %q",
+			result.SUCCESS,
 			y.Spec.Key,
 			y.Spec.File,
-			y.Spec.Value)
+			valueToCheck)
 		return true, nil
-	} else if valueFound && oldVersion != y.Spec.Value {
-		logrus.Infof("\u2717 Key '%s', from file '%v', is incorrectly set to %s and should be %s'",
+	} else if valueFound && oldVersion != valueToCheck {
+		logrus.Infof("%s Key %q, in YAML file %q, is incorrectly set to %s and should be %q",
+			result.FAILURE,
 			y.Spec.Key,
 			y.Spec.File,
 			oldVersion,
-			y.Spec.Value)
+			valueToCheck)
 	} else {
-		logrus.Infof("\u2717 cannot find key '%s' from file '%s'",
+		logrus.Infof("%s cannot find key %q in the YAML file %q",
+			result.FAILURE,
 			y.Spec.Key,
 			y.Spec.File)
 	}
