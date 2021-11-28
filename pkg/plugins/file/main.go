f9d55c3a0dbe13c7441e37551ed60c700d780052
Damien Duportal <damien.duportal@gmail.com>
2021-11-28 20:16:54 +0100

    fix(yaml,file) fail validation when the file does not exist (#401)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/file/main.go
+++ pkg/plugins/file/main.go
@@ -28,42 +28,70 @@ type File struct {
 // New returns a reference to a newly initialized File object from a Filespec
 // or an error if the provided Filespec triggers a validation error.
 func New(spec FileSpec) (*File, error) {
+	newFileResource := &File{
+		spec:             spec,
+		contentRetriever: &text.Text{},
+	}
+	// TODO: generalize the Validate + Normalize as an interface to all resources
+	err := newFileResource.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	err = newFileResource.Normalize()
+	if err != nil {
+		return nil, err
+	}
+
+	return newFileResource, nil
+}
+
+// Normalize ensures that the attributes of the object are following the expected conventions
+func (f *File) Normalize() error {
+	if strings.HasPrefix(f.spec.File, "file://") {
+		f.spec.File = strings.TrimPrefix(f.spec.File, "file://")
+	}
+
+	return nil
+}
+
+// Validate validates the object and returns an error (with all the failed validation messages) if it is not valid
+func (f *File) Validate() error {
 	var validationErrors []string
 
 	// Check for all validation
-	if spec.File == "" {
+	if f.spec.File == "" {
 		validationErrors = append(validationErrors, "Invalid spec for file resource: 'file' is empty.")
+	} else {
+		if !f.contentRetriever.FileExists(f.spec.File) {
+			validationErrors = append(validationErrors, fmt.Sprintf("Invalid spec for yaml resource: the file %q does not exist.", f.spec.File))
+		}
 	}
-	if spec.Line < 0 {
+	if f.spec.Line < 0 {
 		validationErrors = append(validationErrors, "Line cannot be negative for a file resource.")
 	}
-	if spec.Line > 0 {
-		if spec.ForceCreate {
+	if f.spec.Line > 0 {
+		if f.spec.ForceCreate {
 			validationErrors = append(validationErrors, "Validation error in target of type 'file': the attributes `spec.forcecreate` and `spec.line` are mutually exclusive")
 		}
 
-		if len(spec.MatchPattern) > 0 {
+		if len(f.spec.MatchPattern) > 0 {
 			validationErrors = append(validationErrors, "Validation error in target of type 'file': the attributes `spec.matchpattern` and `spec.line` are mutually exclusive")
 		}
 
-		if len(spec.ReplacePattern) > 0 {
+		if len(f.spec.ReplacePattern) > 0 {
 			validationErrors = append(validationErrors, "Validation error in target of type 'file': the attributes `spec.replacepattern` and `spec.line` are mutually exclusive")
 		}
 	}
-	if len(spec.Content) > 0 && len(spec.ReplacePattern) > 0 {
+	if len(f.spec.Content) > 0 && len(f.spec.ReplacePattern) > 0 {
 		validationErrors = append(validationErrors, "Validation error in target of type 'file': the attributes `spec.replacepattern` and `spec.line` are mutually exclusive")
 	}
 	// Return all the validation errors if found any
 	if len(validationErrors) > 0 {
-		return nil, fmt.Errorf("Validation error: the provided manifest configuration had the following validation errors:\n%s", strings.Join(validationErrors, "\n\n"))
+		return fmt.Errorf("Validation error: the provided manifest configuration had the following validation errors:\n%s", strings.Join(validationErrors, "\n\n"))
 	}
-	if strings.HasPrefix(spec.File, "file://") {
-		spec.File = strings.TrimPrefix(spec.File, "file://")
-	}
-	return &File{
-		spec:             spec,
-		contentRetriever: &text.Text{},
-	}, nil
+
+	return nil
 }
 
 // Read defines CurrentContent to the content of the file which path is specified in spec.File
