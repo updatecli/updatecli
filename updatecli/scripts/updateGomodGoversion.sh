b527434e4f72a3a6a8a6c6489bb375723c86d3e9
Damien Duportal <damien.duportal@gmail.com>
2021-12-21 15:12:07 +0100

    chore(updatecli) fix golang upgrade script
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- updatecli/scripts/updateGomodGoversion.sh
+++ updatecli/scripts/updateGomodGoversion.sh
@@ -13,24 +13,31 @@ go_version="${2}"
 new_version="$(echo "${go_version}" | cut -d. -f1,2)"
 tmp_dir="$(mktemp -d)"
 
-## Ensures that the correct golang version is used
+## Ensures that there is a golang version installed
 {
-  curl --silent --show-error --location --output "${tmp_dir}/go.tgz" \
-    "https://golang.org/dl/go${go_version}.linux-amd64.tar.gz"
-  mkdir -p "${tmp_dir}/.bin"
-  tar xzf "${tmp_dir}/go.tgz" -C "${tmp_dir}/.bin"
-  export PATH="${PATH}":"${tmp_dir}"/.bin/go/bin
+  if ! command -v go
+  then
+    curl --silent --show-error --location --output "${tmp_dir}/go.tgz" \
+      "https://golang.org/dl/go${go_version}.$(uname -s | tr '[:upper:]' '[:lower:]')-amd64.tar.gz"
+    mkdir -p "${tmp_dir}/.bin"
+    tar xzf "${tmp_dir}/go.tgz" -C "${tmp_dir}/.bin"
+    export PATH="${PATH}":"${tmp_dir}"/.bin/go/bin
+  fi
+
   command -v go
   go version
 } >&2
 
 ## Copy go mod's directory to a temp directory an start working from this temp. dir.
-cp -r "${go_mod_dir}" "${tmp_dir}" >&2
+cp -r "${go_mod_dir}"/* "${tmp_dir}" >&2
 cd "${tmp_dir}" >&2
+GOPATH="$(mktemp -d)"
+export GOPATH
 
 ## Update go mod properly
 go mod edit -go="${new_version}" >&2
 go mod tidy >&2
+echo "" >> go.mod ## Ad empty endline to be POSIX compliant
 
 ## Show new go mod
 cat go.mod
