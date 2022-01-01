3bbb72ea7f73e810e8d22cf30fe580374278a1c3
Damien Duportal <damien.duportal@gmail.com>
2022-01-01 16:36:18 +0100

    feat(dockerdigest,dockerimage) refactor docker(.*) resources into their own packages to fully supports image architectures
    == Functional changes:
    
    - Use the Registry API v2 for all dockerdigest and dockerimage calls:
      * Deprecates the "Bearer Token" specification in favor of username + password (or anonymous), as it's generated automatically as part of the V2 API
      * Fully support Architecture for images (bugfix for DockerHub images, new feature for quay.io images)
    
    == Technical changes:
    
    - Introduces a utility `docker` package with sub-packages
      * Defines the default values for image naming
      * Introduces `docker/dockerimage` sub-package to manage Docker images parsing logic
      * Introduces `docker/dockerregistry` sub-package to manage registry API logic
    
    - Introduces packages to allow unit testing with mocks from HTTP clients and Docker utilities:
      * Introduces `docker/dockermocks` sub-package to provides mocks for DockerRegistry logic
      * Introduces `pkg/core/httpclient` package to define HTTP client interface (and allows mocking it on each package)
    
    - Refactor dockerdigest resource:
      * In its own package, using the docker/* utilities
      * Add a `New` constructor method
      * Add unit tests with >80% code coverage
    
    - Refactor dockerimage resource:
      * In its own package, using the docker/* utilities
      * Add a `New` constructor method
      * Add unit tests with >80% code coverage
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

---
+++ pkg/plugins/dockerimage/main.go
@@ -0,0 +1,84 @@
+package dockerimage
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/updatecli/updatecli/pkg/plugins/docker/dockerimage"
+	"github.com/updatecli/updatecli/pkg/plugins/docker/dockerregistry"
+)
+
+// Spec defines a specification for a "dockerimage" resource
+// parsed from an updatecli manifest file
+type Spec struct {
+	Architecture string
+	Image        string
+	Tag          string
+	Username     string
+	Password     string
+	Token        string
+}
+
+// DockerImage defines a resource of type "dockerimage"
+type DockerImage struct {
+	spec     Spec
+	registry dockerregistry.Registry
+	image    dockerimage.Image
+}
+
+// New returns a reference to a newly initialized DockerImage object from a dockerimage.Spec
+// or an error if the provided Spec triggers a validation error.
+func New(newSpec Spec) (*DockerImage, error) {
+
+	newImage, err := dockerimage.New(
+		// concatenate user-provided image name (that may contain registry and namespace along with the namespace) with the user-provided tag
+		fmt.Sprintf("%s:%s", newSpec.Image, newSpec.Tag),
+		newSpec.Architecture,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	newRegistry := dockerregistry.New(
+		newImage.Registry,
+		newSpec.Username,
+		newSpec.Password,
+	)
+
+	newResource := &DockerImage{
+		spec:     newSpec,
+		registry: newRegistry,
+		image:    newImage,
+	}
+
+	err = newResource.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return newResource, nil
+}
+
+// Validate validates the object and returns an error (with all the failed validation messages) if it is not valid
+func (di *DockerImage) Validate() error {
+	var validationErrors []string
+
+	if len(di.spec.Token) > 0 {
+		if len(di.spec.Username) > 0 && len(di.spec.Password) > 0 {
+			validationErrors = append(validationErrors, "Specifying a (bearer) token is invalid when a username and a password are provided.")
+		}
+	}
+
+	if len(di.spec.Username) > 0 && len(di.spec.Password) == 0 {
+		validationErrors = append(validationErrors, "Docker registry username provided but not the password")
+	} else if len(di.spec.Username) == 0 && len(di.spec.Password) > 0 {
+		validationErrors = append(validationErrors, "Docker registry password provided but not the username")
+	}
+
+	// Return all the validation errors if found any
+	if len(validationErrors) > 0 {
+		return fmt.Errorf("Validation error: the provided manifest configuration had the following validation errors:\n%s", strings.Join(validationErrors, "\n\n"))
+	}
+
+	return nil
+}
