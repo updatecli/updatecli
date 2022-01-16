a67ea3a6cf2db8a4b7385ee141b447bd214233f4
Damien Duportal <damien.duportal@gmail.com>
2022-01-16 11:48:35 +0100

    chore(gittag) refactor condition to use only 1 logical route + support of disablesourceinput
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/gittag/condition.go
+++ pkg/plugins/gittag/condition.go
@@ -2,7 +2,6 @@ package gittag
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
@@ -12,7 +11,27 @@ import (
 
 // Condition checks that a git tag exists
 func (gt *GitTag) Condition(source string) (bool, error) {
-	if len(gt.spec.VersionFilter.Pattern) == 0 {
+	return gt.condition(source)
+}
+
+// ConditionFromSCM test if a tag exist from a git repository specific from SCM
+func (gt *GitTag) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error) {
+	path := scm.GetDirectory()
+
+	if len(gt.spec.Path) > 0 {
+		logrus.Warningf("Path is defined and set to %q but is overridden by the scm definition %q",
+			gt.spec.Path,
+			path)
+	}
+	gt.spec.Path = path
+
+	return gt.condition(source)
+}
+
+func (gt *GitTag) condition(source string) (bool, error) {
+	// If source input is empty, then it as disabled by the user with `disablesourceinput: true`
+	if source != "" {
+		logrus.Infof("Source Input Value detected: using it as spec.versionfilter.pattern")
 		gt.spec.VersionFilter.Pattern = source
 	}
 
@@ -23,7 +42,6 @@ func (gt *GitTag) Condition(source string) (bool, error) {
 	}
 
 	tags, err := generic.Tags(gt.spec.Path)
-
 	if err != nil {
 		logrus.Errorln(err)
 		return false, err
@@ -36,63 +54,19 @@ func (gt *GitTag) Condition(source string) (bool, error) {
 	tag := gt.foundVersion.ParsedVersion
 
 	if len(tag) == 0 {
-		err = fmt.Errorf("no git tag matching pattern %q, found", gt.spec.VersionFilter.Pattern)
+		err = fmt.Errorf("No Git Tag matching pattern %q, found", gt.spec.VersionFilter.Pattern)
 		return false, err
 	}
-
-	if strings.Compare(tag, gt.spec.VersionFilter.Pattern) == 0 {
-		logrus.Printf("%s git tag %q matching\n", result.SUCCESS, gt.spec.VersionFilter.Pattern)
-		return true, nil
-	}
-
-	logrus.Printf("%s git tag %q not matching %q\n",
-		result.FAILURE,
-		gt.spec.VersionFilter.Pattern,
-		tag)
-
-	return false, nil
-}
-
-// ConditionFromSCM test if a tag exist from a git repository specific from SCM
-func (gt *GitTag) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error) {
-	path := scm.GetDirectory()
-
-	if len(gt.spec.VersionFilter.Pattern) == 0 {
-		gt.spec.VersionFilter.Pattern = source
-	}
-
-	if len(gt.spec.Path) > 0 {
-		logrus.Warningf("Path is defined and set to %q but is overridden by the scm definition %q",
-			gt.spec.Path,
-			path)
-	}
-	gt.spec.Path = path
-
-	err := gt.Validate()
-	if err != nil {
-		return false, err
-	}
-
-	tags, err := generic.Tags(gt.spec.Path)
-
-	if err != nil {
-		logrus.Errorln(err)
-		return false, err
-	}
-
-	gt.foundVersion, err = gt.spec.VersionFilter.Search(tags)
-	if err != nil {
-		return false, err
-	}
-	tag := gt.foundVersion.ParsedVersion
 
 	if tag == gt.spec.VersionFilter.Pattern {
 		logrus.Printf("%s Git Tag %q matching\n", result.SUCCESS, gt.spec.VersionFilter.Pattern)
 		return true, nil
 	}
+
 	logrus.Printf("%s Git Tag %q not matching %q\n",
 		result.FAILURE,
 		gt.spec.VersionFilter.Pattern,
 		tag)
+
 	return false, nil
 }
