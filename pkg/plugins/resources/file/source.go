8c0ba9e2be1506d57ccea0685a89d0e6013bb967
Hervé Le Meur <hlemeur@cloudbees.com>
2022-01-30 15:50:47 +0100

    fix: sources

--- pkg/plugins/resources/file/source.go
+++ pkg/plugins/resources/file/source.go
@@ -14,7 +14,18 @@ import (
 // Source return a file content
 func (f *File) Source(workingDir string) (string, error) {
 	var validationErrors []string
+	var foundContent string
 
+	if len(f.spec.Files) == 0 && len(f.spec.File) == 0 {
+		// TODO: to be updated after 'file' deprecation
+		validationErrors = append(validationErrors, "Invalid spec for file resource: both 'file' and 'files' are empty.")
+	}
+	if len(f.spec.Files) > 0 && len(f.spec.File) > 0 {
+		validationErrors = append(validationErrors, "Validation error in source of type 'file': the attributes `spec.file` and `spec.files` are mutually exclusive")
+	}
+	if len(f.spec.Files) > 1 {
+		validationErrors = append(validationErrors, "Validation error in source of type 'file': the attributes `spec.files` can't contain more than one element for sources")
+	}
 	if len(f.spec.ReplacePattern) > 0 {
 		validationErrors = append(validationErrors, "Validation error in source of type 'file': the attribute `spec.replacepattern` is only supported for targets.")
 	}
@@ -39,25 +50,28 @@ func (f *File) Source(workingDir string) (string, error) {
 		return "", err
 	}
 
-	foundContent := f.CurrentContent
-	// If a matchPattern is specified, then retrieve the string matched and returns the (eventually) multi-line string
-	if len(f.spec.MatchPattern) > 0 {
-		reg, err := regexp.Compile(f.spec.MatchPattern)
-		if err != nil {
-			logrus.Errorf("Validation error in source of type 'file': Unable to parse the regexp specified at f.spec.MatchPattern (%q)", f.spec.MatchPattern)
-			return "", err
+	// Looping on the only filePath in 'files'
+	for filePath := range f.files {
+		foundContent = f.files[filePath]
+		// If a matchPattern is specified, then retrieve the string matched and returns the (eventually) multi-line string
+		if len(f.spec.MatchPattern) > 0 {
+			reg, err := regexp.Compile(f.spec.MatchPattern)
+			if err != nil {
+				logrus.Errorf("Validation error in source of type 'file': Unable to parse the regexp specified at f.spec.MatchPattern (%q)", f.spec.MatchPattern)
+				return "", err
+			}
+
+			// Check if there is any match in the file
+			if !reg.MatchString(f.CurrentContent) {
+				return "", fmt.Errorf("No line matched in the file %q for the pattern %q", filePath, f.spec.MatchPattern)
+			}
+			matchedStrings := reg.FindAllString(f.CurrentContent, -1)
+
+			foundContent = strings.Join(matchedStrings, "\n")
 		}
 
-		// Check if there is any match in the file
-		if !reg.MatchString(f.CurrentContent) {
-			return "", fmt.Errorf("No line matched in the file %q for the pattern %q", f.spec.File, f.spec.MatchPattern)
-		}
-		matchedStrings := reg.FindAllString(f.CurrentContent, -1)
+		logrus.Infof("%s Content: found from file %q:\n%v", result.SUCCESS, filePath, foundContent)
 
-		foundContent = strings.Join(matchedStrings, "\n")
 	}
-
-	logrus.Infof("%s Content: found from file %q:\n%v", result.SUCCESS, f.spec.File, foundContent)
-
 	return foundContent, nil
 }
