4a453d0e7bc16bd6c156832eb895c1b00bc27c7d
Olblak <me@olblak.com>
2022-02-26 09:49:28 +0100

    Improve jsonschme test code
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/config/jsonschema/main_test.go
+++ pkg/core/config/jsonschema/main_test.go
@@ -1,84 +1,51 @@
 package jsonschema
 
 import (
-	"io/ioutil"
-	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type Dataset []Data
 
 // Data represent a single json schema test data
 type Data struct {
-	updatecliManifest string
-	expectedErr       error
-	expectedResult    bool
+	// name defines a test name
+	name                  string
+	updatecliManifestPath string
+	expectedErrMessage    error
+	expectedErr           bool
+	expectedResult        bool
 }
 
 var (
 	TestDataset Dataset = Dataset{
 		{
-			updatecliManifest: "../../../../e2e/updatecli.d",
-			expectedResult:    true,
-			expectedErr:       nil,
+			name:                  "Test using e2e updateli manifest",
+			updatecliManifestPath: "../../../../e2e/updatecli.d",
+			expectedResult:        true,
+			expectedErrMessage:    nil,
+			expectedErr:           true,
 		},
 	}
 )
-
-func readFile(file string) ([]byte, error) {
-	c, err := os.Open(file)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer c.Close()
-
-	content, err := ioutil.ReadAll(c)
-	if err != nil {
-		return nil, err
-	}
-
-	return content, nil
-}
 
 // TestValidate tests the function Validate by Using updatecli manifest
 // from the e2e directory.
 func TestValidate(t *testing.T) {
 
-	failingManifest := []string{}
-
 	for _, d := range TestDataset {
-		updatecliManifests, err := getFilesWithSuffix(d.updatecliManifest, ".yaml")
-		if err != nil {
-			t.Errorf("%s", err)
-		}
-
-		for _, manifest := range updatecliManifests {
-			t.Logf("Validating file %q\n", manifest)
-			m, err := readFile(manifest)
-			if err != nil {
-				t.Errorf("%s", err)
+		t.Run(d.name, func(t *testing.T) {
+			gotResult, gotErr := Validate(d.updatecliManifestPath)
+			if d.expectedErr {
+				require.Error(t, gotErr)
 			}
 
-			result, err := Validate(m)
-			if err != nil {
-				t.Errorf("Unexpected error for file %q\nError: %s",
-					manifest, err)
+			if gotResult != d.expectedResult {
+				t.Error()
 			}
-			if d.expectedResult != result {
-				failingManifest = append(failingManifest, manifest)
-				t.Errorf("Expecting file %q to be valid", manifest)
-			}
-		}
 
+			require.NoError(t, gotErr)
+		})
 	}
-
-	if len(failingManifest) > 0 {
-		t.Logf("%d updatecli manifests failed\n", len(failingManifest))
-		for _, m := range failingManifest {
-			t.Logf("\t%s\n", m)
-		}
-	}
-
 }
