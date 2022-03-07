b732e43c089dfb70b94921e22f812c2275d1eec5
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-03-07 15:37:23 +0100

    Update pkg/core/transformer/findSubMatch.go
    Co-authored-by: Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>

--- pkg/core/transformer/findSubMatch.go
+++ pkg/core/transformer/findSubMatch.go
@@ -1,6 +1,6 @@
 package transformer
 
-// FindSubMatch is struct used to feed regexp.findSubMatch
+// FindSubMatch is a struct used to feed regexp.findSubMatch
 type FindSubMatch struct {
 	Pattern      string `yaml:"pattern"`
 	CaptureIndex int    `yaml:"captureIndex"`
