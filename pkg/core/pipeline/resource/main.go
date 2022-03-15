9d71d4a58ef7f14a6092a3d049f86c5c53118b54
Olblak <me@olblak.com>
2022-03-15 22:20:13 +0100

    Implement resource logic for jsonschema
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/pipeline/resource/main.go
+++ pkg/core/pipeline/resource/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/iancoleman/orderedmap"
+	"github.com/invopop/jsonschema"
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/config/jsonschema/commentmap"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
 	"github.com/updatecli/updatecli/pkg/core/transformer"
 	"github.com/updatecli/updatecli/pkg/plugins/resources/awsami"
@@ -22,16 +26,21 @@ import (
 
 type ResourceConfig struct {
 	DependsOn []string `yaml:"depends_on"`
-	Name      string
-	Kind      string
-	// Deprecated in favor of Transformers on 2021/01/3
+	// Name defines a resource name
+	Name string
+	// Kind defines a resource kind such as yaml
+	Kind string
+	// **Deprecated** Please consider `Transformers`
 	Prefix string
-	// Deprecated in favor of Transformers on 2021/01/3
-	Postfix      string
+	// **Deprecated** Please consider `Transformers`
+	Postfix string
+	// Define a lit of Transformer
 	Transformers transformer.Transformers
-	Spec         interface{} `jsonschema:"type=object"`
-	// Deprecated field on version [1.17.0]
-	Scm   map[string]interface{}
+	// Define resource spec according its kind
+	Spec interface{} `jsonschema:"type=object"`
+	// **Deprecated** Please look in the top scms resource
+	Scm map[string]interface{}
+	// Define which scm resource is linked to the current resource
 	SCMID string `yaml:"scmID"` // SCMID references a uniq scm configuration
 }
 
@@ -75,4 +84,86 @@ type Resource interface {
 	Target(source string, dryRun bool) (bool, error)
 	TargetFromSCM(source string, scm scm.ScmHandler, dryRun bool) (changed bool, files []string, message string, err error)
 	Changelog() string
+}
+
+// Need to do reflect of ResourceConfig
+func (ResourceConfig) JSONSchema() *jsonschema.Schema {
+
+	type Resource struct {
+		Kind string
+		Spec interface{}
+	}
+
+	type ResourceConfigSchema ResourceConfig
+
+	type Resources []Resource
+
+	// SchemaResources contains list of every resource mapping
+	var SchemaResources Resources = Resources{
+		Resource{
+			Kind: "jenkins",
+			Spec: &jenkins.Spec{},
+		},
+		Resource{
+			Kind: "file",
+			Spec: &file.Spec{},
+		},
+		Resource{
+			Kind: "yaml",
+			Spec: &yaml.Spec{},
+		},
+		Resource{
+			Kind: "aws/ami",
+			Spec: &awsami.Spec{},
+		},
+	}
+
+	var err error
+
+	r := new(jsonschema.Reflector)
+
+	r.Anonymous = true
+	r.YAMLEmbeddedStructs = true
+	r.DoNotReference = true
+	r.RequiredFromJSONSchemaTags = true
+
+	commentMap, err := commentmap.Get("../../../../pkg")
+
+	if err != nil {
+		logrus.Errorf(err.Error())
+		return nil
+	}
+
+	resourceSchema := r.Reflect(ResourceConfigSchema{})
+
+	for _, updatecliResource := range SchemaResources {
+		r := new(jsonschema.Reflector)
+
+		r.Anonymous = true
+		r.PreferYAMLSchema = true
+		r.YAMLEmbeddedStructs = true
+		r.DoNotReference = true
+		r.RequiredFromJSONSchemaTags = true
+		r.CommentMap = commentMap
+
+		// schema we need a way to remove schema
+
+		// Main resource type schema such as source or condition
+		s := r.Reflect(updatecliResource.Spec)
+		s.ContentSchema = nil
+
+		var spec jsonschema.Schema
+
+		spec.Type = "object"
+		spec.Required = []string{"kind"}
+		spec.Properties = orderedmap.New()
+		spec.Properties.Set("spec", s.Properties)
+		spec.Properties.Set("kind", jsonschema.Schema{
+			Enum: []interface{}{updatecliResource.Kind}})
+
+		resourceSchema.OneOf = append(resourceSchema.OneOf, &spec)
+
+	}
+
+	return resourceSchema
 }
