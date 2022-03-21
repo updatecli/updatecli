0197f47e5cc5df679dee1fafb588087aa330af28
Damien Duportal <damien.duportal@gmail.com>
2022-03-21 20:36:25 +0100

    cleanup(gittag, githubrelease) remove deprecated code for field `version` (#590)
    * chore(e2e) allow to specify a custom updatecli binary path for local testing
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * chore(scms) remove usage of 'source' in SCM logic
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup(gittag, githubrelease) remove deprecated code for version (version.Filter is used instead)
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/resources/githubrelease/main.go
+++ pkg/plugins/resources/githubrelease/main.go
@@ -1,46 +1,56 @@
 package githubrelease
 
 import (
-	"path"
-
 	"github.com/mitchellh/mapstructure"
-	"github.com/updatecli/updatecli/pkg/core/tmp"
 	"github.com/updatecli/updatecli/pkg/plugins/scms/github"
 	"github.com/updatecli/updatecli/pkg/plugins/utils/version"
 )
 
+// Spec defines a specification for a "gittag" resource
+// parsed from an updatecli manifest file
+type Spec struct {
+	Owner         string         // Owner specifies repository owner
+	Repository    string         // Repository specifies the name of a repository for a specific owner
+	Token         string         // Token specifies the credential used to authenticate with
+	URL           string         // URL specifies the default github url in case of GitHub enterprise
+	Username      string         // Username specifies the username used to authenticate with Github API
+	VersionFilter version.Filter // VersionFilter provides parameters to specify version pattern and its type like regex, semver, or just latest.
+}
+
 // GitHubRelease defines a resource of kind "githubrelease"
 type GitHubRelease struct {
 	ghHandler     github.GithubHandler
-	versionFilter version.Filter
+	versionFilter version.Filter // Holds the "valid" version.filter, that might be different than the user-specified filter (Spec.VersionFilter)
 	foundVersion  version.Version
 }
 
 // New returns a new valid GitHubRelease object.
 func New(spec interface{}) (*GitHubRelease, error) {
-
-	newSpec := github.Spec{}
+	newSpec := Spec{}
 
 	err := mapstructure.Decode(spec, &newSpec)
 	if err != nil {
 		return &GitHubRelease{}, err
 	}
 
-	if newSpec.Directory == "" {
-		newSpec.Directory = path.Join(tmp.Directory, newSpec.Owner, newSpec.Repository)
+	newHandler, err := github.New(github.Spec{
+		Owner:      newSpec.Owner,
+		Repository: newSpec.Repository,
+		Token:      newSpec.Token,
+		URL:        newSpec.URL,
+		Username:   newSpec.Username,
+	})
+	if err != nil {
+		return &GitHubRelease{}, err
 	}
 
-	if newSpec.URL == "" {
-		newSpec.URL = "github.com"
-	}
-
-	newHandler, err := github.New(newSpec)
+	newFilter, err := newSpec.VersionFilter.Init()
 	if err != nil {
 		return &GitHubRelease{}, err
 	}
 
 	return &GitHubRelease{
 		ghHandler:     newHandler,
-		versionFilter: newHandler.Spec.VersionFilter,
+		versionFilter: newFilter,
 	}, nil
 }
