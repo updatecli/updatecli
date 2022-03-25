3a1722d1b61ce2af421ffb452235a3f5ccd0953d
Olblak <me@olblak.com>
2022-03-25 16:43:31 +0100

    Add jsonschema command verbosity
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/engine/main.go
+++ pkg/core/engine/main.go
@@ -267,6 +267,36 @@ func (e *Engine) Show() error {
 }
 
 func GenerateSchema(baseSchemaID, schemaDir string) error {
+
+	logrus.Infof("\n\n%s\n", strings.Repeat("+", len("Json Schema")+4))
+	logrus.Infof("+ %s +\n", strings.ToTitle("Json Schema"))
+	logrus.Infof("%s\n\n", strings.Repeat("+", len("Json Schema")+4))
+
+	err := schema.CloneCommentDirectory()
+
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		tmperr := schema.CleanCommentDirectory()
+		if err != nil {
+			err = fmt.Errorf("%s\n%s", err, tmperr)
+		}
+	}()
+
 	s := schema.New(baseSchemaID, schemaDir)
+	err = s.GenerateSchema(&config.Config{})
+	if err != nil {
+		return err
+	}
+
+	logrus.Infof("```\n%s\n```\n", s)
+
+	err = s.Save()
+	if err != nil {
+		return err
+	}
+
 	return s.GenerateSchema(&config.Config{})
 }
