7b290cbaf3e4e30a7565383ae4bc671855feb7cb
Hervé Le Meur <hlemeur@cloudbees.com>
2022-04-11 20:42:46 +0200

    fix: lint

--- pkg/plugins/resources/yaml/target.go
+++ pkg/plugins/resources/yaml/target.go
@@ -124,7 +124,7 @@ func (y *Yaml) target(source string, dryRun bool) (bool, []string, string, error
 				return false, files, message.String(), err
 			}
 			// TODO: check if all this encoder stuff is needed, and why yamlIndent isn't respected
-			err = y.contentRetriever.WriteToFile(string(y.files[filePath]), filePath)
+			err = y.contentRetriever.WriteToFile(y.files[filePath], filePath)
 			if err != nil {
 				return false, files, message.String(), err
 			}
