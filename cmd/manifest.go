11a9faa55a2f44bce3a6692476cb8642e83e55cf
Olblak <me@olblak.com>
2022-04-27 21:44:58 +0200

    Add subcommand manifest upgrade
    Signed-off-by: Olblak <me@olblak.com>

---
+++ cmd/manifest.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	manifestCmd = &cobra.Command{
+		Use:   "manifest",
+		Short: "manifest executes specific manifest task such as upgrade",
+	}
+
+	manifestUpgradeCmd = &cobra.Command{
+		Use:   "upgrade",
+		Short: "upgrade executes manifest upgrade task",
+		Run: func(cmd *cobra.Command, args []string) {
+			e.Options.Config.ManifestFile = cfgFile
+			e.Options.Config.ValuesFiles = valuesFiles
+			e.Options.Config.SecretsFiles = secretsFiles
+
+			err := run("manifest/upgrade")
+			if err != nil {
+				logrus.Errorf("command failed")
+				os.Exit(1)
+			}
+		},
+	}
+)
+
+func init() {
+	manifestUpgradeCmd.Flags().StringVarP(&cfgFile, "config", "c", "./updatecli.yaml", "Sets config file or directory. (default: './updatecli.yaml')")
+	manifestUpgradeCmd.Flags().StringArrayVarP(&valuesFiles, "values", "v", []string{}, "Sets values file uses for templating")
+	manifestUpgradeCmd.Flags().StringArrayVar(&secretsFiles, "secrets", []string{}, "Sets Sops secrets file uses for templating")
+
+	manifestCmd.AddCommand(manifestUpgradeCmd)
+}
