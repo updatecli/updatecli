f2648a61394d00c8a08413efe711b2327bad48c0
Olblak <me@olblak.com>
2022-06-13 18:02:15 +0200

    Fix manifest upgrade diff
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/config/main.go
+++ pkg/core/config/main.go
@@ -199,7 +199,7 @@ func (c *Config) IsManifestDifferentThanOnDisk() (bool, error) {
 	}
 
 	edits := myers.ComputeEdits(span.URIFromPath(c.filename), string(onDiskData), string(data))
-	diff := fmt.Sprint(gotextdiff.ToUnified(c.filename+"(old)", c.filename+"(new)", string(data), edits))
+	diff := fmt.Sprint(gotextdiff.ToUnified(c.filename+"(old)", c.filename+"(new)", string(onDiskData), edits))
 
 	logrus.Debugf("%s Updatecli manifest change required\n%s", result.ATTENTION, diff)
 
