9f0ec9e5aba081fd9996c70c447608e6a3090ae6
Hervé Le Meur <hlemeur@cloudbees.com>
2022-06-21 11:39:53 +0200

    Revert "wip: first stab at files for condition too"
    This reverts commit 957f21e6dea61ba172dd7d12f6173615563aebaa.

--- pkg/plugins/resources/yaml/condition.go
+++ pkg/plugins/resources/yaml/condition.go
@@ -33,90 +33,84 @@ func (y *Yaml) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error)
 }
 
 func (y *Yaml) condition(source string) (bool, error) {
-	// TODO: check if needed
-	// // Test if target reference a file with a prefix like https:// or file://, as we don't know how to update those files.
-	// for filePath := range y.files {
-	// 	// Test at runtime if a file exist (no ForceCreate for kind: yaml)
-	// 	if !y.contentRetriever.FileExists(filePath) {
-	// 		return false, fmt.Errorf("the yaml file %q does not exist", filePath)
-	// 	}
-	// }
+	var fileContent string
+	var filePath string
 
-	// Start by retrieving the specified files content
+	if len(y.files) > 1 {
+		validationError := fmt.Errorf("Validation error in conditions of type 'yaml': the attributes `spec.files` can't contain more than one element for conditions")
+		logrus.Errorf(validationError.Error())
+		return false, validationError
+	}
+
+	// Start by retrieving the specified file's content
 	if err := y.Read(); err != nil {
 		return false, err
 	}
+	out := yaml.Node{}
 
-	// loop over file(s)
-	for filePath := range y.files {
-		out := yaml.Node{}
-
-		err := yaml.Unmarshal([]byte(y.files[filePath]), &out)
-
-		if err != nil {
-			return false, fmt.Errorf("cannot unmarshal data: %v", err)
-		}
-
-		// If a source is provided, then the key 'Value' cannot be specified
-		valueToCheck := y.spec.Value
-
-		// When user want to only check the existence of a YAML key
-		if y.spec.KeyOnly {
-			// Then there must not be any specified Value
-			if y.spec.Value != "" {
-				validationError := fmt.Errorf("Validation error in condition of type 'yaml': both `spec.value` and `spec.keyonly` specified while mutually exclusive. Remove one of these 2 directives.")
-				logrus.Errorf(validationError.Error())
-				return false, validationError
-			}
-
-			valueFound, _, _ := replace(&out, strings.Split(y.spec.Key, "."), "", 1)
-			if valueFound {
-				logrus.Infof("%s Key %q, in YAML file %q, is correctly found",
-					result.SUCCESS,
-					y.spec.Key,
-					filePath)
-			} else {
-				return false, nil
-			}
-		} else {
-			// When user want to check the value of YAML key and when the input source value is not empty
-			if source != "" {
-				// Then there must not be any specified Value
-				if y.spec.Value != "" {
-					validationError := fmt.Errorf("Validation error in condition of type 'yaml': input source value detected, while `spec.value` specified. Add 'disablesourceinput: true' to your manifest to keep ``spec.value`.")
-					logrus.Errorf(validationError.Error())
-					return false, validationError
-				}
-
-				// Use the source input value in this case
-				valueToCheck = source
-			}
-
-			valueFound, oldVersion, _ := replace(&out, strings.Split(y.spec.Key, "."), valueToCheck, 1)
-
-			if valueFound {
-				if oldVersion == valueToCheck {
-					logrus.Infof("%s Key %q, in YAML file %q, is correctly set to %q",
-						result.SUCCESS,
-						y.spec.Key,
-						filePath,
-						valueToCheck)
-				} else {
-					logrus.Infof("%s Key %q, in YAML file %q, is incorrectly set to %s and should be %q",
-						result.FAILURE,
-						y.spec.Key,
-						filePath,
-						oldVersion,
-						valueToCheck)
-					return false, nil
-				}
-			} else {
-				return false, fmt.Errorf("%s cannot find key %q in the YAML file %q",
-					result.FAILURE,
-					y.spec.Key,
-					filePath)
-			}
-		}
+	// loop over the only file
+	for theFilePath := range y.files {
+		fileContent = y.files[theFilePath]
+		filePath = theFilePath
 	}
-	return true, nil
+	err := yaml.Unmarshal([]byte(fileContent), &out)
+
+	if err != nil {
+		return false, fmt.Errorf("cannot unmarshal data: %v", err)
+	}
+
+	// If a source is provided, then the key 'Value' cannot be specified
+	valueToCheck := y.spec.Value
+
+	// When user want to only check the existence of a YAML key
+	if y.spec.KeyOnly {
+		// Then there must not be any specified Value
+		if y.spec.Value != "" {
+			validationError := fmt.Errorf("Validation error in condition of type 'yaml': both `spec.value` and `spec.keyonly` specified while mutually exclusive. Remove one of these 2 directives.")
+			logrus.Errorf(validationError.Error())
+			return false, validationError
+		}
+
+		valueFound, _, _ := replace(&out, strings.Split(y.spec.Key, "."), "", 1)
+
+		return valueFound, nil
+	}
+
+	// When user want to check the value of YAML key and when the input source value is not empty
+	if source != "" {
+		// Then there must not be any specified Value
+		if y.spec.Value != "" {
+			validationError := fmt.Errorf("Validation error in condition of type 'yaml': input source value detected, while `spec.value` specified. Add 'disablesourceinput: true' to your manifest to keep ``spec.value`.")
+			logrus.Errorf(validationError.Error())
+			return false, validationError
+		}
+
+		// Use the source input value in this case
+		valueToCheck = source
+	}
+
+	valueFound, oldVersion, _ := replace(&out, strings.Split(y.spec.Key, "."), valueToCheck, 1)
+
+	if valueFound {
+		if oldVersion == valueToCheck {
+			logrus.Infof("%s Key %q, in YAML file %q, is correctly set to %q",
+				result.SUCCESS,
+				y.spec.Key,
+				filePath,
+				valueToCheck)
+			return true, nil
+		}
+
+		logrus.Infof("%s Key %q, in YAML file %q, is incorrectly set to %s and should be %q",
+			result.FAILURE,
+			y.spec.Key,
+			filePath,
+			oldVersion,
+			valueToCheck)
+		return false, nil
+	}
+	return false, fmt.Errorf("%s cannot find key %q in the YAML file %q",
+		result.FAILURE,
+		y.spec.Key,
+		filePath)
 }
