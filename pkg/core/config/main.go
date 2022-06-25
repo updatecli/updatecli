fb137f604967d4e6943fc1eba2a5e4d14e90bf3a
Olblak <me@olblak.com>
2022-06-25 09:18:44 +0200

    Move error variable to a specific file
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/config/main.go
+++ pkg/core/config/main.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -24,28 +23,6 @@ import (
 	"github.com/hexops/gotextdiff"
 	"github.com/hexops/gotextdiff/myers"
 	"github.com/hexops/gotextdiff/span"
-)
-
-var (
-	// ErrConfigFileTypeNotSupported is returned when updatecli try to read
-	// an unsupported file type.
-	ErrConfigFileTypeNotSupported = errors.New("file extension not supported")
-
-	// ErrBadConfig is returned when updatecli try to read
-	// a wrong configuration.
-	ErrBadConfig = errors.New("wrong updatecli configuration")
-
-	// ErrNoEnvironmentVariableSet is returned when during the templating process,
-	// it tries to access en environment variable not set.
-	ErrNoEnvironmentVariableSet = errors.New("environment variable doesn't exist")
-
-	// ErrNoKeyDefined is returned when during the templating process, it tries to
-	// retrieve a key value which is not defined in the configuration
-	ErrNoKeyDefined = errors.New("key not defined in configuration")
-
-	// ErrNotAllowedTemplatedKey is returned when
-	// we are planning to template at runtime unauthorized keys such map key
-	ErrNotAllowedTemplatedKey = errors.New("not allowed templated key")
 )
 
 // Config contains cli configuration
