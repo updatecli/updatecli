11bb9479696cf1011770fd218e1958619de95a72
Olblak <me@olblak.com>
2022-07-24 13:06:11 +0200

    Fix autodiscovery helm test
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/helm/main_test.go
+++ pkg/plugins/autodiscovery/helm/main_test.go
@@ -7,10 +7,98 @@ import (
 	goyaml "gopkg.in/yaml.v3"
 )
 
+var (
+	expectedPipelines map[string]string = map[string]string{
+		"epinio-minio": `name: epinio-minio
+sources:
+    minio:
+        name: Get latest "minio" Helm Chart Version
+        kind: helmchart
+        spec:
+            name: minio
+            url: https://charts.min.io/
+conditions:
+    minio:
+        name: Ensure dependency "minio" is specified
+        kind: yaml
+        spec:
+            file: epinio/Chart.yaml
+            key: dependencies[0].name
+            value: minio
+        disablesourceinput: true
+targets:
+    minio:
+        name: Bump chart dependency "minio" in Chart "epinio"
+        kind: helmchart
+        spec:
+            file: Chart.yaml
+            key: dependencies[0].version
+            name: epinio
+            versionincrement: minor
+        sourceid: minio
+`,
+		"epinio-kubed": `name: epinio-kubed
+sources:
+    kubed:
+        name: Get latest "kubed" Helm Chart Version
+        kind: helmchart
+        spec:
+            name: kubed
+            url: https://charts.appscode.com/stable/
+conditions:
+    kubed:
+        name: Ensure dependency "kubed" is specified
+        kind: yaml
+        spec:
+            file: epinio/Chart.yaml
+            key: dependencies[1].name
+            value: kubed
+        disablesourceinput: true
+targets:
+    kubed:
+        name: Bump chart dependency "kubed" in Chart "epinio"
+        kind: helmchart
+        spec:
+            file: Chart.yaml
+            key: dependencies[1].version
+            name: epinio
+            versionincrement: minor
+        sourceid: kubed
+`,
+		"epinio-epinio-ui": `name: epinio-epinio-ui
+sources:
+    epinio-ui:
+        name: Get latest "epinio-ui" Helm Chart Version
+        kind: helmchart
+        spec:
+            name: epinio-ui
+            url: https://epinio.github.io/helm-charts
+conditions:
+    epinio-ui:
+        name: Ensure dependency "epinio-ui" is specified
+        kind: yaml
+        spec:
+            file: epinio/Chart.yaml
+            key: dependencies[2].name
+            value: epinio-ui
+        disablesourceinput: true
+targets:
+    epinio-ui:
+        name: Bump chart dependency "epinio-ui" in Chart "epinio"
+        kind: helmchart
+        spec:
+            file: Chart.yaml
+            key: dependencies[2].version
+            name: epinio
+            versionincrement: minor
+        sourceid: epinio-ui
+`}
+)
+
 func TestDiscoverManifests(t *testing.T) {
 
 	spec := Spec{
-		RootDir: "/home/olblak/Projects/Epinio/helm-charts",
+		RootDir: "testdata/chart",
 	}
 
 	helm, err := New(spec, "")
@@ -33,8 +121,13 @@ func TestDiscoverManifests(t *testing.T) {
 			t.Errorf("%v", err)
 		}
 
-		fmt.Printf("%v\n", string(output))
+		if string(output) != expectedPipelines[pipeline.Name] {
+			fmt.Println("Wrong result")
+			fmt.Printf("Expected:\n>>>\n%q\n>>>\n\n", expectedPipelines[pipeline.Name])
+			fmt.Printf("Got:\n<<<\n%q\n<<<\n", output)
+			t.Fail()
+		}
+
 	}
 
-	t.Fail()
 }
