adfc1f8941759ed42cd49f01ef1ea4473cfbf40b
Olivier Vernin <olivier@vernin.me>
2022-09-02 18:41:13 +0200

    Feat: Add Autodiscovery for Maven dependencies  (#833)
    * Add parameters repositories to maven resource
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add maven dependency autodiscovery
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * User a better testdata example
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Move pom.xml spec to a specific go file
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add function to retrieve dependenycManagement and parent information from pom.xml
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add parentPom and dependencyManagement support
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: dependencies weren't correctly updated
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add maven autodiscovery main test
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix maven condition title
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Handle empty dependency/parent
    
    Signed-off-by: Olblak <me@olblak.com>
    
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/autodiscovery/maven/main.go
@@ -0,0 +1,130 @@
+package maven
+
+import (
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/config"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/pullrequest"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
+
+	discoveryConfig "github.com/updatecli/updatecli/pkg/core/pipeline/autodiscovery/config"
+)
+
+// Spec defines the parameters which can be provided to the Helm builder.
+type Spec struct {
+	// RootDir defines the root directory used to recursively search for Helm Chart
+	RootDir string `yaml:",omitempty"`
+	// Disable allows to disable the helm chart crawler
+	Disable bool `yaml:",omitempty"`
+	// Ignore allows to specify rule to ignore autodiscovery a specific Helm based on a rule
+	Ignore MatchingRules `yaml:",omitempty"`
+	// Only allows to specify rule to only autodiscover manifest for a specific Helm based on a rule
+	Only MatchingRules `yaml:",omitempty"`
+}
+
+// Maven hold all information needed to generate helm manifest.
+type Maven struct {
+	// spec defines the settings provided via an updatecli manifest
+	spec Spec
+	// rootDir defines the root directory from where looking for Helm Chart
+	rootDir string
+}
+
+// New return a new valid Helm object.
+func New(spec interface{}, rootDir string) (Maven, error) {
+	var s Spec
+
+	err := mapstructure.Decode(spec, &s)
+	if err != nil {
+		return Maven{}, err
+	}
+
+	dir := rootDir
+	if len(s.RootDir) > 0 {
+		dir = s.RootDir
+	}
+
+	// If no RootDir have been provided via settings,
+	// then fallback to the current process path.
+	if len(dir) == 0 {
+		logrus.Errorln("no working directrory defined")
+		return Maven{}, err
+	}
+
+	return Maven{
+		spec:    s,
+		rootDir: dir,
+	}, nil
+
+}
+
+func (m Maven) DiscoverManifests(input discoveryConfig.Input) ([]config.Spec, error) {
+
+	logrus.Infof("\n\n%s\n", strings.ToTitle("Maven"))
+	logrus.Infof("%s\n", strings.Repeat("=", len("Maven")+1))
+
+	manifests, err := m.discoverDependenciesManifests()
+
+	if err != nil {
+		return nil, err
+	}
+
+	dependencyManagementManifests, err := m.discoverDependencyManagementsManifests()
+
+	if err != nil {
+		return nil, err
+	}
+
+	manifests = append(manifests, dependencyManagementManifests...)
+
+	parentPomdependencyManifests, err := m.discoverParentPomDependencyManifests()
+
+	if err != nil {
+		return nil, err
+	}
+
+	manifests = append(manifests, parentPomdependencyManifests...)
+
+	// Set scm configuration if specified
+	for i := range manifests {
+		// Set scm configuration if specified
+		if len(input.ScmID) > 0 {
+			SetScm(&manifests[i], *input.ScmSpec, input.ScmID)
+		}
+
+		// Set pullrequest configuration if specified
+		if len(input.PullrequestID) > 0 {
+			SetPullrequest(&manifests[i], *input.PullRequestSpec, input.PullrequestID)
+		}
+	}
+
+	return manifests, nil
+}
+
+func SetScm(configSpec *config.Spec, scmSpec scm.Config, scmID string) {
+	configSpec.SCMs = make(map[string]scm.Config)
+	configSpec.SCMs[scmID] = scmSpec
+
+	for id, condition := range configSpec.Conditions {
+		condition.SCMID = scmID
+		configSpec.Conditions[id] = condition
+	}
+
+	for id, target := range configSpec.Targets {
+		target.SCMID = scmID
+		configSpec.Targets[id] = target
+	}
+
+}
+
+func SetPullrequest(configSpec *config.Spec, pullrequestSpec pullrequest.Config, pullrequestID string) {
+	configSpec.PullRequests = make(map[string]pullrequest.Config)
+	configSpec.PullRequests[pullrequestID] = pullrequestSpec
+}
+
+// RunDisabled returns a bool saying if a run should be done
+func (m Maven) Enabled() bool {
+	return !m.spec.Disable
+}
