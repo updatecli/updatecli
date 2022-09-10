c801eae13d8cc7fcacc2991a557e02eaa5a32082
Olblak <me@olblak.com>
2022-09-10 08:55:46 +0200

    Improve xml utils readability
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/xml/utils.go
+++ pkg/plugins/resources/xml/utils.go
@@ -7,12 +7,12 @@ import (
 
 // joinPathwithworkingDirectoryPath To merge File path with current working dire, unless file is an http url
 func joinPathWithWorkingDirectoryPath(fileName, workingDir string) string {
-	if workingDir != "" &&
-		!filepath.IsAbs(fileName) &&
-		!strings.HasPrefix(fileName, "https://") &&
-		!strings.HasPrefix(fileName, "http://") {
-		fileName = filepath.Join(workingDir, fileName)
+	if workingDir == "" ||
+		filepath.IsAbs(fileName) ||
+		strings.HasPrefix(fileName, "https://") ||
+		strings.HasPrefix(fileName, "http://") {
+		return fileName
 	}
 
-	return fileName
+	return filepath.Join(workingDir, fileName)
 }
