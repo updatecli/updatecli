e381c76548e61e8b25cb0b574c4ee9ea76d47883
Olivier Vernin <olivier@vernin.me>
2022-09-11 17:46:50 +0200

    e2e test should be executed from repository root directory (#860)
    * Always use the latest Go version available for the go.mod
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * e2e shouldn't retrieve information named ERROR
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * correctly set e2e test rootdir
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix test_skipped_targets script name
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * facepalm
    
    Signed-off-by: Olblak <me@olblak.com>
    
    Signed-off-by: Olblak <me@olblak.com>

---
+++ e2e/scripts/test_warning.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eux
+
+: "${VENOM_VAR_binpath:? Please set VENOM_VAR_binpath to updatecli binary dirname}"
+: "${VENOM_VAR_rootpath:=../..}"
+
+## In order for the Updatecli manifest to work, we must be at the root of the git repository 
+pushd "$VENOM_VAR_rootpath"
+
+"$VENOM_VAR_binpath/updatecli" diff --config  e2e/updatecli.d/warning.d/
