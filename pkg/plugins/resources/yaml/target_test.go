425459746bd71ac7c8f42c581d7addbf55245308
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-12 11:39:05 +0200

    chore: add missing Test_TargetFromSCM test

--- pkg/plugins/resources/yaml/target_test.go
+++ pkg/plugins/resources/yaml/target_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
 	"github.com/updatecli/updatecli/pkg/core/text"
 )
 
@@ -242,4 +243,124 @@ github:
 	}
 }
 
-// TODO: Test_TargetFromSCM
+func Test_TargetFromSCM(t *testing.T) {
+	tests := []struct {
+		name             string
+		spec             Spec
+		files            map[string]string
+		scm              scm.ScmHandler
+		inputSourceValue string
+		mockedContents   map[string]string
+		mockedError      error
+		wantedFiles      []string
+		wantedContents   map[string]string
+		wantedResult     bool
+		wantedError      bool
+		dryRun           bool
+	}{
+		{
+			name: "Passing case with both input source and specified value (specified value should be used)",
+			spec: Spec{
+				File:  "test.yaml",
+				Key:   "github.owner",
+				Value: "obiwankenobi",
+			},
+			files: map[string]string{
+				"/tmp/test.yaml": "",
+			},
+			inputSourceValue: "olblak",
+			mockedContents: map[string]string{
+				"/tmp/test.yaml": `---
+github:
+  owner: olblak
+  repository: charts
+`,
+			},
+			wantedFiles: []string{
+				"/tmp/test.yaml",
+			},
+			// Note: the re-encoded file doesn't contain any separator anymore
+			// TODO: fix indentation
+			wantedContents: map[string]string{
+				"/tmp/test.yaml": `github:
+    owner: obiwankenobi
+    repository: charts
+`,
+			},
+			wantedResult: true,
+		},
+		{
+			name: "Passing case with 'Files', both input source and specified value (specified value should be used)",
+			spec: Spec{
+				Files: []string{
+					"test.yaml",
+					"bar.yaml",
+				},
+				Key:   "github.owner",
+				Value: "obiwankenobi",
+			},
+			files: map[string]string{
+				"/tmp/test.yaml": "",
+				"/tmp/bar.yaml":  "",
+			},
+			inputSourceValue: "olblak",
+			mockedContents: map[string]string{
+				"/tmp/test.yaml": `---
+github:
+  owner: olblak
+  repository: charts
+`,
+				"/tmp/bar.yaml": `---
+github:
+  owner: asterix
+  repository: charts
+`,
+			},
+			// returned files are sorted
+			wantedFiles: []string{
+				"/tmp/bar.yaml",
+				"/tmp/test.yaml",
+			},
+			// Note: the updated files don't contain separator anymore
+			// TODO: fix indentation
+			wantedContents: map[string]string{
+				"/tmp/test.yaml": `github:
+    owner: obiwankenobi
+    repository: charts
+`,
+				"/tmp/bar.yaml": `github:
+    owner: obiwankenobi
+    repository: charts
+`,
+			},
+			wantedResult: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockedText := text.MockTextRetriever{
+				Contents: tt.mockedContents,
+				Err:      tt.mockedError,
+			}
+			y := &Yaml{
+				spec:             tt.spec,
+				contentRetriever: &mockedText,
+				files:            tt.files,
+			}
+			gotResult, gotFiles, _, gotErr := y.TargetFromSCM(tt.inputSourceValue, tt.scm, tt.dryRun)
+			if tt.wantedError {
+				assert.Error(t, gotErr)
+				return
+			}
+
+			require.NoError(t, gotErr)
+			assert.Equal(t, tt.wantedResult, gotResult)
+			assert.Equal(t, tt.wantedFiles, gotFiles)
+
+			for filePath := range y.files {
+				defer os.Remove(filePath)
+				assert.Equal(t, tt.wantedContents[filePath], mockedText.Contents[filePath])
+			}
+		})
+	}
+}
