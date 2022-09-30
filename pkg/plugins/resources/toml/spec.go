5bf7ba61167a7d99a90705745f8cdb63e6245daa
Olblak <me@olblak.com>
2022-09-30 22:08:13 +0200

    Add files support to toml
    Refactor toml to use dasel package
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/toml/spec.go
+++ pkg/plugins/resources/toml/spec.go
@@ -1,10 +1,16 @@
 package toml
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+)
 
 type Spec struct {
 	// [s][c][t] File specifies the toml file to manipulate
 	File string `yaml:",omitempty"`
+	// [s][c][t] Files specifies a list of Json file to manipuate
+	Files []string `yaml:",omitempty"`
 	// [s][c][t] Key specifies the query to retrieve an information from a toml file
 	Key string `yaml:",omitempty"`
 	// [s][c][t] Value specifies the value for a specific key. Default to source output
@@ -18,14 +24,33 @@ var (
 	ErrSpecFileUndefined = errors.New("toml file not specified")
 	// ErrSpecKeyUndefined is returned if a key wasn't specified
 	ErrSpecKeyUndefined = errors.New("toml key undefined")
+	// ErrSpecFileAndFilesDefines when we both spec File and Files have been specified
+	ErrSpecFileAndFilesDefined = errors.New("parameter \"file\" and \"files\" are mutually exclusive")
+	// ErrWrongSpec is returned when the Spec has wrong content
+	ErrWrongSpec error = errors.New("wrong spec content")
 )
 
-func (s *Spec) Validate() (errs []error) {
-	if len(s.File) == 0 {
+func (s *Spec) Validate() error {
+	var errs []error
+
+	if len(s.File) == 0 && len(s.Files) == 0 {
 		errs = append(errs, ErrSpecFileUndefined)
 	}
 	if len(s.Key) == 0 {
 		errs = append(errs, ErrSpecKeyUndefined)
 	}
-	return errs
+
+	if len(s.File) > 0 && len(s.Files) > 0 {
+		errs = append(errs, ErrSpecFileAndFilesDefined)
+	}
+
+	for _, e := range errs {
+		logrus.Errorln(e)
+	}
+
+	if len(errs) > 0 {
+		return ErrWrongSpec
+	}
+
+	return nil
 }
