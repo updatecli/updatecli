bbe3cd1bb2e773ee364b371c42cd37465354995b
Olblak <me@olblak.com>
2022-09-30 21:27:27 +0200

    Refactor json package
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/json/spec.go
+++ pkg/plugins/resources/json/spec.go
@@ -1,10 +1,16 @@
 package json
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+)
 
 type Spec struct {
 	// [s][c][t] File specifies the Json file to manipuate
 	File string `yaml:",omitempty"`
+	// [s][c][t] Files specifies a list of Json file to manipuate
+	Files []string `yaml:",omitempty"`
 	// [s][c][t] Key specifies the Jsonpath key to manipuate
 	Key string `yaml:",omitempty"`
 	// [s][c][t] Value specifies the Jsonpath key to manipuate. Default to source output
@@ -14,16 +20,34 @@ type Spec struct {
 }
 
 var (
-	ErrSpecFileUndefined = errors.New("json file not specified")
-	ErrSpecKeyUndefined  = errors.New("json key undefined")
+	ErrSpecFileUndefined       = errors.New("json file not specified")
+	ErrSpecKeyUndefined        = errors.New("json key undefined")
+	ErrSpecFileAndFilesDefined = errors.New("parameter \"file\" and \"files\" are mutually exclusive")
+	// ErrWrongSpec is returned when the Spec has wrong content
+	ErrWrongSpec error = errors.New("wrong spec content")
 )
 
-func (s *Spec) Validate() (errs []error) {
-	if len(s.File) == 0 {
+func (s *Spec) Validate() error {
+	var errs []error
+
+	if len(s.File) == 0 && len(s.Files) == 0 {
 		errs = append(errs, ErrSpecFileUndefined)
 	}
 	if len(s.Key) == 0 {
 		errs = append(errs, ErrSpecKeyUndefined)
 	}
-	return errs
+
+	if len(s.File) > 0 && len(s.Files) > 0 {
+		errs = append(errs, ErrSpecFileAndFilesDefined)
+	}
+
+	for _, e := range errs {
+		logrus.Errorln(e)
+	}
+
+	if len(errs) > 0 {
+		return ErrWrongSpec
+	}
+
+	return nil
 }
