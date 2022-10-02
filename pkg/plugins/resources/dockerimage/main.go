2daebff6c2dc7f15afe023982ef101c6b3434287
Sambhav Kothari <sambhavs.email@gmail.com>
2022-10-01 23:18:49 -0400

    Improve docker image/digest support
    Signed-off-by: Sambhav Kothari <sambhavs.email@gmail.com>

--- pkg/plugins/resources/dockerimage/main.go
+++ pkg/plugins/resources/dockerimage/main.go
@@ -1,12 +1,11 @@
 package dockerimage
 
 import (
-	"fmt"
-	"strings"
-
+	"github.com/google/go-containerregistry/pkg/authn"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/mitchellh/mapstructure"
-	"github.com/updatecli/updatecli/pkg/plugins/utils/docker/dockerimage"
-	"github.com/updatecli/updatecli/pkg/plugins/utils/docker/dockerregistry"
+	"github.com/updatecli/updatecli/pkg/plugins/utils/docker"
 	"github.com/updatecli/updatecli/pkg/plugins/utils/version"
 )
 
@@ -18,22 +17,16 @@ type Spec struct {
 	// [S][C][T] Image specifies the container image such as `updatecli/updatecli`
 	Image string `yaml:",omitempty"`
 	// [C][T] Tag specifies the container image tag such as `latest`
-	Tag string `yaml:",omitempty"`
-	// [S][C][T] Username specifies the container registry username to use for authentication. Not compatible with token
-	Username string `yaml:",omitempty"`
-	// [S][C][T] Password specifies the container registry password to use for authentication. Not compatible with token
-	Password string `yaml:",omitempty"`
-	// [S][C][T] Token specifies the container registry token to use for authentication. Not compatible with username/password
-	Token string `yaml:",omitempty"`
+	Tag                   string `yaml:",omitempty"`
+	docker.InlineKeyChain `yaml:",inline" mapstructure:",squash"`
 	// [S] VersionFilter provides parameters to specify version pattern and its type like regex, semver, or just latest.
 	VersionFilter version.Filter `yaml:",omitempty"`
 }
 
 // DockerImage defines a resource of type "dockerimage"
 type DockerImage struct {
-	spec     Spec
-	registry dockerregistry.Registry
-	image    dockerimage.Image
+	spec    Spec
+	options []remote.Option
 	// versionFilter holds the "valid" version.filter, that might be different than the user-specified filter (Spec.VersionFilter)
 	versionFilter version.Filter
 	foundVersion  version.Version
@@ -50,63 +43,35 @@ func New(spec interface{}) (*DockerImage, error) {
 		return nil, err
 	}
 
-	newImage, err := dockerimage.New(
-		// concatenate user-provided image name (that may contain registry and namespace along with the namespace) with the user-provided tag
-		fmt.Sprintf("%s:%s", newSpec.Image, newSpec.Tag),
-		newSpec.Architecture,
-	)
-	if err != nil {
-		return nil, err
-	}
-
 	newFilter, err := newSpec.VersionFilter.Init()
 	if err != nil {
 		return nil, err
 	}
 
-	newRegistry := dockerregistry.New(
-		newImage.Registry,
-		newSpec.Username,
-		newSpec.Password,
-	)
-
 	newResource := &DockerImage{
 		spec:          newSpec,
-		registry:      newRegistry,
-		image:         *newImage,
 		versionFilter: newFilter,
 	}
 
-	err = newResource.Validate()
+	err = newSpec.InlineKeyChain.Validate()
 	if err != nil {
 		return nil, err
 	}
 
+	keychains := []authn.Keychain{}
+
+	if !newSpec.InlineKeyChain.Empty() {
+		keychains = append(keychains, newSpec.InlineKeyChain)
+	}
+
+	keychains = append(keychains, authn.DefaultKeychain)
+	arch := newSpec.Architecture
+	if arch == "" {
+		arch = "amd64"
+	}
+	newResource.options = append(newResource.options, remote.WithPlatform(v1.Platform{Architecture: arch, OS: "linux"}))
+	newResource.options = append(newResource.options, remote.WithAuthFromKeychain(authn.NewMultiKeychain(keychains...)))
 	return newResource, nil
-}
-
-// Validate validates the object and returns an error (with all the failed validation messages) if it is not valid
-func (di *DockerImage) Validate() error {
-	var validationErrors []string
-
-	if len(di.spec.Token) > 0 {
-		if len(di.spec.Username) > 0 && len(di.spec.Password) > 0 {
-			validationErrors = append(validationErrors, "Specifying a (bearer) token is invalid when a username and a password are provided.")
-		}
-	}
-
-	if len(di.spec.Username) > 0 && len(di.spec.Password) == 0 {
-		validationErrors = append(validationErrors, "Docker registry username provided but not the password")
-	} else if len(di.spec.Username) == 0 && len(di.spec.Password) > 0 {
-		validationErrors = append(validationErrors, "Docker registry password provided but not the username")
-	}
-
-	// Return all the validation errors if found any
-	if len(validationErrors) > 0 {
-		return fmt.Errorf("validation error: the provided manifest configuration had the following validation errors:\n%s", strings.Join(validationErrors, "\n\n"))
-	}
-
-	return nil
 }
 
 // Changelog returns the changelog for this resource, or an empty string if not supported
