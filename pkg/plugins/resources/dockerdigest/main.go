ec2f16eb6a2d73b9f1af49f0c05e8429353947c6
Olivier Vernin <olivier@vernin.me>
2022-10-12 18:37:30 +0200

    Add docker digest support for Updatecli condition (#924)
    * Docker image condition should default tag to source output
    
    Put back dockerimage condition message
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add docker digest condition
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add docker e2e tests
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Add return line at the end of the file
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * comment docker e2e test that require credentials
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * comment tests that require authentication
    
    Signed-off-by: Olblak <me@olblak.com>
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/dockerdigest/main.go
+++ pkg/plugins/resources/dockerdigest/main.go
@@ -10,12 +10,14 @@ import (
 
 // Spec defines a specification for a "dockerdigest" resource parsed from an updatecli manifest file
 type Spec struct {
-	// Architecture specifies the container image architecture such as `amd64`
+	// [s][c] Architecture specifies the container image architecture such as `amd64`
 	Architecture string `yaml:",omitempty"`
-	// Image specifies the container image such as `updatecli/updatecli`
+	// [s][c] Image specifies the container image such as `updatecli/updatecli`
 	Image string `yaml:",omitempty"`
-	// Tag specifies the container image tag such as `latest`
-	Tag                   string `yaml:",omitempty"`
+	// [s] Tag specifies the container image tag such as `latest`
+	Tag string `yaml:",omitempty"`
+	// [c] Digest specifies the container image digest such as `@sha256:ce782db15ab5491c6c6178da8431b3db66988ccd11512034946a9667846952a6`
+	Digest                string `yaml:",omitempty"`
 	docker.InlineKeyChain `yaml:",inline" mapstructure:",squash"`
 }
 
