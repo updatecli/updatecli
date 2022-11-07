7eee174eb196f60b15e22f11afc6940f8de518f7
Olivier Vernin <olivier@vernin.me>
2022-11-07 08:47:55 +0100

    Allow multiple query combined with versionfilter to toml source (#963)
    Signed-off-by: Olblak <me@olblak.com>
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/toml/spec.go
+++ pkg/plugins/resources/toml/spec.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/plugins/utils/version"
 )
 
 type Spec struct {
@@ -11,19 +12,23 @@ type Spec struct {
 	File string `yaml:",omitempty"`
 	// [c][t] Files specifies a list of Json file to manipuate
 	Files []string `yaml:",omitempty"`
+	// [s][c][t] Query allows to used advanced query. Override the parameter key
+	Query string `yaml:",omitempty"`
 	// [s][c][t] Key specifies the query to retrieve an information from a toml file
 	Key string `yaml:",omitempty"`
 	// [s][c][t] Value specifies the value for a specific key. Default to source output
 	Value string `yaml:",omitempty"`
-	// [c][t] Multiple allows to query multiple values at once
-	Multiple bool `yaml:",omitempty"`
+	// [c][t] *Deprecated* Please look at query parameter to achieve similar objective
+	Multiple bool `yaml:",omitempty" jsonschema:"-"`
+	// [s]VersionFilter provides parameters to specify version pattern and its type like regex, semver, or just latest.
+	VersionFilter version.Filter `yaml:",omitempty"`
 }
 
 var (
 	// ErrSpecFileUndefined is returned if a file wasn't specified
 	ErrSpecFileUndefined = errors.New("toml file undefined")
 	// ErrSpecKeyUndefined is returned if a key wasn't specified
-	ErrSpecKeyUndefined = errors.New("toml key undefined")
+	ErrSpecKeyUndefined = errors.New("toml key or query undefined")
 	// ErrSpecFileAndFilesDefines when we both spec File and Files have been specified
 	ErrSpecFileAndFilesDefined = errors.New("parameter \"file\" and \"files\" are mutually exclusive")
 	// ErrWrongSpec is returned when the Spec has wrong content
@@ -36,7 +41,7 @@ func (s *Spec) Validate() error {
 	if len(s.File) == 0 && len(s.Files) == 0 {
 		errs = append(errs, ErrSpecFileUndefined)
 	}
-	if len(s.Key) == 0 {
+	if len(s.Key) == 0 && len(s.Query) == 0 {
 		errs = append(errs, ErrSpecKeyUndefined)
 	}
 
