97164838f2eaf71276cb5b32f9a9dfe0abec6efa
Olblak <me@olblak.com>
2022-11-07 19:55:54 +0100

    Implement github release conditoin
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/githubrelease/condition.go
+++ pkg/plugins/resources/githubrelease/condition.go
@@ -3,13 +3,54 @@ package githubrelease
 import (
 	"fmt"
 
+	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
+	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
-func (ghr GitHubRelease) Condition(source string) (bool, error) {
-	return false, fmt.Errorf("Condition not supported for the plugin GitHub Release")
+func (gr GitHubRelease) Condition(source string) (bool, error) {
+	versions, err := gr.ghHandler.SearchReleases(gr.spec.Type)
+
+	if err != nil {
+		logrus.Error(err)
+		return false, err
+	}
+
+	if len(versions) == 0 {
+		switch gr.spec.DisableTagSearch {
+		case true:
+			return false, fmt.Errorf("no GitHub release found, exiting")
+		case false:
+			logrus.Infof("%s No GitHub Release found. As fallback Looking at published git tags", result.ATTENTION)
+			versions, err = gr.ghHandler.SearchTags()
+			if err != nil {
+				logrus.Errorf("%s", err)
+				return false, err
+			}
+			if len(versions) == 0 {
+				return false, fmt.Errorf("no GitHub release or git tags found, exiting")
+			}
+		}
+	}
+
+	gr.foundVersion, err = gr.versionFilter.Search(versions)
+	if err != nil {
+		return false, err
+	}
+
+	value := gr.foundVersion.GetVersion()
+
+	if len(value) == 0 {
+		logrus.Infof("%s No Github Release version found matching pattern %q", result.FAILURE, gr.versionFilter.Pattern)
+		return false, fmt.Errorf("no Github Release version found matching pattern %q", gr.versionFilter.Pattern)
+	} else if len(value) > 0 {
+		logrus.Infof("%s Github Release version %q found matching pattern %q", result.SUCCESS, value, gr.versionFilter.Pattern)
+		return true, nil
+	}
+
+	return false, fmt.Errorf("something unexpected happened in Github source")
 }
 
 func (ghr GitHubRelease) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error) {
-	return false, fmt.Errorf("Condition not supported for the plugin GitHub Release")
+	return false, fmt.Errorf("condition not supported for plugin GitHub Release used with scm")
 }
