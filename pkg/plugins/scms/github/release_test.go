e0739ade412e23641a2d42f260af53a1cbc16986
Olblak <me@olblak.com>
2022-11-19 17:26:32 +0100

    Add GitHub release test cases
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/scms/github/release_test.go
+++ pkg/plugins/scms/github/release_test.go
@@ -12,11 +12,200 @@ func TestSearchReleases(t *testing.T) {
 	tests := []struct {
 		name         string
 		spec         Spec
+		releaseType  ReleaseType
 		mockedQuery  *releasesQuery
 		mockedError  error
 		wantReleases []string
 		wantErr      bool
 	}{
+		{
+			name: "Case with a mix of releases (draft, prerelease, release) and default ReleaseType filter",
+			spec: Spec{
+				Owner:      "updatecli",
+				Repository: "updatecli",
+				Token:      "superSecretTokenForJoe",
+				Username:   "joe",
+			},
+			mockedQuery: &releasesQuery{
+				RateLimit: RateLimit{
+					Cost:      1,
+					Remaining: 5000,
+				},
+				Repository: struct {
+					Releases repositoryRelease `graphql:"releases(last: 100, before: $before, orderBy: $orderBy)"`
+				}{
+					Releases: repositoryRelease{
+						TotalCount: 4,
+						PageInfo: PageInfo{
+							HasNextPage: false,
+							EndCursor:   "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+						},
+						Edges: []releaseEdge{
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:    "v0.18.4 🌈",
+									TagName: "v0.18.4",
+									IsDraft: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:     "v0.18.3",
+									TagName:  "v0.18.3",
+									IsLatest: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMS0xMS0xM1QxMjozNzoxMiswMTowMM4DK66W",
+								Node: releaseNode{
+									Name:         "v1.0.0",
+									TagName:      "v1.0.0",
+									IsPrerelease: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+								Node: releaseNode{
+									Name:    "v0.0.1",
+									TagName: "v0.0.1",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantReleases: []string{"v0.0.1", "v0.18.3"},
+		},
+		{
+			name: "Case with a mix of releases (draft, prerelease, release) and a ReleaseType with draft, prerelease and releases",
+			spec: Spec{
+				Owner:      "updatecli",
+				Repository: "updatecli",
+				Token:      "superSecretTokenForJoe",
+				Username:   "joe",
+			},
+			releaseType: ReleaseType{
+				Draft:      true,
+				PreRelease: true,
+				Release:    true,
+			},
+			mockedQuery: &releasesQuery{
+				RateLimit: RateLimit{
+					Cost:      1,
+					Remaining: 5000,
+				},
+				Repository: struct {
+					Releases repositoryRelease `graphql:"releases(last: 100, before: $before, orderBy: $orderBy)"`
+				}{
+					Releases: repositoryRelease{
+						TotalCount: 4,
+						PageInfo: PageInfo{
+							HasNextPage: false,
+							EndCursor:   "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+						},
+						Edges: []releaseEdge{
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:    "v0.18.4 🌈",
+									TagName: "v0.18.4",
+									IsDraft: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:     "v0.18.3",
+									TagName:  "v0.18.3",
+									IsLatest: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMS0xMS0xM1QxMjozNzoxMiswMTowMM4DK66W",
+								Node: releaseNode{
+									Name:         "v1.0.0",
+									TagName:      "v1.0.0",
+									IsPrerelease: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+								Node: releaseNode{
+									Name:    "v0.0.1",
+									TagName: "v0.0.1",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantReleases: []string{"v0.0.1", "v1.0.0", "v0.18.3", "v0.18.4"},
+		},
+		{
+			name: "Case with a mix of releases (draft, prerelease, release) and a ReleaseType with 'Latest'",
+			spec: Spec{
+				Owner:      "updatecli",
+				Repository: "updatecli",
+				Token:      "superSecretTokenForJoe",
+				Username:   "joe",
+			},
+			releaseType: ReleaseType{
+				Latest: true,
+			},
+			mockedQuery: &releasesQuery{
+				RateLimit: RateLimit{
+					Cost:      1,
+					Remaining: 5000,
+				},
+				Repository: struct {
+					Releases repositoryRelease `graphql:"releases(last: 100, before: $before, orderBy: $orderBy)"`
+				}{
+					Releases: repositoryRelease{
+						TotalCount: 4,
+						PageInfo: PageInfo{
+							HasNextPage: false,
+							EndCursor:   "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+						},
+						Edges: []releaseEdge{
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:    "v0.18.4 🌈",
+									TagName: "v0.18.4",
+									IsDraft: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: releaseNode{
+									Name:     "v0.18.3",
+									TagName:  "v0.18.3",
+									IsLatest: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMS0xMS0xM1QxMjozNzoxMiswMTowMM4DK66W",
+								Node: releaseNode{
+									Name:         "v1.0.0",
+									TagName:      "v1.0.0",
+									IsPrerelease: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+								Node: releaseNode{
+									Name:    "v0.0.1",
+									TagName: "v0.0.1",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantReleases: []string{"v0.18.3"},
+		},
 		{
 			name: "Nominal case with 4 kubernetes tags",
 			spec: Spec{
@@ -100,7 +289,7 @@ func TestSearchReleases(t *testing.T) {
 					mockedErr:   tt.mockedError,
 				},
 			}
-			releaseType := ReleaseType{}
+			releaseType := tt.releaseType
 			releaseType.Init()
 
 			got, err := sut.SearchReleases(releaseType)
