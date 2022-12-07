25ddb717f77f54709cb27a79809cb37234ae57a5
Olivier Vernin <olivier@vernin.me>
2022-12-07 08:27:30 +0100

    Add Dockerfile Autodiscovery (#1003)
    * Add dockerfile autodiscovery
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Update pkg/plugins/autodiscovery/dockerfile/matchingRule.go
    
    * rename pullrequest to action
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Show warning if dockerfile instruction contains multiple variable
    
    Signed-off-by: Olblak <me@olblak.com>
    
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/autodiscovery/dockerfile/dockerfile.go
@@ -0,0 +1,183 @@
+package dockerfile
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/config"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/resource"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/source"
+	"github.com/updatecli/updatecli/pkg/core/pipeline/target"
+	"github.com/updatecli/updatecli/pkg/plugins/resources/dockerfile"
+	"github.com/updatecli/updatecli/pkg/plugins/resources/dockerimage"
+)
+
+var (
+	// DefaultFileMatch specifies accepted Helm chart metadata file name
+	DefaultFileMatch []string = []string{
+		"Dockerfile",
+		"Dockerfile.*",
+	}
+)
+
+func (h Dockerfile) discoverDockerfileManifests() ([]config.Spec, error) {
+
+	var manifests []config.Spec
+
+	foundDockerfiles, err := searchDockerfiles(
+		h.rootDir,
+		h.filematch)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, foundDockerfile := range foundDockerfiles {
+
+		relativeFoundDockerfile, err := filepath.Rel(h.rootDir, foundDockerfile)
+		if err != nil {
+			// Let try the next one if it fails
+			logrus.Errorln(err)
+			continue
+		}
+
+		dirname := filepath.Dir(relativeFoundDockerfile)
+		basename := filepath.Base(relativeFoundDockerfile)
+
+		instructions, err := parseDockerfile(foundDockerfile)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(instructions) == 0 {
+			continue
+		}
+
+		for _, instruction := range instructions {
+
+			// For the time being, it's not possible to retrieve a list of tag for a specific digest
+			// without a significant amount f api call. More information on following issue
+			// https://github.com/google/go-containerregistry/issues/1297
+			// until a better solution, we don't handle docker image digest
+			if strings.Contains(instruction.image, "@sha256") {
+				logrus.Debugf("Docker Digest is not supported at the moment for %q", instruction.image)
+				continue
+			}
+
+			imageArray := strings.Split(instruction.image, ":")
+
+			// Get container image name and tag
+			imageName := ""
+			imageTag := ""
+			switch len(imageArray) {
+			case 2:
+				imageName = imageArray[0]
+				imageTag = imageArray[1]
+			case 1:
+				imageName = imageArray[0]
+			}
+
+			manifestName := fmt.Sprintf("Bump Docker Image Tag for %q", imageName)
+
+			// Test if the ignore rule based on path is respected
+			if len(h.spec.Ignore) > 0 {
+				if h.spec.Ignore.isMatchingRule(
+					h.rootDir,
+					relativeFoundDockerfile,
+					instruction.image,
+					instruction.arch,
+				) {
+
+					logrus.Debugf("Ignoring Dockerfile %q from %q, as matching ignore rule(s)\n",
+						basename,
+						dirname)
+					continue
+				}
+			}
+
+			// Test if the only rule based on path is respected
+			if len(h.spec.Only) > 0 {
+				if !h.spec.Only.isMatchingRule(
+					h.rootDir,
+					relativeFoundDockerfile,
+					instruction.image,
+					instruction.arch) {
+
+					logrus.Debugf("Ignoring Dockerfile %q from %q, as not matching only rule(s)\n",
+						basename,
+						dirname)
+					continue
+				}
+			}
+
+			sourceSpec := dockerimage.NewDockerImageSpecFromImage(imageName, imageTag, h.spec.Auths)
+
+			if sourceSpec == nil {
+				logrus.Debugln("no source spec detected")
+				continue
+			}
+
+			if instruction.arch != "" {
+				sourceSpec.Architecture = instruction.arch
+			}
+
+			if err != nil {
+				logrus.Errorln(err)
+				continue
+
+			}
+
+			targetMatcher := ""
+			targetKeyword := instruction.name
+
+			// Depending on the instruction the matcher will be different
+			switch instruction.name {
+			case "FROM":
+				targetMatcher = imageName
+			case "ARG":
+				targetMatcher = instruction.value
+				targetMatcher = strings.TrimPrefix(targetMatcher, instruction.trimArgPrefix)
+				targetMatcher = strings.TrimSuffix(targetMatcher, instruction.trimArgSuffix)
+			}
+
+			manifest := config.Spec{
+				Name: manifestName,
+				Sources: map[string]source.Config{
+					imageName: {
+						ResourceConfig: resource.ResourceConfig{
+							Name: fmt.Sprintf("[%s] Get latest Docker Image Tag", imageName),
+							Kind: "dockerimage",
+							Spec: *sourceSpec,
+						},
+					},
+				},
+				Targets: map[string]target.Config{
+					imageName: {
+						SourceID: imageName,
+						ResourceConfig: resource.ResourceConfig{
+							Name: fmt.Sprintf("[%s] Bump Docker Image tag in %q",
+								imageName,
+								relativeFoundDockerfile),
+							Kind: "dockerfile",
+							Spec: dockerfile.Spec{
+								File: relativeFoundDockerfile,
+								Instruction: map[string]string{
+									"keyword": targetKeyword,
+									"matcher": targetMatcher,
+								},
+							},
+						},
+					},
+				},
+			}
+
+			// To Add transformers
+
+			manifests = append(manifests, manifest)
+		}
+	}
+
+	return manifests, nil
+}
---
+++ pkg/plugins/autodiscovery/dockerfile/utils.go
@@ -0,0 +1,280 @@
+package dockerfile
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+const (
+	// extractVariableRegex defines the regular expression used to extract Dockerfile ARGS from value
+	//extractVariableRegex string = `(.*)\$\{(.*)\}(.*)`
+	extractVariableRegex string = `\$\{([a-zA-Z.+-_]*)\}`
+
+	FromInstruction = "FROM"
+	ArgInstruction  = "ARG"
+)
+
+var (
+	// The compiled version of the regex created at init() is cached here so it
+	// only needs to be created once.
+	regexVariableName *regexp.Regexp
+)
+
+func init() {
+	regexVariableName = regexp.MustCompile(extractVariableRegex)
+}
+
+// instruction defines the struct holding instruction information used to craft the dockerfile manifest
+type instruction struct {
+	// name define the instruction name such as FROM or ARG
+	name string
+	// value define the main instruction value based on the type.
+	value string
+	// arch stores the arch information for a specific FROM. Fetch from the arch flag
+	arch string
+	// image stores the full image including tag needed to update either the FROM or ARG instruction
+	image string
+	// trimArgPrefix is only used when we need to update an ARG value based on a FROM information.
+	trimArgPrefix string
+	// trimArgSuffix is only used when we need to update an ARG value based on a FROM information.
+	trimArgSuffix string
+}
+
+// searchDockerfiles will look, recursively, for every files matching the default pattern "Dockerfile" from a root directory.
+func searchDockerfiles(rootDir string, files []string) ([]string, error) {
+
+	dockerfiles := []string{}
+
+	// To do switch to WalkDir which is more efficient, introduced in 1.16
+	err := filepath.Walk(rootDir, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		for _, f := range files {
+			match, err := filepath.Match(f, info.Name())
+			if err != nil {
+				logrus.Errorln(err)
+				continue
+			}
+			if match {
+				dockerfiles = append(dockerfiles, path)
+			}
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Debugf("%d potential Dockerfile(s) found", len(dockerfiles))
+
+	return dockerfiles, nil
+}
+
+// parseDockerfile reads a Dockerfile for information that could be automated such as ARG or FROM
+func parseDockerfile(filename string) ([]instruction, error) {
+
+	if _, err := os.Stat(filename); err != nil {
+		return nil, err
+	}
+
+	v, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer v.Close()
+
+	dockerfileContent, err := io.ReadAll(v)
+	if err != nil {
+		return nil, err
+	}
+
+	instructions, err := searchInstructions(dockerfileContent)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(instructions) == 0 {
+		return nil, nil
+	}
+
+	return instructions, nil
+}
+
+// Search for both a Dockerfile instruction required to define the update manifest.
+// While the dockerfile instruction is not case sensitive, its value is
+func searchInstructions(dockerfileContent []byte) ([]instruction, error) {
+	instructions := []instruction{}
+
+	data, err := parser.Parse(bytes.NewReader(dockerfileContent))
+	if err != nil {
+		return nil, err
+	}
+
+	args := map[string]string{}
+
+	i := 0
+	node := data.AST
+	for _, n := range node.Children {
+		switch n.Value {
+		case FromInstruction:
+			value := searchFromValue(n)
+			i++
+
+			// Parse Platform flag to extract a potential arch
+			platform := searchInstructionFlag("platform", n.Flags)
+			arch := ""
+			switch regexVariableName.Match([]byte(platform)) {
+			case true:
+				_, argName, _, err := extractArgName(platform)
+				if err != nil {
+					logrus.Warningln(err)
+					continue
+				}
+
+				if _, found := args[argName]; !found {
+					logrus.Debugf("No arg key %q found", argName)
+				} else {
+					arch = parsePlatform(strings.ReplaceAll(platform, "${"+argName+"}", args[argName]))
+				}
+			case false:
+				arch = parsePlatform(platform)
+			}
+
+			match := regexVariableName.Match([]byte(value))
+			switch match {
+			case true:
+				prefix, argName, suffix, err := extractArgName(value)
+
+				if err != nil {
+					logrus.Warningln(err)
+					continue
+				}
+
+				if _, found := args[argName]; found {
+					inst := instruction{
+						name:          ArgInstruction,
+						value:         argName,
+						arch:          arch,
+						image:         strings.ReplaceAll(value, "${"+argName+"}", args[argName]),
+						trimArgPrefix: prefix,
+						trimArgSuffix: suffix,
+					}
+					instructions = append(instructions, inst)
+					continue
+				}
+				logrus.Debugf("no arg key %q found", argName)
+
+			case false:
+				inst := instruction{
+					name:  FromInstruction,
+					value: value,
+					arch:  arch,
+					image: value,
+				}
+				instructions = append(instructions, inst)
+			}
+
+		// If we identify an ARG key/value then we store that information to use later
+		// with a FROM instruction to rebuild the docker image name + tag
+		// So we can be able to update the ARG instruction
+		case ArgInstruction:
+			lastArgs := searchArgsValue(n)
+			// Override old args if the same key is specified multiple time
+			for key, value := range lastArgs {
+				args[key] = value
+			}
+		}
+		i++
+	}
+	return instructions, err
+}
+
+func searchInstructionFlag(flagName string, flags []string) string {
+	instructionPrefix := "--" + flagName + "="
+	for _, flag := range flags {
+		if strings.HasPrefix(flag, instructionPrefix) {
+			return strings.TrimPrefix(flag, instructionPrefix)
+		}
+	}
+	return ""
+}
+
+func searchFromValue(n *parser.Node) string {
+	for nod := n.Next; nod != nil; nod = nod.Next {
+		return nod.Value
+	}
+	return ""
+}
+
+func searchArgsValue(n *parser.Node) map[string]string {
+	args := map[string]string{}
+	for nod := n.Next; nod != nil; nod = nod.Next {
+
+		switch strings.Contains(nod.Value, "=") {
+		case true:
+			s := strings.Split(nod.Value, "=")
+			args[s[0]] = s[1]
+		case false:
+			args[nod.Value] = ""
+
+			if nod.Next != nil {
+				args[nod.Value] = nod.Next.Value
+				nod = nod.Next
+			}
+		}
+	}
+	return args
+}
+
+func extractArgName(value string) (prefix, argName, suffix string, err error) {
+	founds := regexVariableName.FindAllStringSubmatch(value, -1)
+
+	if len(founds) > 1 {
+		err = errors.New("more than one variable detected in the Dockerfile instruction. Updatecli do not support this at the moment")
+		return
+	}
+
+	for _, found := range founds {
+		startingIndex := strings.Index(value, "${"+found[1]+"}")
+		endingIndex := startingIndex + len("${"+found[1]+"}")
+
+		argName = found[1]
+
+		if startingIndex > 0 {
+			prefix = value[:startingIndex]
+		}
+
+		if len(value) > endingIndex {
+			suffix = value[endingIndex:]
+		}
+	}
+
+	return prefix, argName, suffix, nil
+}
+
+func parsePlatform(platform string) (arch string) {
+	p := strings.Split(platform, "/")
+	switch len(p) {
+	case 3:
+		arch = p[1]
+	case 2:
+		arch = p[1]
+	}
+
+	return arch
+}
