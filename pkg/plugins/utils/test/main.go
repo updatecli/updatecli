e949db4705984568af6e67269253deaab79e3556
Olblak <me@olblak.com>
2022-12-27 10:59:18 +0100

    Add testing function
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/utils/test/main.go
+++ pkg/plugins/utils/test/main.go
@@ -20,5 +20,21 @@ func AssertConfigSpecEqualByteArray(t *testing.T, spec *config.Spec, manifest st
 	err := yamlEncoder.Encode(spec)
 	require.NoError(t, err)
 
-	return assert.Equal(t, buf.String(), manifest)
+	return assert.Equal(t, buf.String(), yamlMarshalUnmarshal(t, manifest))
+}
+
+// yamlMarshalUmarshal is used to parse a manifest to ensure it's a valid yaml one.
+// yamlMarshalUmarshal is also used to trim single quotes from yaml values
+func yamlMarshalUnmarshal(t *testing.T, manifest string) string {
+
+	var spec config.Spec
+	yaml.Unmarshal([]byte(manifest), &spec)
+
+	buf := bytes.NewBufferString("")
+	yamlEncoder := yaml.NewEncoder(buf)
+	yamlEncoder.SetIndent(2)
+	err := yamlEncoder.Encode(spec)
+	require.NoError(t, err)
+
+	return buf.String()
 }
