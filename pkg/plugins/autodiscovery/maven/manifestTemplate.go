cf91560c2d0c393463a6abe6aec42faa47da7f66
Olblak <me@olblak.com>
2022-12-27 11:00:44 +0100

    Add single quotes to maven manifest
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/maven/manifestTemplate.go
+++ pkg/plugins/autodiscovery/maven/manifestTemplate.go
@@ -2,53 +2,53 @@ package maven
 
 var (
 	// manifestTemplate is the Go template used to generate Fleet manifests
-	manifestTemplate string = `name: {{ .ManifestName }}
+	manifestTemplate string = `name: '{{ .ManifestName }}'
 sources:
   {{ .SourceID }}:
-    name: {{ .SourceName }}
-    kind: {{ .SourceKind }}
+    name: '{{ .SourceName }}'
+    kind: '{{ .SourceKind }}'
     spec:
+      artifactid: '{{ .SourceArtifactID }}'
+      groupid: '{{ .SourceGroupID }}'
   {{- if .SourceRepositories }}
       repositories:
   {{- range $repo := .SourceRepositories }}
-        - {{ $repo }}
+        - '{{ $repo }}'
   {{- end }}
   {{- end }}
-      groupid: {{ .SourceGroupID }}
-      artifactid: {{ .SourceArtifactID }}
 conditions:
   {{ .ConditionArtifactID }}:
-    name: {{ .ConditionArtifactIDName }}
-    kind: xml
+    name: '{{ .ConditionArtifactIDName }}'
+    kind: 'xml'
 {{- if .ScmID }}
-    scmid: {{ .ScmID }}
+    scmid: '{{ .ScmID }}'
 {{ end }}
     spec:
-      file: {{ .File }}
-      path: {{ .ConditionArtifactIDPath }}
-      value: {{ .ConditionArtifactIDValue }}
+      file: '{{ .File }}'
+      path: '{{ .ConditionArtifactIDPath }}'
+      value: '{{ .ConditionArtifactIDValue }}'
     disablesourceinput: true
   {{ .ConditionGroupID }}:
-    name: {{ .ConditionGroupIDName }}
-    kind: xml
+    name: '{{ .ConditionGroupIDName }}'
+    kind: 'xml'
 {{- if .ScmID }}
-    scmid: {{ .ScmID }}
+    scmid: '{{ .ScmID }}'
 {{ end }}
     spec:
-      file: {{ .File }}
-      path: {{ .ConditionGroupIDPath }}
-      value: {{ .ConditionGroupIDValue }}
+      file: '{{ .File }}'
+      path: '{{ .ConditionGroupIDPath }}'
+      value: '{{ .ConditionGroupIDValue }}'
     disablesourceinput: true
 targets:
   {{ .TargetID }}:
-    name: {{ .TargetName }}
-    kind: xml
+    name: '{{ .TargetName }}'
+    kind: 'xml'
 {{- if .ScmID }}
-    scmid: {{ .ScmID }}
+    scmid: '{{ .ScmID }}'
 {{ end }}
     spec:
-      file: {{ .File }}
-      path: {{ .TargetXMLPath }}
-    sourceid: {{ .SourceID }}
+      file: '{{ .File }}'
+      path: '{{ .TargetXMLPath }}'
+    sourceid: '{{ .SourceID }}'
 `
 )
