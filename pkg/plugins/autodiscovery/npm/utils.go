b63923a8a2a15592f2fe938b28b774883cb214f2
Olblak <me@olblak.com>
2023-01-02 21:07:55 +0100

    Add function to check package version
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/npm/utils.go
+++ pkg/plugins/autodiscovery/npm/utils.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
+	"github.com/sirupsen/logrus"
 )
 
 const (
@@ -40,4 +44,71 @@ func searchPackageJsonFiles(rootDir string) ([]string, error) {
 	}
 
 	return foundFiles, nil
+}
+
+// isVersionConstraintSpecified checks if the version is a version constraint that should be handle by npm
+// and not updatecli
+// https://docs.npmjs.com/cli/v6/configuring-npm/package-json#dependencies
+func isVersionConstraintSpecified(packageName, packageVersion string, strictSemver bool) bool {
+	// version set to an empty string is equivalent to *
+
+	if packageVersion == "" || packageVersion == "latest" || packageVersion == "*" {
+		logrus.Debugf("Ignoring dependency %q. It contains a version constraint %q handled by NPM", packageName, packageVersion)
+		logrus.Debugln("You probably want to adopt a better versioning strategy")
+		return true
+	}
+
+	// Check version start with
+	for _, toIgnorePrefix := range []string{">", "<", "~", "^", "*"} {
+		if strings.HasPrefix(packageVersion, toIgnorePrefix) {
+			logrus.Debugf("Ignoring dependency %q. It contains a version constraint %q handled by NPM", packageName, packageVersion)
+			return true
+		}
+	}
+
+	// Ignore http urls paths dependencies
+	// https://docs.npmjs.com/cli/v6/configuring-npm/package-json#git-urls-as-dependencies
+	for _, uriPrefix := range []string{"https://", "http://", "ftp://", "file://"} {
+		if strings.HasPrefix(packageVersion, uriPrefix) {
+			logrus.Debugf("Ignoring dependency %q. Updating URL is not supported at this time. Feel free to reach out to suggest an update scenario", packageName)
+			return true
+		}
+	}
+
+	// Ignore git urls paths dependencies
+	// https://docs.npmjs.com/cli/v6/configuring-npm/package-json#git-urls-as-dependencies
+	for _, gitPrefix := range []string{"git://", "git+ssh://", "git+https://", "git+file://"} {
+		if strings.HasPrefix(packageVersion, gitPrefix) {
+			logrus.Debugf("Ignoring dependency %q. Updating Git URL is not supported at this time. Feel free to reach out to suggest an update scenario", packageName)
+			return true
+		}
+	}
+
+	// Ignore local paths dependencies
+	// https://docs.npmjs.com/cli/v6/configuring-npm/package-json#local-paths
+	for _, localPath := range []string{"../", "./", "~/", "/"} {
+		if strings.HasPrefix(packageVersion, localPath) {
+			logrus.Debugf("Ignoreing dependency %q. Updating local path is not supported at this time. Feel free to reach out to suggest an update scenario", packageName)
+			return true
+		}
+	}
+
+	switch strictSemver {
+	case true:
+		_, err := semver.StrictNewVersion(packageVersion)
+		if err != nil {
+			logrus.Debugln(err)
+			logrus.Debugf("NPM package version %s detected. Updatecli only update version following strict semantic versioning", packageVersion)
+			return true
+		}
+	case false:
+		_, err := semver.NewVersion(packageVersion)
+		if err != nil {
+			logrus.Debugln(err)
+			logrus.Debugf("NPM package version %s detected. Updatecli only update version following semantic versioning", packageVersion)
+			return true
+		}
+	}
+
+	return false
 }
--- pkg/plugins/autodiscovery/npm/utils_test.go
+++ pkg/plugins/autodiscovery/npm/utils_test.go
@@ -32,3 +32,38 @@ func TestSearchPackageJsonFiles(t *testing.T) {
 		})
 	}
 }
+
+func TestIsVersionConstraintSpecified(t *testing.T) {
+
+	dataset := []struct {
+		version        string
+		strictSemver   bool
+		expectedResult bool
+	}{
+		{expectedResult: false, version: "1.0.0"},
+		{expectedResult: false, version: "1.0.0-alpha"},
+		{expectedResult: false, version: "1.0.0+alpha"},
+		{expectedResult: true, version: "1.0.0_alpha"},
+		{expectedResult: false, version: "1.0"},
+		{expectedResult: true, version: "1.0", strictSemver: true},
+		{expectedResult: false, version: "1"},
+		{expectedResult: true, version: "~1.0"},
+		{expectedResult: true, version: "1.x"},
+		{expectedResult: true, version: ">1.0.0"},
+		{expectedResult: true, version: ">=1.0.0"},
+		{expectedResult: true, version: "<1.0.0"},
+		{expectedResult: true, version: "<=1.0.0"},
+		{expectedResult: true, version: "<=1.0.0"},
+		{expectedResult: true, version: "file://../dyl"},
+		{expectedResult: true, version: "<1.0.0 || >= 2.3.1 < 2.4.5 || >=2.5.2 < 3.0.0"},
+		{expectedResult: true, version: "latest"},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.version, func(t *testing.T) {
+			gotResult := isVersionConstraintSpecified("foo", d.version, d.strictSemver)
+			assert.Equal(t, gotResult, d.expectedResult)
+		})
+	}
+
+}
