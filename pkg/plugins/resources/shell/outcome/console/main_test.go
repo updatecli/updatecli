e49bebfc38ad02b1e95532a8ac6e8e66413b4f4e
Olblak <me@olblak.com>
2023-01-05 23:51:01 +0100

    Add shell success criteria for handling the current workflow
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/resources/shell/outcome/console/main_test.go
@@ -0,0 +1,230 @@
+package console
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSourceResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		exitCode                   int
+		stdout                     string
+		expectedResultOutput       string
+		expectedResultErrorMessage error
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name:                 "Test succeeded without command output",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: "",
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             0,
+			stdout:               "1.2.3",
+			expectedResultOutput: "1.2.3",
+		},
+		{
+			name:                       "Test failed with no command output",
+			exitCode:                   1,
+			stdout:                     "",
+			expectedResultOutput:       "",
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+		},
+		{
+			name:                       "Test failed with command output",
+			exitCode:                   2,
+			stdout:                     "1.2.3",
+			expectedResultOutput:       "",
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 2"),
+			expectedError:              true,
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(&d.exitCode, &d.stdout)
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+				return
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotSourceResult, gotErr := c.SourceResult()
+
+			assert.Equal(t, gotSourceResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedResultErrorMessage)
+			}
+
+		})
+	}
+}
+
+func TestConditionResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		exitCode                   int
+		stdout                     string
+		expectedResultOutput       bool
+		expectedResultErrorMessage error
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name:                 "Test succeeded with no command output",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: true,
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             0,
+			stdout:               "1.2.3",
+			expectedResultOutput: true,
+		},
+		{
+			name:                       "Test failed with no command output",
+			exitCode:                   1,
+			stdout:                     "",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+		},
+		{
+			name:                       "Test failed with command output",
+			exitCode:                   2,
+			stdout:                     "1.2.3",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 2"),
+			expectedError:              true,
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(&d.exitCode, &d.stdout)
+
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+				return
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotConditionResult, gotErr := c.ConditionResult()
+
+			assert.Equal(t, gotConditionResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedResultErrorMessage)
+			case false:
+				assert.NoError(t, gotErr)
+			}
+		})
+	}
+}
+
+func TestTargetResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		exitCode                   int
+		stdout                     string
+		expectedResultOutput       bool
+		expectedResultErrorMessage error
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name:                 "Test succeeded with no command output",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: false,
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             0,
+			stdout:               "1.2.3",
+			expectedResultOutput: true,
+		},
+		{
+			name:                       "Test failed with no command output",
+			exitCode:                   1,
+			stdout:                     "",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+		},
+		{
+			name:                       "Test failed with command output",
+			exitCode:                   2,
+			stdout:                     "1.2.3",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 2"),
+			expectedError:              true,
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(&d.exitCode, &d.stdout)
+
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotTargetResult, gotErr := c.TargetResult()
+
+			assert.Equal(t, gotTargetResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedResultErrorMessage)
+			case false:
+				assert.NoError(t, gotErr)
+			}
+		})
+	}
+}
+
+func TestPreCommand(t *testing.T) {
+	exitCode := 3
+	stdout := "1.2.3"
+
+	c, gotErr := New(&exitCode, &stdout)
+	assert.NoError(t, gotErr)
+
+	if c.PreCommand() != nil {
+		t.Fail()
+	}
+}
+
+func TestPostCommand(t *testing.T) {
+	exitCode := 3
+	stdout := "1.2.3"
+
+	c, gotErr := New(&exitCode, &stdout)
+	assert.NoError(t, gotErr)
+
+	if c.PostCommand() != nil {
+		t.Fail()
+	}
+}
