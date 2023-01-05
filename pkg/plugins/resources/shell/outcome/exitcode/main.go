ed1acdd79c42bb8d977ae98ac90dead438eea73d
Olblak <me@olblak.com>
2023-01-05 23:51:33 +0100

    Add shell resource success criteria based on exitcode
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/resources/shell/outcome/exitcode/main.go
@@ -0,0 +1,115 @@
+package exitcode
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/sirupsen/logrus"
+)
+
+type Spec struct {
+	// Warning defines the command exit code used by Updatecli to identify a change need. Default to 2 if no exitcode have been specified
+	Warning int `yaml:",omitempty" jsonschema:"required"`
+	// Success defines the command exit code used by Updatecli to identify no changes are needed. Default to 0 if no exitcode have been specified
+	Success int `yaml:",omitempty" jsonschema:"required"`
+	// Failure defines the command exit code used by Updatecli to identify that something went wrong. Default to 1 if no exitcode have been specified
+	Failure int `yaml:",omitempty" jsonschema:"required"`
+}
+
+type ExitCode struct {
+	output   *string
+	exitCode *int
+	spec     Spec
+}
+
+func New(spec interface{}, exitCode *int, output *string) (*ExitCode, error) {
+	var s Spec
+	err := mapstructure.Decode(spec, &s)
+	if err != nil {
+		return nil, err
+	}
+
+	// Updatecli assumes that if all exitcode have the default value set to 0
+	// Then it means that no user input were provided and we fallback to default values
+	// Where
+	// success is 0
+	// failure is 1
+	// warning is 2
+	if s.Failure == 0 && s.Warning == 0 && s.Success == 0 {
+		s.Success = 0
+		s.Failure = 1
+		s.Warning = 2
+	}
+	err = s.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	if exitCode == nil {
+		return nil, errors.New("exitCode pointer is not set")
+	}
+	return &ExitCode{
+		exitCode: exitCode,
+		output:   output,
+		spec:     s,
+	}, nil
+}
+
+func (s Spec) Validate() error {
+	var errs []error
+	if s.Failure == s.Success {
+		errs = append(errs, fmt.Errorf("exit code can't be the same for success and failure - %d", s.Failure))
+	}
+	if len(errs) > 0 {
+		for i := range errs {
+			logrus.Errorln(errs[i])
+		}
+		return fmt.Errorf("wrong exit spec")
+	}
+	return nil
+}
+
+// PreCommand defines operations needed to be executed before the shell commmand
+func (e *ExitCode) PreCommand() error {
+	return nil
+}
+
+// PostCommand defines operations needed to be executed after the shell command
+func (e *ExitCode) PostCommand() error {
+	return nil
+}
+
+// SourceOutcome defines the success criteria for a source using the shell resource
+func (e *ExitCode) SourceResult() (string, error) {
+	switch *e.exitCode {
+	case e.spec.Success:
+		return *e.output, nil
+	default:
+		return "", fmt.Errorf("shell command failed. Expected exit code %d but got %d", e.spec.Success, *e.exitCode)
+	}
+}
+
+// ConditionResult defines the success criteria for a condition using the shell resource
+func (e *ExitCode) ConditionResult() (bool, error) {
+	switch *e.exitCode {
+	case e.spec.Success:
+		return true, nil
+	default:
+		return false, fmt.Errorf("shell command failed. Expected exit code %d but got %d", e.spec.Success, *e.exitCode)
+	}
+}
+
+// TargetResult defines the success criteria for a target using the shell resource
+func (e *ExitCode) TargetResult() (bool, error) {
+	switch *e.exitCode {
+	case e.spec.Success:
+		return true, nil
+	case e.spec.Warning:
+		return false, nil
+	case e.spec.Failure:
+		return false, fmt.Errorf("shell command failed. Expected exit code %d but got %d", e.spec.Success, *e.exitCode)
+	default:
+		return false, fmt.Errorf("shell command failed. Expected exit code %d to succeed but got %d", e.spec.Success, *e.exitCode)
+	}
+}
---
+++ pkg/plugins/resources/shell/outcome/exitcode/main_test.go
@@ -0,0 +1,363 @@
+package exitcode
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSourceResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		exitCode                   int
+		spec                       Spec
+		stdout                     string
+		expectedResultOutput       string
+		expectedResultErrorMessage string
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name: "Test succeeded without command output",
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: "",
+		},
+		{
+			name:                 "Test succeeded without specifying spec",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: "",
+		},
+		{
+			name:                 "Test failed without specifying spec",
+			exitCode:             2,
+			stdout:               "",
+			expectedResultOutput: "",
+		},
+		{
+			name: "Test succeeded without command output",
+			spec: Spec{
+				Warning: 1,
+				Success: 2,
+				Failure: 1,
+			},
+			exitCode:             2,
+			stdout:               "",
+			expectedResultOutput: "",
+		},
+		{
+			name:     "Test succeeded with command output",
+			exitCode: 0,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+			stdout:               "1.2.3",
+			expectedResultOutput: "1.2.3",
+		},
+		{
+			name:     "Test failed with no command output",
+			exitCode: 1,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+			stdout:                     "",
+			expectedResultOutput:       "",
+			expectedResultErrorMessage: "shell command failed. Expected exit code 0 but got 1",
+			expectedError:              true,
+		},
+		{
+			name:     "Test failed with command output",
+			exitCode: 2,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+			stdout:                     "1.2.3",
+			expectedResultOutput:       "",
+			expectedResultErrorMessage: "shell command failed. Expected exit code 0 but got 2",
+			expectedError:              true,
+		},
+		{
+			name:     "Test failed with wrong exitcode combination",
+			exitCode: 2,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 0,
+			},
+			stdout:                  "1.2.3",
+			expectedResultOutput:    "",
+			expectedError:           true,
+			expectedNewError:        true,
+			expectedNewErrorMessage: errors.New("wrong exit spec"),
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(d.spec, &d.exitCode, &d.stdout)
+
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+				return
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotSourceResult, gotErr := c.SourceResult()
+
+			assert.Equal(t, gotSourceResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr.Error(), d.expectedResultErrorMessage)
+			}
+		})
+	}
+}
+
+func TestConditionResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		exitCode                   int
+		spec                       Spec
+		stdout                     string
+		expectedResultOutput       bool
+		expectedResultErrorMessage error
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name:                 "Test succeeded with no command output",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             0,
+			stdout:               "1.2.3",
+			expectedResultOutput: true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             1,
+			stdout:               "1.2.3",
+			expectedResultOutput: true,
+			spec: Spec{
+				Warning: 2,
+				Success: 1,
+				Failure: 0,
+			},
+		},
+		{
+			name:                       "Test failed with no command output",
+			exitCode:                   1,
+			stdout:                     "",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                       "Test failed with command output",
+			exitCode:                   2,
+			stdout:                     "1.2.3",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 2"),
+			expectedError:              true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(d.spec, &d.exitCode, &d.stdout)
+
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+				return
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotConditionResult, gotErr := c.ConditionResult()
+
+			assert.Equal(t, gotConditionResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedResultErrorMessage)
+			case false:
+				assert.NoError(t, gotErr)
+			}
+		})
+	}
+}
+
+func TestTargetResult(t *testing.T) {
+
+	dataset := []struct {
+		name                       string
+		spec                       Spec
+		exitCode                   int
+		stdout                     string
+		expectedResultOutput       bool
+		expectedResultErrorMessage error
+		expectedError              bool
+		expectedNewError           bool
+		expectedNewErrorMessage    error
+	}{
+		{
+			name:                 "Test succeeded with no command output",
+			exitCode:             0,
+			stdout:               "",
+			expectedResultOutput: true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                 "Test succeeded with command output",
+			exitCode:             0,
+			stdout:               "1.2.3",
+			expectedResultOutput: true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                 "Triggered changed with command output",
+			exitCode:             2,
+			stdout:               "1.2.3",
+			expectedResultOutput: false,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                       "Test failed with no command output",
+			exitCode:                   1,
+			stdout:                     "",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+		{
+			name:                       "Test failed with command output",
+			exitCode:                   1,
+			stdout:                     "1.2.3",
+			expectedResultOutput:       false,
+			expectedResultErrorMessage: errors.New("shell command failed. Expected exit code 0 but got 1"),
+			expectedError:              true,
+			spec: Spec{
+				Warning: 2,
+				Success: 0,
+				Failure: 1,
+			},
+		},
+	}
+
+	for _, d := range dataset {
+		t.Run(d.name, func(t *testing.T) {
+			c, gotErr := New(d.spec, &d.exitCode, &d.stdout)
+
+			switch d.expectedNewError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedNewErrorMessage)
+				return
+			case false:
+				assert.NoError(t, gotErr)
+			}
+
+			gotTargetResult, gotErr := c.TargetResult()
+
+			assert.Equal(t, gotTargetResult, d.expectedResultOutput)
+			switch d.expectedError {
+			case true:
+				assert.Equal(t, gotErr, d.expectedResultErrorMessage)
+			case false:
+				assert.NoError(t, gotErr)
+			}
+		})
+	}
+}
+
+func TestPreCommand(t *testing.T) {
+	exitCode := 3
+	stdout := "1.2.3"
+	spec := Spec{
+		Warning: 2,
+		Success: 0,
+		Failure: 1,
+	}
+
+	c, gotErr := New(spec, &exitCode, &stdout)
+	assert.NoError(t, gotErr)
+
+	if c.PreCommand() != nil {
+		t.Fail()
+	}
+}
+
+func TestPostCommand(t *testing.T) {
+	exitCode := 3
+	stdout := "1.2.3"
+	spec := Spec{
+		Warning: 2,
+		Success: 0,
+		Failure: 1,
+	}
+
+	c, gotErr := New(spec, &exitCode, &stdout)
+	assert.NoError(t, gotErr)
+
+	if c.PostCommand() != nil {
+		t.Fail()
+	}
+}
