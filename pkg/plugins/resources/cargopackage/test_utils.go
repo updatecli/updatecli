fe4a0d50b2d0ac9ed7ff85a02e65df18e2803762
Loïs Postula <lois@postu.la>
2023-01-12 23:55:13 +0100

    [source][condition] Add Cargo Package support (#1081)
    * [enh] Implement new resource for Cargo Package
    
    * [enh] Remove git handling, rely on updatecli scm
    
    * [enh] Handle yanked crates
    
    * [fix] lint
    
    * [enh] Move validate to spec
    
    * [fix] Better error message
    
    * [enh] Add manifest for cargopackage
    
    * [enh] Use default crates repo if none are provided
    
    * [enh] Implement getting crate data from the index api
    
    * [fix] doc change
    
    * [fix] Switch to bool for scm
    
    * Apply suggestions from code review
    
    * Update pkg/plugins/resources/cargopackage/source.go
    
    Co-authored-by: Olivier Vernin <olivier@vernin.me>

---
+++ pkg/plugins/resources/cargopackage/test_utils.go
@@ -0,0 +1,50 @@
+package cargopackage
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func CreateDummyIndex() (string, error) {
+	dir, err := os.MkdirTemp("", "")
+	if err != nil {
+		return "", err
+	}
+	index, err := os.Create(filepath.Join(dir, "config.json"))
+	if err != nil {
+		return "", err
+	}
+	defer index.Close()
+	_, err = fmt.Fprintf(index, "{\"dl\":\"https://example.com\"}")
+	if err != nil {
+		return "", err
+	}
+	crateDir := filepath.Join(dir, "cr/at")
+	err = os.MkdirAll(crateDir, 0750)
+	if err != nil {
+		return "", err
+	}
+	crateFile, err := os.Create(filepath.Join(crateDir, "crate-test"))
+	if err != nil {
+		return "", err
+	}
+	defer crateFile.Close()
+	_, err = fmt.Fprintf(crateFile, "{\"name\":\"crate-test\",\"vers\":\"0.1.0\",\"deps\":[],\"features\":{},\"cksum\":\"b274d286f7a6aad5a7d5b5407e9db0098c94711fb3563bf2e32854a611edfb63\",\"yanked\":false,\"links\":null}\n")
+	if err != nil {
+		return "", err
+	}
+	_, err = fmt.Fprintf(crateFile, "{\"name\":\"crate-test\",\"vers\":\"0.2.0\",\"deps\":[],\"features\":{},\"cksum\":\"b274d286f7a6aad5a7d5b5407e9db0098c94711fb3563bf2e32854a611edfb63\",\"yanked\":false,\"links\":null}\n")
+	if err != nil {
+		return "", err
+	}
+	_, err = fmt.Fprintf(crateFile, "{\"name\":\"crate-test\",\"vers\":\"0.2.2\",\"deps\":[],\"features\":{},\"cksum\":\"b274d286f7a6aad5a7d5b5407e9db0098c94711fb3563bf2e32854a611edfb63\",\"yanked\":false,\"links\":null}\n")
+	if err != nil {
+		return "", err
+	}
+	_, err = fmt.Fprintf(crateFile, "{\"name\":\"crate-test\",\"vers\":\"0.2.3\",\"deps\":[],\"features\":{},\"cksum\":\"b274d286f7a6aad5a7d5b5407e9db0098c94711fb3563bf2e32854a611edfb63\",\"yanked\":true,\"links\":null}\n")
+	if err != nil {
+		return "", err
+	}
+	return dir, nil
+}
