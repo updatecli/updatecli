2762a7db74ff1cfcefa2f462075de78d00213c5e
Loïs Postula <lois@postu.la>
2023-02-02 19:35:52 +0100

    Add Cargo package autodiscovery (#1094)
    * [enh] Bootstrap support for autodiscovery of cargo package
    
    * [enh] support for package specifying dep in inline format
    
    * [enh] Switch to a registry definition
    
    * [enh] Add tests for private registry api with authorization
    
    * [enh] Implement cargo alternate registry support for cargo autodiscovery
    
    * [enh] Less radical error handling in test
    
    * [fix] linting
    
    * [fix] minor formatting issue
    
    * [enh] Deprecate field instead of removing it
    
    * [fix] typo in test expectation
    
    * [enh] Switch test repo to new updatecli-test organization
    
    * [enh] Validate registry config and add boilerplate for rust dep
    
    * [enh] Add mechanism to regenerate lockfile
    
    * [enh] Add workdir and shell changedif to generated cargo autodiscovery manifest
    
    * [enh] Add package version to pr
    
    * [enh] Switch to cargo update
    
    * Various fixes based on local testing
    
    * fix: Replace tab by space in generated yaml
    * fix: Switch back to `cargo generate-lockfile` as it updates the lock
      file without changing version constrains specified in Cargo.toml such
      as 1.0
    * fix: test with the new target name
    
    * Add another cargo e2e test
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * only update strict semantic version in Cargo.toml
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Update pkg/plugins/utils/cargo/main.go
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>
    Co-authored-by: Olivier Vernin <olivier@vernin.me>
    Co-authored-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/autodiscovery/cargo/dependencies.go
@@ -0,0 +1,238 @@
+package cargo
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"sort"
+	"text/template"
+
+	"github.com/updatecli/updatecli/pkg/plugins/utils/cargo"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	// ValidFiles specifies accepted Cargo files.
+	ValidFiles [1]string = [1]string{"Cargo.toml"}
+)
+
+type crateDependency struct {
+	Name     string
+	Registry string
+	Version  string
+	Inlined  bool
+}
+
+type crateMetadata struct {
+	Name            string
+	Dependencies    []crateDependency
+	DevDependencies []crateDependency
+}
+
+func (c Cargo) generateManifest(crateName string, dependency crateDependency, relativeFile string, foundFile string, dependencyType string, targetCargoCleanupEnabled bool) (bytes.Buffer, error) {
+	manifest := bytes.Buffer{}
+
+	// No need to continue if both Cargo.lock and Cargo.toml do not need to be updated
+	if !targetCargoCleanupEnabled && !isStrictSemver(dependency.Version) {
+		return manifest, nil
+	}
+
+	tmpl, err := template.New("manifest").Parse(dependencyManifest)
+	if err != nil {
+		logrus.Debugln(err)
+		return manifest, err
+	}
+	var existingSourceKey string
+	if dependency.Inlined {
+		existingSourceKey = fmt.Sprintf("%s.%s", dependencyType, dependency.Name)
+	} else {
+		existingSourceKey = fmt.Sprintf("%s.%s.version", dependencyType, dependency.Name)
+	}
+	var ConditionQuery string
+	if dependency.Inlined {
+		ConditionQuery = fmt.Sprintf("%s.(?:-=%s)", dependencyType, dependency.Name)
+	} else {
+		ConditionQuery = fmt.Sprintf("%s.(?:-=%s).version", dependencyType, dependency.Name)
+	}
+	var TargetKey string
+	if dependency.Inlined {
+		TargetKey = fmt.Sprintf("%s.%s", dependencyType, dependency.Name)
+	} else {
+		TargetKey = fmt.Sprintf("%s.%s.version", dependencyType, dependency.Name)
+	}
+	var Registry cargo.Registry
+	if dependency.Registry != "" {
+		if registry, found := c.spec.Registries[dependency.Registry]; found {
+			Registry = registry
+		}
+	}
+
+	sourceVersionFilterPattern := dependency.Version
+	if isStrictSemver(dependency.Version) {
+		sourceVersionFilterPattern = ">=" + sourceVersionFilterPattern
+	}
+
+	params := struct {
+		ManifestName               string
+		CrateName                  string
+		DependencyName             string
+		SourceID                   string
+		SourceName                 string
+		SourceVersionFilterKind    string
+		SourceVersionFilterPattern string
+		ExistingSourceID           string
+		ExistingSourceName         string
+		ExistingSourceKey          string
+		ConditionID                string
+		ConditionQuery             string
+		File                       string
+		TargetIDEnable             bool
+		TargetID                   string
+		TargetName                 string
+		TargetFile                 string
+		TargetKey                  string
+		TargetCargoCleanupEnabled  bool
+		TargetWorkdir              string
+		ScmID                      string
+		WithRegistry               bool
+		RegistrySCMID              string
+		RegistryRootDir            string
+		RegistryURL                string
+		RegistryAuthToken          string
+		RegistryHeaderFormat       string
+	}{
+		ManifestName:               fmt.Sprintf("Bump %s %q for %q crate", dependencyType, dependency.Name, crateName),
+		CrateName:                  crateName,
+		DependencyName:             dependency.Name,
+		SourceID:                   dependency.Name,
+		SourceName:                 fmt.Sprintf("Get latest %q crate version", dependency.Name),
+		SourceVersionFilterKind:    "semver",
+		SourceVersionFilterPattern: sourceVersionFilterPattern,
+		ExistingSourceID:           fmt.Sprintf("%s-current-version", dependency.Name),
+		ExistingSourceKey:          existingSourceKey,
+		ExistingSourceName:         fmt.Sprintf("Get current %q crate version", dependency.Name),
+		ConditionID:                dependency.Name,
+		ConditionQuery:             ConditionQuery,
+		File:                       relativeFile,
+		TargetIDEnable:             isStrictSemver(dependency.Version),
+		TargetID:                   dependency.Name,
+		TargetName:                 fmt.Sprintf("Bump crate dependency %q to {{ source %q }}", dependency.Name, dependency.Name),
+		TargetFile:                 filepath.Base(foundFile),
+		TargetKey:                  TargetKey,
+		TargetCargoCleanupEnabled:  targetCargoCleanupEnabled,
+		TargetWorkdir:              filepath.Dir(foundFile),
+		ScmID:                      c.scmID,
+		WithRegistry:               dependency.Registry != "",
+		RegistrySCMID:              Registry.SCMID,
+		RegistryRootDir:            Registry.RootDir,
+		RegistryURL:                Registry.URL,
+		RegistryAuthToken:          Registry.Auth.Token,
+		RegistryHeaderFormat:       Registry.Auth.HeaderFormat,
+	}
+
+	if err := tmpl.Execute(&manifest, params); err != nil {
+		logrus.Debugln(err)
+		return manifest, err
+	}
+	return manifest, nil
+}
+
+func (c Cargo) discoverCargoDependenciesManifests() ([][]byte, error) {
+	var manifests [][]byte
+
+	foundCargoFiles, err := findCargoFiles(
+		c.rootDir,
+		ValidFiles[:],
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, foundCargoFile := range foundCargoFiles {
+		logrus.Debugf("parsing file %q", foundCargoFile)
+
+		relativeFoundCargoFile, err := filepath.Rel(c.rootDir, foundCargoFile)
+		if err != nil {
+			// Jump to the next Cargo if current failed
+			logrus.Debugln(err)
+			continue
+		}
+
+		cargoTargetCleanManifestEnabled := false
+		if cargo.IsLockFileDetected(filepath.Join(filepath.Dir(foundCargoFile), "Cargo.lock")) {
+			switch cargo.IsCargoInstalled() {
+			case true:
+				cargoTargetCleanManifestEnabled = true
+			case false:
+				logrus.Warning("skipping, Cargo lock file detected but Updatecli couldn't detect the cargo command to update it in case of a Cargo.lock update")
+				continue
+			}
+		}
+
+		cargoRelativePath := filepath.Dir(relativeFoundCargoFile)
+		cargoCrateName := filepath.Base(cargoRelativePath)
+
+		// Test if the ignore rule based on path doesn't match
+		if len(c.spec.Ignore) > 0 && c.spec.Ignore.isMatchingIgnoreRule(c.rootDir, relativeFoundCargoFile) {
+			logrus.Debugf("Ignoring Cargo Crate %q from %q, as not matching rule(s)\n",
+				cargoCrateName,
+				cargoRelativePath)
+			continue
+		}
+
+		// Test if the only rule based on path match
+		if len(c.spec.Only) > 0 && !c.spec.Only.isMatchingOnlyRule(c.rootDir, relativeFoundCargoFile) {
+			logrus.Debugf("Ignoring Cargo Crate %q from %q, as not matching rule(s)\n",
+				cargoCrateName,
+				cargoRelativePath)
+			continue
+		}
+
+		// Retrieve Cargo dependencies for each crate
+		crate, err := getCrateMetadata(foundCargoFile)
+		if err != nil {
+			logrus.Debugln(err)
+			continue
+		}
+
+		if crate == nil {
+			continue
+		}
+
+		if len(crate.Dependencies) == 0 && len(crate.DevDependencies) == 0 {
+			continue
+		}
+
+		cr := *crate
+		dependencies := cr.Dependencies
+		sort.Slice(dependencies, func(i, j int) bool {
+			return dependencies[i].Name < dependencies[j].Name
+		})
+		devDependencies := cr.DevDependencies
+		sort.Slice(devDependencies, func(i, j int) bool {
+			return devDependencies[i].Name < devDependencies[j].Name
+		})
+
+		for _, dependency := range dependencies {
+			manifest, err := c.generateManifest(cr.Name, dependency, relativeFoundCargoFile, foundCargoFile, "dependencies", cargoTargetCleanManifestEnabled)
+			if err != nil {
+				logrus.Debugln(err)
+				continue
+			}
+			manifests = append(manifests, manifest.Bytes())
+		}
+		for _, dependency := range devDependencies {
+			manifest, err := c.generateManifest(cr.Name, dependency, relativeFoundCargoFile, foundCargoFile, "dev-dependencies", cargoTargetCleanManifestEnabled)
+			if err != nil {
+				logrus.Debugln(err)
+				continue
+			}
+			manifests = append(manifests, manifest.Bytes())
+		}
+	}
+
+	logrus.Debugf("found manifests: %s", manifests)
+	return manifests, nil
+}
---
+++ pkg/plugins/autodiscovery/cargo/utils.go
@@ -0,0 +1,146 @@
+package cargo
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+
+	"github.com/Masterminds/semver/v3"
+	sv "github.com/Masterminds/semver/v3"
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/text"
+	"github.com/updatecli/updatecli/pkg/plugins/utils/dasel"
+)
+
+// findCargoFiles search, recursively, for every files named Cargo.toml from a root directory.
+func findCargoFiles(rootDir string, validFiles []string) ([]string, error) {
+	cargoFiles := []string{}
+
+	err := filepath.WalkDir(rootDir, func(path string, di fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+
+		if di.IsDir() {
+			return nil
+		}
+
+		for _, f := range validFiles {
+			if di.Name() == f {
+				cargoFiles = append(cargoFiles, path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	logrus.Debugf("%d cargo files(s) found", len(cargoFiles))
+	for _, foundFile := range cargoFiles {
+		cargoFile := filepath.Base(filepath.Dir(foundFile))
+		logrus.Debugf("    * %q", cargoFile)
+	}
+	return cargoFiles, nil
+}
+
+func getDependencies(fc *dasel.FileContent, dependencyType string) ([]crateDependency, error) {
+	var dependencies []crateDependency
+	packages, err := fc.MultipleQuery(fmt.Sprintf(".%s.-", dependencyType))
+	if err != nil {
+		return dependencies, err
+	}
+	for _, pkg := range packages {
+		cd := crateDependency{
+			Name: pkg,
+		}
+		version, err := fc.DaselNode.Query(fmt.Sprintf(".%s.%s.version", dependencyType, pkg))
+		if err != nil {
+			// Cargo dependency has not been defined using a version
+			// It could have been defined using a git repository
+			_, err := fc.DaselNode.Query(fmt.Sprintf(".%s.%s.git", dependencyType, pkg))
+			if err == nil {
+				// TODO: Handle git dependencies
+				continue
+			}
+			// It could have been defined using a path to a local directory
+			_, err = fc.DaselNode.Query(fmt.Sprintf(".%s.%s.path", dependencyType, pkg))
+			if err == nil {
+				// TODO: Handle Path dependencies
+				continue
+			}
+
+			version, err := fc.DaselNode.Query(fmt.Sprintf(".%s.%s", dependencyType, pkg))
+			if err != nil {
+				continue
+			}
+			// Ensure version is semver compliant
+			v := version.String()
+			if _, err = sv.NewVersion(v); err != nil {
+				continue
+			}
+			cd.Version = v
+			cd.Inlined = true
+		} else {
+			cd.Version = version.String()
+		}
+		registry, _ := fc.DaselNode.Query(fmt.Sprintf(".%s.%s.registry", dependencyType, pkg))
+		if err == nil && registry != nil {
+			cd.Registry = registry.String()
+		}
+		dependencies = append(dependencies, cd)
+	}
+	return dependencies, nil
+}
+
+func getCrateMetadata(manifestPath string) (*crateMetadata, error) {
+
+	var crate crateMetadata
+
+	tomlFile := dasel.FileContent{
+		DataType:         "toml",
+		FilePath:         manifestPath,
+		ContentRetriever: &text.Text{},
+	}
+
+	err := tomlFile.Read("")
+
+	if err != nil {
+		return &crateMetadata{}, err
+	}
+
+	name, err := tomlFile.Query("package.name")
+
+	if err != nil {
+		return &crateMetadata{}, err
+	}
+
+	crate.Name = name
+	crate.Dependencies, _ = getDependencies(&tomlFile, "dependencies")
+	crate.DevDependencies, _ = getDependencies(&tomlFile, "dev-dependencies")
+
+	logrus.Debugf("Crate: %q\n", name)
+	logrus.Debugf("Dependencies")
+	for _, value := range crate.Dependencies {
+		logrus.Debugf("Name: %q\n", value.Name)
+		logrus.Debugf("Registry: %q\n", value.Registry)
+		logrus.Debugf("Version: %q\n", value.Version)
+	}
+	logrus.Debugf("Dev-Dependencies")
+	for _, value := range crate.DevDependencies {
+		logrus.Debugf("Name: %q\n", value.Name)
+		logrus.Debugf("Registry: %q\n", value.Registry)
+		logrus.Debugf("Version: %q\n", value.Version)
+	}
+
+	return &crate, nil
+}
+
+func isStrictSemver(version string) bool {
+	if _, err := semver.StrictNewVersion(version); err != nil {
+		return false
+	}
+	return true
+}
