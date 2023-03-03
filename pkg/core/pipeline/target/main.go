da3a6b10da9838363070e71f194089004a7509d4
Alexa Griffith <agriffith50@bloomberg.net>
2023-03-03 03:43:17 -0500

    add more information for disableinputsource (#1201)
    * add more information for disableinputsource
    
    add a little more detail so user understands the impact
    
    * Update pkg/core/pipeline/target/main.go
    
    * Update pkg/core/pipeline/target/main.go
    
    ---------
    
    Co-authored-by: Olivier Vernin <olivier@vernin.me>

--- pkg/core/pipeline/target/main.go
+++ pkg/core/pipeline/target/main.go
@@ -39,11 +39,10 @@ type Config struct {
 	ReportBody string `yaml:",omitempty"`
 	// ! Deprecated - please use all lowercase `sourceid`
 	DeprecatedSourceID string `yaml:"sourceID,omitempty" jsonschema:"-"`
-	// disablesourceinput disables the mechanism to retrieve a default value from a source.
+	// disablesourceinput disables the mechanism to retrieve a default value from a source. For example, if true, source information like changelog will not be accessible for a github/pullrequest action.
 	DisableSourceInput bool `yaml:",omitempty"`
 	// sourceid specifies where retrieving the default value
 	SourceID string `yaml:",omitempty"`
-	// disablesourceinput
 }
 
 // Check verifies if mandatory Targets parameters are provided and return false if not.
