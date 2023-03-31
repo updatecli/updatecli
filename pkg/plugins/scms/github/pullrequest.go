114b507fc584c9ae26f5eb56bad54e208f4d5897
Olivier Vernin <olivier@vernin.me>
2023-03-31 09:33:03 +0200

    Disable GitHub auto merge flag (#1254)
    * Always use the latest Go version available for the go.mod
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Disable GitHub automerge
    
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>
    
    * Update pkg/plugins/scms/github/pullrequest.go
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>
    Signed-off-by: Olivier Vernin <olivier.vernin@suse.com>
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- pkg/plugins/scms/github/pullrequest.go
+++ pkg/plugins/scms/github/pullrequest.go
@@ -194,10 +194,11 @@ func (p *PullRequest) CreateAction(title, changelog, pipelineReport string) erro
 	}
 
 	if p.spec.AutoMerge {
-		err = p.EnablePullRequestAutoMerge()
-		if err != nil {
-			return err
-		}
+		logrus.Warningf("GitHub automerge temporarily disabled on pull request due to https://github.com/updatecli/updatecli/issues/1252")
+		//err = p.EnablePullRequestAutoMerge()
+		//if err != nil {
+		//	return err
+		//}
 	}
 
 	return nil
