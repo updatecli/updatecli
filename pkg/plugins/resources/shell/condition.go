1fa3ee9d1d3ec346b6c27bec6fdcfac7b95bf443
Olblak <me@olblak.com>
2023-05-01 21:13:37 +0200

    refactor shell condition
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/shell/condition.go
+++ pkg/plugins/resources/shell/condition.go
@@ -4,25 +4,22 @@ import (
 	"fmt"
 
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
+	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
 // Condition tests if the provided command (concatenated with the source) is executed with success
-func (s *Shell) Condition(source string) (bool, error) {
-	return s.condition(source, "")
-}
+func (s *Shell) Condition(source string, scm scm.ScmHandler, resultCondition *result.Condition) error {
+	var workingDir string
+	if scm != nil {
+		workingDir = scm.GetDirectory()
+	}
 
-// ConditionFromSCM tests if the provided command (concatenated with the source) is executed with success from the SCM root directory
-func (s *Shell) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error) {
-	return s.condition(source, scm.GetDirectory())
-}
-
-func (s *Shell) condition(source, workingDir string) (bool, error) {
 	// Ensure environment variable(s) are up to date
 	// either it already has a value specified, or it retrieves
 	// the value from the Updatecli process
 	err := s.spec.Environments.Load()
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	// Provides the "UPDATECLI_PIPELINE_STAGE" environment variable set to "condition"
@@ -36,12 +33,12 @@ func (s *Shell) condition(source, workingDir string) (bool, error) {
 
 	err = s.success.PreCommand(s.getWorkingDirPath(workingDir))
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	scriptFilename, err := newShellScript(s.appendSource(source))
 	if err != nil {
-		return false, fmt.Errorf("failed initializing source script - %s", err)
+		return fmt.Errorf("failed initializing source script - %s", err)
 	}
 
 	err = s.executeCommand(command{
@@ -50,13 +47,29 @@ func (s *Shell) condition(source, workingDir string) (bool, error) {
 		Env: env.ToStringSlice(),
 	})
 	if err != nil {
-		return false, fmt.Errorf("failed while running condition script - %s", err)
+		return fmt.Errorf("failed while running condition script - %s", err)
 	}
 
 	err = s.success.PostCommand(s.getWorkingDirPath(workingDir))
 	if err != nil {
-		return false, err
+		return err
 	}
 
-	return s.success.ConditionResult()
+	ok, err := s.success.ConditionResult()
+	if err != nil {
+		return err
+	}
+
+	switch ok {
+	case true:
+		resultCondition.Pass = true
+		resultCondition.Result = result.SUCCESS
+		resultCondition.Description = fmt.Sprintf("shell condition of type %q, passing", s.spec.ChangedIf.Kind)
+	case false:
+		resultCondition.Pass = false
+		resultCondition.Result = result.SUCCESS
+		resultCondition.Description = fmt.Sprintf("shell condition of type %q not passing", s.spec.ChangedIf)
+	}
+
+	return nil
 }
