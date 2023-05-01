e5618c17dc9920ebbf4021217469a9b2d23ccbf1
Olblak <me@olblak.com>
2023-05-01 19:27:38 +0200

    refactor go module condition
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/go/module/condition_test.go
+++ pkg/plugins/resources/go/module/condition_test.go
@@ -1,18 +1,21 @@
 package gomodule
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
 func TestCondition(t *testing.T) {
 	tests := []struct {
-		name           string
-		spec           Spec
-		expectedResult bool
-		expectedError  bool
+		name             string
+		spec             Spec
+		expectedResult   bool
+		expectedError    bool
+		expectedErrorMsg error
 	}{
 		{
 			name: "canonical test",
@@ -30,18 +33,31 @@ func TestCondition(t *testing.T) {
 			},
 			expectedResult: true,
 		},
+		{
+			name: "Test go module version do not exist",
+			spec: Spec{
+				Module:  "github.com/MakeNowJust/heredoc",
+				Version: "v0.0.0",
+			},
+			expectedResult:   true,
+			expectedError:    true,
+			expectedErrorMsg: errors.New("version \"v0.0.0\" doesn't exist"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := New(tt.spec)
 			require.NoError(t, err)
-			gotVersion, err := got.Condition("")
+			gotResult := result.Condition{}
+			err = got.Condition("", nil, &gotResult)
 			if tt.expectedError {
-				assert.Error(t, err)
+				if assert.Error(t, err) {
+					assert.Equal(t, tt.expectedErrorMsg.Error(), err.Error())
+				}
 				return
 			}
 			require.NoError(t, err)
-			assert.Equal(t, tt.expectedResult, gotVersion)
+			assert.Equal(t, tt.expectedResult, gotResult.Pass)
 		})
 	}
 
