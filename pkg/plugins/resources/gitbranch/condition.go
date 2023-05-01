3d0e1a06155199aeaa678229dd845bbf58363090
Olblak <me@olblak.com>
2023-05-01 13:37:41 +0200

    refactor gitbranch condition
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/gitbranch/condition.go
+++ pkg/plugins/resources/gitbranch/condition.go
@@ -1,31 +1,36 @@
 package gitbranch
 
 import (
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
 	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
 // Condition checks that a git branch exists
-func (gt *GitBranch) Condition(source string) (bool, error) {
-	return gt.condition(source)
+func (gt *GitBranch) Condition(source string) error {
+	return gt.condition(source, nil, &result.Condition{})
 }
 
 // ConditionFromSCM test if a branch exists from a git repository specific from SCM
-func (gt *GitBranch) ConditionFromSCM(source string, scm scm.ScmHandler) (bool, error) {
-	path := scm.GetDirectory()
+func (gt *GitBranch) ConditionFromSCM(source string, scm scm.ScmHandler) error {
 
-	if len(gt.spec.Path) > 0 {
-		logrus.Warningf("Path is defined and set to %q but is overridden by the scm definition %q",
-			gt.spec.Path,
-			path)
-	}
-	gt.spec.Path = path
-
-	return gt.condition(source)
+	return gt.condition(source, nil, &result.Condition{})
 }
 
-func (gt *GitBranch) condition(source string) (bool, error) {
+func (gt *GitBranch) condition(source string, scm scm.ScmHandler, resultCondition *result.Condition) error {
+
+	if scm != nil {
+		path := scm.GetDirectory()
+
+		if len(gt.spec.Path) > 0 {
+			logrus.Warningf("Path is defined and set to %q but is overridden by the scm definition %q",
+				gt.spec.Path,
+				path)
+		}
+		gt.spec.Path = path
+	}
 
 	gt.branch = gt.spec.Branch
 	// If source input is empty, then it means that it was disabled by the user with `disablesourceinput: true`
@@ -36,29 +41,26 @@ func (gt *GitBranch) condition(source string) (bool, error) {
 
 	err := gt.Validate()
 	if err != nil {
-		return false, err
+		return fmt.Errorf("git tag validation: %w", err)
 	}
 
 	branches, err := gt.nativeGitHandler.Branches(gt.spec.Path)
 	if err != nil {
-		return false, err
+		return fmt.Errorf("searching git branches: %w", err)
 	}
 
-	found := false
 	for _, b := range branches {
 		if b == gt.branch {
-			found = true
+			resultCondition.Pass = true
+			resultCondition.Result = result.SUCCESS
+			resultCondition.Description = fmt.Sprintf("git branch %q matching", gt.branch)
+			return nil
 		}
 	}
 
-	if found {
-		logrus.Printf("%s Git branch %q matching\n", result.SUCCESS, gt.branch)
-		return true, nil
-	}
+	resultCondition.Pass = false
+	resultCondition.Result = result.FAILURE
+	resultCondition.Description = fmt.Sprintf("git branch %q not found", gt.branch)
 
-	logrus.Printf("%s Git branch %q not found\n",
-		result.FAILURE,
-		gt.branch)
-
-	return false, nil
+	return nil
 }
