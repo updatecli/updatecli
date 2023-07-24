e0186e1f19ca53217113cd0f2fb7d03c0e06154b
Olblak <me@olblak.com>
2023-07-25 00:39:56 +0200

    ignore gosec warning
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/udash/env.go
+++ pkg/core/udash/env.go
@@ -4,7 +4,7 @@ var (
 	// DefaultEnvVariableAPIURL defines the default environment variable used to retrieve API url
 	DefaultEnvVariableAPIURL string = "UPDATECLI_UDASH_API_URL"
 	// DefaultEnvVariableAccessToken defines the default environment variable used to retrieve API access token
-	DefaultEnvVariableAccessToken string = "UPDATECLI_UDASH_ACCESS_TOKEN"
+	DefaultEnvVariableAccessToken string = "UPDATECLI_UDASH_ACCESS_TOKEN" // #nosec G101
 	// DefaultEnvVariableURL defines the default environment variable used to retrieve url
 	DefaultEnvVariableURL string = "UPDATECLI_UDASH_URL"
 )
