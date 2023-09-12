f4f8e614116abdabfe8e27b7cb14153638876d66
Matthew Warman <mcwarman@gmail.com>
2023-09-12 08:16:26 +0100

    feat(ci): add terraform registry source (#1576)

---
+++ pkg/plugins/resources/terraform/registry/source_test.go
@@ -0,0 +1,74 @@
+package registry
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/updatecli/updatecli/pkg/core/httpclient"
+	"github.com/updatecli/updatecli/pkg/core/result"
+)
+
+func TestSource(t *testing.T) {
+	tests := []struct {
+		name            string
+		spec            Spec
+		expectedResult  string
+		expectedError   bool
+		mockedHttpBody  string
+		mockedHttpError error
+	}{
+		{
+			name: "Success - provider",
+			spec: Spec{
+				Type:      "provider",
+				Namespace: "hashicorp",
+				Name:      "kubernetes",
+			},
+			expectedResult: "2.23.0",
+			mockedHttpBody: `{ "versions" : ["2.23.0"] }`,
+		},
+		{
+			name: "Success - provider",
+			spec: Spec{
+				Type:         "module",
+				Namespace:    "terraform-aws-modules",
+				Name:         "vpc",
+				TargetSystem: "aws",
+			},
+			expectedResult: "5.1.1",
+			mockedHttpBody: `{ "versions" : ["5.1.1"] }`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New(tt.spec)
+			require.NoError(t, err)
+
+			got.webClient = &httpclient.MockClient{
+				DoFunc: func(req *http.Request) (*http.Response, error) {
+					body := tt.mockedHttpBody
+					statusCode := 200
+					return &http.Response{
+						StatusCode: statusCode,
+						Body:       ioutil.NopCloser(strings.NewReader(body)),
+					}, tt.mockedHttpError
+				},
+			}
+
+			gotResult := result.Source{}
+			err = got.Source("", &gotResult)
+
+			if tt.expectedError {
+				assert.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			assert.Equal(t, tt.expectedResult, gotResult.Information)
+		})
+	}
+}
