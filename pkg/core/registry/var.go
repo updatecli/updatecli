93673d129f44668c8b5e3774a808e5ff7a304a86
Olblak <me@olblak.com>
2023-09-19 08:46:25 +0200

    Fix comment typo
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/registry/var.go
+++ pkg/core/registry/var.go
@@ -5,7 +5,7 @@ var (
 	updatecliManifestMediaType string = "application/io.updatecli.manifest"
 	// updatecliValueMediaType is the OCI media type for updatecli value file.
 	updatecliValueMediaType string = "application/io.updatecli.value"
-	// updatecliSecretMediatType is the OCI media type for updatecli secret file.
+	// updatecliSecretMediaType is the OCI media type for updatecli secret file.
 	updatecliSecretMediaType string = "application/io.updatecli.secret"
 	// ociArtifactType is the media type for updatecli OCI artifacts.
 	ociArtifactType string = "application/io.updatecli.artifact"
