0b8a67031002c76344b89b7d46afe71405e9cda7
Olblak <me@olblak.com>
2023-09-20 14:57:30 +0200

    Ignore linter rule
    fix typo
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/engine/registry.go
+++ pkg/core/engine/registry.go
@@ -4,12 +4,13 @@ import (
 	"github.com/updatecli/updatecli/pkg/core/registry"
 )
 
-// PullFromRegistry retrieves an Updateli policy from an OCI registry.
-func (e *Engine) PullFromRegistry(policyReference string, disableTLS bool) error {
+// PullFromRegistry retrieves an Updatecli policy from an OCI registry.
+func (e *Engine) PullFromRegistry(policyReference string, disableTLS bool) (err error) {
 
 	PrintTitle("Registry")
 
-	_, _, _, err := registry.Pull(policyReference, disableTLS)
+	//nolint:dogsled
+	_, _, _, err = registry.Pull(policyReference, disableTLS)
 	if err != nil {
 		return err
 	}
@@ -17,13 +18,13 @@ func (e *Engine) PullFromRegistry(policyReference string, disableTLS bool) error
 	return nil
 }
 
-// PushToRegistry pushes an Updateli policy to an OCI registry.
-func (e *Engine) PushToRegistry(manifests, valuesFiles, secretsFiles, policyReference []string, disableTLS bool, filestore string) error {
+// PushToRegistry pushes an Updatecli policy to an OCI registry.
+func (e *Engine) PushToRegistry(manifests, valuesFiles, secretsFiles, policyReference []string, disableTLS bool, fileStore string) error {
 
 	PrintTitle("Registry")
 
 	manifests = sanitizeUpdatecliManifestFilePath(manifests)
-	err := registry.Push(manifests, valuesFiles, secretsFiles, policyReference, disableTLS, filestore)
+	err := registry.Push(manifests, valuesFiles, secretsFiles, policyReference, disableTLS, fileStore)
 	if err != nil {
 		return err
 	}
