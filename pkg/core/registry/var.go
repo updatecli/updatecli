ee45e1e1f8c5846a5cbfd12ad85764081b176e25
Olblak <me@olblak.com>
2023-09-24 20:46:13 +0200

    rename oci mediatype
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/registry/var.go
+++ pkg/core/registry/var.go
@@ -2,13 +2,13 @@ package registry
 
 var (
 	// updatecliManifestMediaType is the OCI media type for updatecli manifests.
-	updatecliManifestMediaType string = "application/io.updatecli.manifest"
+	updatecliManifestMediaType string = "application/io.updatecli.policy.manifest"
 	// updatecliValueMediaType is the OCI media type for updatecli value file.
-	updatecliValueMediaType string = "application/io.updatecli.value"
+	updatecliValueMediaType string = "application/io.updatecli.policy.value"
 	// updatecliSecretMediaType is the OCI media type for updatecli secret file.
-	updatecliSecretMediaType string = "application/io.updatecli.secret"
+	updatecliSecretMediaType string = "application/io.updatecli.policy.secret"
 	// ociArtifactType is the media type for updatecli OCI artifacts.
-	ociArtifactType string = "application/io.updatecli.artifact"
+	ociArtifactType string = "application/io.updatecli.policy"
 	// ociDefaultTag is the default tag for updatecli OCI images.
 	ociDefaultTag string = "latest"
 )
