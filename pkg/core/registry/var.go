34831a83b6c581c7fa920f2771159e8bb5bd87a6
Olblak <me@olblak.com>
2023-10-04 20:25:53 +0200

    Add alpha to artifact layer too
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/core/registry/var.go
+++ pkg/core/registry/var.go
@@ -2,11 +2,11 @@ package registry
 
 var (
 	// updatecliManifestMediaType is the OCI media type for updatecli manifests.
-	updatecliManifestMediaType string = "application/io.updatecli.policy.manifest"
+	updatecliManifestMediaType string = "application/io.updatecli.policy.manifest.alpha"
 	// updatecliValueMediaType is the OCI media type for updatecli value file.
-	updatecliValueMediaType string = "application/io.updatecli.policy.value"
+	updatecliValueMediaType string = "application/io.updatecli.policy.value.alpha"
 	// updatecliSecretMediaType is the OCI media type for updatecli secret file.
-	updatecliSecretMediaType string = "application/io.updatecli.policy.secret"
+	updatecliSecretMediaType string = "application/io.updatecli.policy.secret.alpha"
 	// ociArtifactType is the media type for updatecli OCI artifacts.
 	ociArtifactType string = "application/io.updatecli.policy.alpha"
 	// ociDefaultTag is the default tag for updatecli OCI images.
