b0a905731860c54bdd084919df4b67c1ca17cc01
Olivier Vernin <olivier@vernin.me>
2023-10-23 15:46:18 +0200

    feat: support for pinning docker digests by image config (#1712)
    * feat: support for pinning docker digests by image config
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * chore: run go mod tidy
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * Ignore golang.org/x/exp automated update
    
    Signed-off-by: Olblak <me@olblak.com>
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/dockerdigest/condition_test.go
+++ pkg/plugins/resources/dockerdigest/condition_test.go
@@ -16,10 +16,34 @@ func TestCondition(t *testing.T) {
 		expectedResult result.Condition
 	}{
 		{
-			name: "Test condition with a digest specified via the manifest",
+			name: "Test condition with a digest specified via the manifest without architecture",
 			spec: Spec{
 				Image:  "ghcr.io/updatecli/updatecli",
-				Digest: "v0.35.0@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Digest: "v0.64.1@sha256:0c7a19c9607b349cb90af92cb0ec98a70074192eb1a3463c3883f10663024ce5",
+			},
+			expectedResult: result.Condition{
+				Result: "SUCCESS",
+				Pass:   true,
+			},
+		},
+		{
+			name: "Test condition with a digest specified via the manifest for arm64 architecture",
+			spec: Spec{
+				Image:        "ghcr.io/updatecli/updatecli",
+				Architecture: "arm64",
+				Digest:       "v0.64.1@sha256:80c8393095062a96e74ac7e23aab35c8b639b890d94f2fe8fbcbbc983993e1de",
+			},
+			expectedResult: result.Condition{
+				Result: "SUCCESS",
+				Pass:   true,
+			},
+		},
+		{
+			name: "Test condition with a digest specified via the manifest for amd64 architecture",
+			spec: Spec{
+				Image:        "ghcr.io/updatecli/updatecli",
+				Architecture: "amd64",
+				Digest:       "v0.64.1@sha256:4eb444331ca649b24fa8905a98cc1bf922111b7b07292b86a30d3977e5e8f56d",
 			},
 			expectedResult: result.Condition{
 				Result: "SUCCESS",
@@ -30,8 +54,9 @@ func TestCondition(t *testing.T) {
 			name:         "Test condition with a digest specified via the source output",
 			sourceOutput: "v0.35.0@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
 			spec: Spec{
-				Image:  "ghcr.io/updatecli/updatecli",
-				Digest: "v0.35.0@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Image:        "ghcr.io/updatecli/updatecli",
+				Digest:       "v0.35.0@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Architecture: "amd64",
 			},
 			expectedResult: result.Condition{
 				Result: "SUCCESS",
@@ -41,8 +66,9 @@ func TestCondition(t *testing.T) {
 		{
 			name: "Test condition with a digest specified via the manifest without tag",
 			spec: Spec{
-				Image:  "ghcr.io/updatecli/updatecli",
-				Digest: "@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Image:        "ghcr.io/updatecli/updatecli",
+				Digest:       "@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Architecture: "amd64",
 			},
 			expectedResult: result.Condition{
 				Result: "SUCCESS",
@@ -53,8 +79,9 @@ func TestCondition(t *testing.T) {
 			name:         "Test condition with a digest specified via the source output",
 			sourceOutput: "v0.35.0@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
 			spec: Spec{
-				Image:  "ghcr.io/updatecli/updatecli",
-				Digest: "@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Image:        "ghcr.io/updatecli/updatecli",
+				Digest:       "@sha256:6e1833e5240ac52ecf7609623f18ec4536151e0f58b7243b92fd71ecdf3b94df",
+				Architecture: "amd64",
 			},
 			expectedResult: result.Condition{
 				Result: "SUCCESS",
