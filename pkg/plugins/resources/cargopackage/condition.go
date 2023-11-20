96d8481f498608fb29384ffe03e8bfef285e4cda
Damien Duportal <damien.duportal@gmail.com>
2023-11-20 16:30:47 +0100

    wip: update cargopackage resource to new Condition method signature
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/resources/cargopackage/condition.go
+++ pkg/plugins/resources/cargopackage/condition.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
-	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
-// Condition test if a tag exists from a git repository specific from SCM
-func (cp *CargoPackage) Condition(source string, scm scm.ScmHandler, resultCondition *result.Condition) error {
+// Condition checks if a cargo package with a specific version is published
+// We assume that if we can't find the package version in the index, then it means it doesn't exist.
+func (cp *CargoPackage) Condition(source string, scm scm.ScmHandler) (pass bool, message string, err error) {
 	if scm != nil {
 		path := scm.GetDirectory()
 		if cp.spec.Registry.RootDir != "" {
@@ -26,37 +26,24 @@ func (cp *CargoPackage) Condition(source string, scm scm.ScmHandler, resultCondi
 		cp.registry.RootDir = path
 	}
 
-	return cp.condition(source, resultCondition)
-}
-
-// Condition checks if a cargo package with a specific version is published
-// We assume that if we can't find the package version in the index, then it means it doesn't exist.
-func (cp *CargoPackage) condition(source string, resultCondition *result.Condition) error {
 	versionToCheck := cp.spec.Version
 	if versionToCheck == "" {
 		versionToCheck = source
 	}
 	if len(versionToCheck) == 0 {
-		return errors.New("no version defined")
+		return false, "", errors.New("no version defined")
 	}
 
 	_, versions, err := cp.getVersions()
 	if err != nil {
-		return fmt.Errorf("getting cargo package version: %w", err)
+		return false, "", fmt.Errorf("getting cargo package version: %w", err)
 	}
 
 	for _, v := range versions {
 		if v == versionToCheck {
-			resultCondition.Result = result.SUCCESS
-			resultCondition.Pass = true
-			resultCondition.Description = fmt.Sprintf("release version %q available\n", versionToCheck)
-			return nil
+			return true, fmt.Sprintf("release version %q available\n", versionToCheck), nil
 		}
 	}
 
-	resultCondition.Result = result.FAILURE
-	resultCondition.Description = fmt.Sprintf("version %q doesn't exist\n", versionToCheck)
-	resultCondition.Pass = false
-
-	return nil
+	return false, fmt.Sprintf("version %q doesn't exist\n", versionToCheck), nil
 }
