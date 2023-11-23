ce61aa8c9b841a34fa42ed6cee34a0c964fa95cc
Damien Duportal <damien.duportal@gmail.com>
2023-11-23 09:07:38 +0100

    wip: update gitlabtag resource to new Condition method signature
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/resources/gitlab/tag/condition.go
+++ pkg/plugins/resources/gitlab/tag/condition.go
@@ -5,24 +5,20 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
-	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
-func (g *Gitlab) Condition(source string, scm scm.ScmHandler, resultCondition *result.Condition) error {
+func (g *Gitlab) Condition(source string, scm scm.ScmHandler) (pass bool, message string, err error) {
 	if scm != nil {
 		logrus.Warningf("Condition not supported for the plugin GitHub Release")
 	}
 
 	tags, err := g.SearchTags()
 	if err != nil {
-		return fmt.Errorf("looking for GitLab tags: %w", err)
+		return false, "", fmt.Errorf("looking for GitLab tags: %w", err)
 	}
 
 	if len(tags) == 0 {
-		resultCondition.Result = result.FAILURE
-		resultCondition.Pass = false
-		resultCondition.Description = "no GitLab tag found"
-		return nil
+		return false, "no GitLab tag found", nil
 	}
 
 	tag := source
@@ -31,16 +27,9 @@ func (g *Gitlab) Condition(source string, scm scm.ScmHandler, resultCondition *r
 	}
 	for _, t := range tags {
 		if t == tag {
-			resultCondition.Result = result.SUCCESS
-			resultCondition.Pass = true
-			resultCondition.Description = fmt.Sprintf("GitLab tag %q found", t)
-			return nil
+			return true, fmt.Sprintf("GitLab tag %q found", t), nil
 		}
 	}
 
-	resultCondition.Result = result.FAILURE
-	resultCondition.Pass = false
-	resultCondition.Description = fmt.Sprintf("no GitLab tag found matching pattern %q of kind %q", g.spec.VersionFilter.Pattern, g.spec.VersionFilter.Kind)
-
-	return nil
+	return false, fmt.Sprintf("no GitLab tag found matching pattern %q of kind %q", g.spec.VersionFilter.Pattern, g.spec.VersionFilter.Kind), nil
 }
--- pkg/plugins/resources/gitlab/tag/condition_test.go
+++ pkg/plugins/resources/gitlab/tag/condition_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
 func TestCondition(t *testing.T) {
@@ -99,8 +98,7 @@ func TestCondition(t *testing.T) {
 			g, gotErr := New(tt.manifest)
 			require.NoError(t, gotErr)
 
-			gotResult := result.Condition{}
-			gotErr = g.Condition("", nil, &gotResult)
+			gotResult, _, gotErr := g.Condition("", nil)
 
 			if tt.wantErr {
 				require.Error(t, gotErr)
@@ -108,7 +106,7 @@ func TestCondition(t *testing.T) {
 				require.NoError(t, gotErr)
 			}
 
-			assert.Equal(t, tt.wantResult, gotResult.Pass)
+			assert.Equal(t, tt.wantResult, gotResult)
 
 		})
 
