22939b7dd3d5ef948b3a98aae26b2e5df63d055b
Damien Duportal <damien.duportal@gmail.com>
2023-11-23 09:35:26 +0100

    wip: update gomod resource to new Condition method signature
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/resources/go/gomod/condition.go
+++ pkg/plugins/resources/go/gomod/condition.go
@@ -1,57 +1,48 @@
 package gomod
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
-	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
 // Condition checks if a specific stable Golang version is published
-func (g *GoMod) Condition(source string, scm scm.ScmHandler, resultCondition *result.Condition) error {
-	var err error
-
+func (g *GoMod) Condition(source string, scm scm.ScmHandler) (pass bool, message string, err error) {
 	versionToCheck := g.spec.Version
 	if versionToCheck == "" {
 		versionToCheck = source
 	}
 
 	if len(versionToCheck) == 0 {
-		return errors.New("no version defined")
+		return false, "no version defined", nil
 	}
 
 	g.foundVersion, err = g.version(g.filename)
 	if err != nil {
 		if err == ErrModuleNotFound {
-			return fmt.Errorf("module path %q not found", g.spec.Module)
+			return false, "", fmt.Errorf("module path %q not found", g.spec.Module)
 		}
 
-		return fmt.Errorf("looking for Golang version: %w", err)
+		return false, "", fmt.Errorf("looking for Golang version: %w", err)
 	}
 
 	if g.foundVersion == versionToCheck {
 		switch g.kind {
 		case kindGolang:
-			resultCondition.Result = result.SUCCESS
-			resultCondition.Pass = true
-			resultCondition.Description = fmt.Sprintf("Golang version %q found", g.foundVersion)
+			return true, fmt.Sprintf("Golang version %q found", g.foundVersion), nil
 
 		case kindModule:
-			resultCondition.Result = result.SUCCESS
-			resultCondition.Pass = true
-			resultCondition.Description = fmt.Sprintf("Module version %q found for %q", g.foundVersion, g.spec.Module)
+			return true, fmt.Sprintf("Module version %q found for %q", g.foundVersion, g.spec.Module), nil
 		}
-		return nil
 	}
 
 	switch g.kind {
 	case kindGolang:
-		return fmt.Errorf("golang version %q found, expecting %q",
-			g.foundVersion, versionToCheck)
+		return false, fmt.Sprintf("golang version %q found, expecting %q",
+			g.foundVersion, versionToCheck), nil
 	case kindModule:
-		return fmt.Errorf("golang module version %q found for %q, expecting %q",
-			g.foundVersion, g.spec.Module, versionToCheck)
+		return false, fmt.Sprintf("golang module version %q found for %q, expecting %q",
+			g.foundVersion, g.spec.Module, versionToCheck), nil
 	}
-	return fmt.Errorf("something unexpected happened")
+	return false, "", fmt.Errorf("something unexpected happened")
 }
