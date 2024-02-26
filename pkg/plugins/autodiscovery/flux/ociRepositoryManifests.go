8fa83bca68403acdffe6cc32f18064d99dcac040
Olivier Vernin <olivier@vernin.me>
2024-02-26 17:00:54 +0100

    feat: add flux autodiscovery plugin (#1950)
    * feat: add flux autodiscovery plugin
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: remove duplicated variable declaration
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: don't call unnecessary string function
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: use logrus instead of fmt.Print
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * core: use appropriate error function
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: add file endline
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(spelling): add worlds to allow.txt
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(spelling): add few more words
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * feat: use fluxcd api
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(spelling): add gvk to allow.txt
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * chore: use multiline comment with //
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * feat: allow to opt in/out helmrelease and ociregistry
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * feat: make matchinrule setting more generic
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(spelling): rename ocirepository
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: update flux e2e tests
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(spelling): add kyverno to allow.txt
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * chore: various cleanup
    
    Signed-off-by: Olblak <me@olblak.com>
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/autodiscovery/flux/ociRepositoryManifests.go
@@ -0,0 +1,131 @@
+package flux
+
+import (
+	"bytes"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/plugins/resources/dockerimage"
+)
+
+func (f Flux) discoverOCIRepositoryManifests() [][]byte {
+
+	var manifests [][]byte
+
+	for _, foundFluxFile := range f.ociRepositoryFiles {
+		logrus.Debugf("parsing oci repository file %q", foundFluxFile)
+
+		relateFoundFluxFile, err := filepath.Rel(f.rootDir, foundFluxFile)
+		if err != nil {
+			logrus.Debugln(err)
+			continue
+		}
+
+		data, err := loadOCIRepository(foundFluxFile)
+		if err != nil {
+			logrus.Debugln(err)
+			continue
+		}
+
+		if data == nil {
+			continue
+		}
+
+		ociName := strings.TrimPrefix(data.Spec.URL, "oci://")
+		ociVersion := data.Spec.Reference.Tag
+
+		// Skip pipeline if at least of the oci url or oci tag is not specified
+		if len(ociName) == 0 || len(ociVersion) == 0 {
+			continue
+		}
+
+		if len(f.spec.Ignore) > 0 {
+			if f.spec.Ignore.isMatchingRules(f.rootDir, relateFoundFluxFile, "", ociName, ociVersion) {
+				logrus.Debugf("Ignoring OCI repository %q from %q, as matching ignore rule(s)\n", ociName, relateFoundFluxFile)
+				continue
+			}
+		}
+
+		if len(f.spec.Only) > 0 {
+			if !f.spec.Only.isMatchingRules(f.rootDir, relateFoundFluxFile, "", ociName, ociVersion) {
+				logrus.Debugf("Ignoring OCI repository %q from %q, as not matching only rule(s)\n", ociName, relateFoundFluxFile)
+				continue
+			}
+		}
+
+		versionFilterKind := defaultVersionFilterKind
+		versionFilterPattern := defaultVersionFilterPattern
+		tagFilter := ""
+
+		sourceSpec := dockerimage.NewDockerImageSpecFromImage(ociName, ociVersion, f.spec.Auths)
+		if sourceSpec != nil {
+			versionFilterKind = sourceSpec.VersionFilter.Kind
+			versionFilterPattern = sourceSpec.VersionFilter.Pattern
+			tagFilter = sourceSpec.TagFilter
+		}
+
+		if !f.spec.VersionFilter.IsZero() {
+			versionFilterKind = f.versionFilter.Kind
+			versionFilterPattern, err = f.versionFilter.GreaterThanPattern(ociVersion)
+			if err != nil {
+				logrus.Debugf("building version filter pattern: %s", err)
+				versionFilterPattern = ociVersion
+			}
+		}
+
+		var tmpl *template.Template
+		if f.digest && sourceSpec != nil {
+			tmpl, err = template.New("manifest").Parse(ociRepositoryManifestTemplateDigestAndLatest)
+			if err != nil {
+				logrus.Debugf("parsing oci repository file %q: %s", foundFluxFile, err)
+				continue
+			}
+		} else if f.digest && sourceSpec == nil {
+			tmpl, err = template.New("manifest").Parse(ociRepositoryManifestTemplateDigest)
+			if err != nil {
+				logrus.Debugf("parsing oci repository file %q: %s", foundFluxFile, err)
+				continue
+			}
+		} else if !f.digest && sourceSpec != nil {
+			tmpl, err = template.New("manifest").Parse(ociRepositoryManifestTemplateLatest)
+			if err != nil {
+				logrus.Debugf("parsing oci repository file %q: %s", foundFluxFile, err)
+				continue
+			}
+		} else {
+			logrus.Infoln("No source spec detected")
+			continue
+		}
+
+		params := struct {
+			OCIName              string
+			OCIVersion           string
+			File                 string
+			ImageName            string
+			VersionFilterKind    string
+			VersionFilterPattern string
+			ScmID                string
+			TagFilter            string
+		}{
+			OCIName:              ociName,
+			OCIVersion:           ociVersion,
+			File:                 relateFoundFluxFile,
+			VersionFilterKind:    versionFilterKind,
+			VersionFilterPattern: versionFilterPattern,
+			ScmID:                f.scmID,
+			TagFilter:            tagFilter,
+		}
+
+		manifest := bytes.Buffer{}
+		if err := tmpl.Execute(&manifest, params); err != nil {
+			logrus.Debugln(err)
+			continue
+		}
+
+		manifests = append(manifests, manifest.Bytes())
+	}
+
+	return manifests
+}
