7609286f610efc472c2eae8e537fa7da004ca3f1
Olblak <me@olblak.com>
2024-04-03 20:16:59 +0200

    add empty cleanAction to none GitHub scm
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/resources/gitea/pullrequest/clean.go
@@ -0,0 +1,12 @@
+package pullrequest
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/reports"
+)
+
+// CleanAction verify if existing action requires some operations
+func (g *Gitea) CleanAction(report reports.Action) error {
+	logrus.Debugln("cleaning Gitea pull-request is not yet supported. Feel free to open an issue to mark your interest.")
+	return nil
+}
---
+++ pkg/plugins/resources/gitlab/mergerequest/clean.go
@@ -0,0 +1,12 @@
+package mergerequest
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/updatecli/updatecli/pkg/core/reports"
+)
+
+// CleanAction verify if existing action requires some operations
+func (g *Gitlab) CleanAction(report reports.Action) error {
+	logrus.Debugln("cleaning Gitlab merge request is not yet supported. Feel free to open an issue to mark your interest.")
+	return nil
+}
