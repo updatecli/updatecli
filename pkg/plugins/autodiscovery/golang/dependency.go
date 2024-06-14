37254de78c5ae511c7300ac2799224f98d35e8c4
Olblak <me@olblak.com>
2024-06-14 21:18:40 +0200

    fix: golang autodiscovery should allow go version update only
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/golang/dependency.go
+++ pkg/plugins/autodiscovery/golang/dependency.go
@@ -64,6 +64,10 @@ func (g Golang) discoverDependencyManifests() ([][]byte, error) {
 		}
 
 		for goModule, goModuleVersion := range goModules {
+			// Skip golang module manifest if there is only one rule on the go version
+			if g.spec.Only.isGoVersionOnly() {
+				break
+			}
 			// Test if the ignore rule based on path is respected
 			if len(g.spec.Ignore) > 0 {
 				if g.spec.Ignore.isMatchingRules(g.rootDir, relativeFoundFile, goVersion, goModule, goModuleVersion) {
