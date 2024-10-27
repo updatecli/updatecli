aee57054ccc1c0819066c526d7fb5735153c171e
Damien Duportal <damien.duportal@gmail.com>
2024-10-27 17:05:00 +0100

    fix(temurin) fail explicitly when specifying condition or target of kind 'temurin'
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg/plugins/resources/temurin/condition.go
+++ pkg/plugins/resources/temurin/condition.go
@@ -1,6 +1,8 @@
 package temurin
 
 import (
+	"fmt"
+
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
 )
 
@@ -9,5 +11,6 @@ Condition tests if the response of the specified HTTP request meets assertion.
 If no assertion is specified, it only checks for successful HTTP response code (HTTP/1xx, HTTP/2xx or HTTP/3xx).
 */
 func (t *Temurin) Condition(source string, scm scm.ScmHandler) (pass bool, message string, err error) {
-	return false, "", nil
+	message = "Condition not supported for the resources of kind 'temurin'"
+	return false, message, fmt.Errorf(message)
 }
