59441823fe173b339691a05b60a1ff4899a5c91a
Olivier Vernin <olivier@vernin.me>
2024-10-28 10:02:41 +0100

    Update pkg/plugins/resources/temurin/condition.go

--- pkg/plugins/resources/temurin/condition.go
+++ pkg/plugins/resources/temurin/condition.go
@@ -11,6 +11,6 @@ Condition tests if the response of the specified HTTP request meets assertion.
 If no assertion is specified, it only checks for successful HTTP response code (HTTP/1xx, HTTP/2xx or HTTP/3xx).
 */
 func (t *Temurin) Condition(source string, scm scm.ScmHandler) (pass bool, message string, err error) {
-	message = "Condition not supported for the resource of kind 'temurin'"
+	message = "Condition not supported for resource of kind 'temurin'"
 	return false, message, fmt.Errorf(message)
 }
