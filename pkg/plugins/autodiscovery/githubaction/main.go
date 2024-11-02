7a158f5647043aa158667d66066aa91676da4504
Olblak <me@olblak.com>
2024-11-02 08:48:52 +0100

    fix: correctly handle rootdir
    This commit also clean up spec comment and remove unused token variable
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/githubaction/main.go
+++ pkg/plugins/autodiscovery/githubaction/main.go
@@ -1,6 +1,8 @@
 package githubaction
 
 import (
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/mitchellh/mapstructure"
@@ -21,20 +23,29 @@ var (
 
 // Spec defines the parameters which can be provided to the Github Action crawler.
 type Spec struct {
+	// files allows to specify the accepted GitHub Action workflow file name
+	//
+	// default:
+	//   - ".github/workflows/*.yaml",
+	//   - ".github/workflows/*.yml",
+	//   - ".gitea/workflows/*.yaml",
+	//   - ".gitea/workflows/*.yml",
+	//   - ".forgejo/workflows/*.yaml",
+	//   - ".forgejo/workflows/*.yml",
 	Files []string `yaml:",omitempty"`
-	// ignore allows to specify rule to ignore autodiscovery a specific Flux helmrelease based on a rule
+	// ignore allows to specify rule to ignore autodiscovery a specific GitHub action based on a rule
 	//
 	// default: empty
 	//
 	Ignore MatchingRules `yaml:",omitempty"`
-	// only allows to specify rule to only autodiscover manifest for a specific Flux helm release based on a rule
+	// only allows to specify rule to only autodiscover manifest for a specific GitHub action based on a rule
 	//
 	// default: empty
 	//
 	Only MatchingRules `yaml:",omitempty"`
-	// OCIRepository allows to specify if OCI repository files should be updated
+	// rootDir allows to specify the root directory from where looking for GitHub Action
 	//
-	// default: true
+	// default: empty
 	RootDir string `yaml:",omitempty"`
 	// versionfilter provides parameters to specify the version pattern used when generating manifest.
 	//
@@ -61,6 +72,23 @@ type Spec struct {
 	//
 	VersionFilter version.Filter `yaml:",omitempty"`
 	// Credentials allows to specify the credentials to use to authenticate to the git provider
+	// The ID of the credential must be the domain of the git provider to configure
+	//
+	// default: empty
+	//
+	// examples:
+	// ```
+	//   autodiscovery:
+	//     crawlers:
+	//       github/action:
+	//         credentials:
+	//           "code.forgejo.com":
+	//             kind: gitea
+	//             token: xxx
+	//           "github.com":
+	//             kind: github
+	//             token: '{{ requiredEnv "GITHUB_TOKEN" }}'
+	// ```
 	Credentials map[string]gitProviderToken `yaml:",omitempty"`
 }
 
@@ -80,8 +108,6 @@ type GitHubAction struct {
 	versionFilter version.Filter
 	// workflowFiles is a list of HelmRelease files found
 	workflowFiles []string
-	// token allows to specify a GitHub token to use for GitHub API requests
-	token string
 }
 
 type gitProviderToken struct {
@@ -91,12 +117,13 @@ type gitProviderToken struct {
 	Kind string `yaml:",omitempty"`
 	// Token defines the Token to use to authenticate to the git provider
 	//
-	// The default value depeneds on the action domain
+	// The default value depends on the action domain
 	// For 'github.com', the default value is set to first environment detected
 	//  1. "UPDATECLI_GITHUB_TOKEN"
 	//  2. "GITHUB_TOKEN"
 	//
 	// For 'gitea.com' and 'codeberg.org', the default value is set to first environment detected
+	//  1. "UPDATECLI_GITHUB_TOKEN"
 	//  1. "GITEA_TOKEN"
 	Token string `yaml:",omitempty"`
 }
@@ -111,7 +138,10 @@ func New(spec interface{}, rootDir, scmID string) (GitHubAction, error) {
 	}
 
 	dir := rootDir
-	if len(s.RootDir) > 0 {
+	if path.IsAbs(s.RootDir) {
+		if scmID != "" {
+			logrus.Warningf("rootdir %q is an absolute path, scmID %q will be ignored", s.RootDir, scmID)
+		}
 		dir = s.RootDir
 	}
 
@@ -149,7 +179,14 @@ func (g GitHubAction) DiscoverManifests() ([][]byte, error) {
 	logrus.Infof("\n\n%s\n", strings.ToTitle("GitHub Action"))
 	logrus.Infof("%s\n", strings.Repeat("=", len("GitHub Action")+1))
 
-	err := g.searchWorkflowFiles(g.rootDir, g.files)
+	searchFromDir := g.rootDir
+	// If the spec.RootDir is an absolute path, then it as already been set
+	// correctly in the New function.
+	if g.spec.RootDir != "" && !path.IsAbs(g.spec.RootDir) {
+		searchFromDir = filepath.Join(g.rootDir, g.spec.RootDir)
+	}
+
+	err := g.searchWorkflowFiles(searchFromDir, g.files)
 	if err != nil {
 		return nil, err
 	}
