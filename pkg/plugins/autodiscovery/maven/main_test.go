b218dc3008bf521963992b2e89e22c1a9b0df706
Olivier Vernin <olivier@vernin.me>
2024-12-16 17:37:30 +0100

    feat(maven): Support settings.xml for setting credentials and mirrors (#3270)
    * feat(maven): Support mirrors from settings.xml
    
    Now Updatecli can retrieve maven artifact from mirrored url specified by
    settings.xml files
    
    Updatecli fetches information in the following files (order matter):
    * './settings.xml'
    * '$HOME/.m2/settings.xml'
    * '$MAVEN_HOME/conf/settings.xml'
    * '$M2_HOME/conf/settings.xml'
    
    We also refactor the test to move them from a subpackage to the maven
    one
    
    We refactor the target name to follow conventional commits as target
    name are used for commit title and pull request title by default.
    
    * chore: add mirrorof test cases
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: correctly handle settings.xml
    
    * chore: refactor maven autodiscovery to remove code duplication
    
    * chore: refactor look up for settings.xml
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: refactor maven repositories mirror & credentials
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix: scenario without settings.xml
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * chore: improve debug message
    
    Signed-off-by: Olblak <me@olblak.com>
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>

---
+++ pkg/plugins/autodiscovery/maven/main_test.go
@@ -0,0 +1,249 @@
+package maven
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDiscoverManifests(t *testing.T) {
+	testdata := []struct {
+		name              string
+		rootDir           string
+		expectedPipelines []string
+	}{
+		{
+			name:    "Scenario - with external http mirrorof",
+			rootDir: "testdata/externalhttp",
+			expectedPipelines: []string{`name: 'Bump Maven dependency org.apache.commons/commons-lang3'
+sources:
+  org.apache.commons/commons-lang3:
+    name: 'Get latest Maven Artifact version "org.apache.commons/commons-lang3"'
+    kind: 'maven'
+    spec:
+      artifactid: 'commons-lang3'
+      groupid: 'org.apache.commons'
+      repositories:
+        - 'https://mirror.example.com/maven'
+      versionfilter:
+        kind: 'latest'
+        pattern: 'latest'
+conditions:
+  artifactid:
+    name: 'Ensure dependency artifactId "commons-lang3" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/artifactId'
+      value: 'commons-lang3'
+    disablesourceinput: true
+  groupid:
+    name: 'Ensure dependency groupId "org.apache.commons" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/groupId'
+      value: 'org.apache.commons'
+    disablesourceinput: true
+targets:
+  org.apache.commons/commons-lang3:
+    name: 'deps(maven): update "org.apache.commons/commons-lang3" to {{ source "org.apache.commons/commons-lang3" }}'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/version'
+    sourceid: 'org.apache.commons/commons-lang3'
+`},
+		},
+		{
+			name:    "Scenario - with excluded mirrorof",
+			rootDir: "testdata/exclude",
+			expectedPipelines: []string{`name: 'Bump Maven dependency org.apache.commons/commons-lang3'
+sources:
+  org.apache.commons/commons-lang3:
+    name: 'Get latest Maven Artifact version "org.apache.commons/commons-lang3"'
+    kind: 'maven'
+    spec:
+      artifactid: 'commons-lang3'
+      groupid: 'org.apache.commons'
+      repositories:
+        - 'https://foo:bar@mirror.example.com/maven'
+        - 'http://bar-repo.example.com/maven'
+      versionfilter:
+        kind: 'latest'
+        pattern: 'latest'
+conditions:
+  artifactid:
+    name: 'Ensure dependency artifactId "commons-lang3" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/artifactId'
+      value: 'commons-lang3'
+    disablesourceinput: true
+  groupid:
+    name: 'Ensure dependency groupId "org.apache.commons" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/groupId'
+      value: 'org.apache.commons'
+    disablesourceinput: true
+targets:
+  org.apache.commons/commons-lang3:
+    name: 'deps(maven): update "org.apache.commons/commons-lang3" to {{ source "org.apache.commons/commons-lang3" }}'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/version'
+    sourceid: 'org.apache.commons/commons-lang3'
+`},
+		},
+		{
+			name:    "Scenario - Default",
+			rootDir: "testdata/default",
+			expectedPipelines: []string{`name: 'Bump Maven dependency com.jcraft/jsch'
+sources:
+  com.jcraft/jsch:
+    name: 'Get latest Maven Artifact version "com.jcraft/jsch"'
+    kind: 'maven'
+    spec:
+      artifactid: 'jsch'
+      groupid: 'com.jcraft'
+      repositories:
+        - 'https://repo.jenkins-ci.org/public/'
+        - 'https://mirror.example.com/maven2'
+      versionfilter:
+        kind: 'latest'
+        pattern: 'latest'
+conditions:
+  artifactid:
+    name: 'Ensure dependency artifactId "jsch" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/artifactId'
+      value: 'jsch'
+    disablesourceinput: true
+  groupid:
+    name: 'Ensure dependency groupId "com.jcraft" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/groupId'
+      value: 'com.jcraft'
+    disablesourceinput: true
+targets:
+  com.jcraft/jsch:
+    name: 'deps(maven): update "com.jcraft/jsch" to {{ source "com.jcraft/jsch" }}'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencies/dependency[1]/version'
+    sourceid: 'com.jcraft/jsch'
+`, `name: 'Bump Maven dependencyManagement io.jenkins.tools.bom/bom-2.346.x'
+sources:
+  io.jenkins.tools.bom/bom-2.346.x:
+    name: 'Get latest Maven Artifact version "io.jenkins.tools.bom/bom-2.346.x"'
+    kind: 'maven'
+    spec:
+      artifactid: 'bom-2.346.x'
+      groupid: 'io.jenkins.tools.bom'
+      repositories:
+        - 'https://repo.jenkins-ci.org/public/'
+        - 'https://mirror.example.com/maven2'
+      versionfilter:
+        kind: 'latest'
+        pattern: 'latest'
+conditions:
+  artifactid:
+    name: 'Ensure dependencyManagement artifactId "bom-2.346.x" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencyManagement/dependencies/dependency[1]/artifactId'
+      value: 'bom-2.346.x'
+    disablesourceinput: true
+  groupid:
+    name: 'Ensure dependencyManagement groupId "io.jenkins.tools.bom" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencyManagement/dependencies/dependency[1]/groupId'
+      value: 'io.jenkins.tools.bom'
+    disablesourceinput: true
+targets:
+  io.jenkins.tools.bom/bom-2.346.x:
+    name: 'deps(maven): update "io.jenkins.tools.bom/bom-2.346.x" to {{ source "io.jenkins.tools.bom/bom-2.346.x" }}'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/dependencyManagement/dependencies/dependency[1]/version'
+    sourceid: 'io.jenkins.tools.bom/bom-2.346.x'
+`, `name: 'Bump Maven parent Pom org.jenkins-ci.plugins/plugin'
+sources:
+  org.jenkins-ci.plugins/plugin:
+    name: 'Get latest Parent Pom Artifact version "org.jenkins-ci.plugins/plugin"'
+    kind: 'maven'
+    spec:
+      artifactid: 'plugin'
+      groupid: 'org.jenkins-ci.plugins'
+      repositories:
+        - 'https://repo.jenkins-ci.org/public/'
+        - 'https://mirror.example.com/maven2'
+      versionfilter:
+        kind: 'latest'
+        pattern: 'latest'
+conditions:
+  artifactid:
+    name: 'Ensure parent artifactId "plugin" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/parent/artifactId'
+      value: 'plugin'
+    disablesourceinput: true
+  groupid:
+    name: 'Ensure parent pom.xml groupId "org.jenkins-ci.plugins" is specified'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/parent/groupId'
+      value: 'org.jenkins-ci.plugins'
+    disablesourceinput: true
+targets:
+  org.jenkins-ci.plugins/plugin:
+    name: 'deps(maven): update "org.jenkins-ci.plugins/plugin" to {{ source "org.jenkins-ci.plugins/plugin" }}'
+    kind: 'xml'
+    spec:
+      file: 'pom.xml'
+      path: '/project/parent/version'
+    sourceid: 'org.jenkins-ci.plugins/plugin'
+`},
+		},
+	}
+
+	for _, tt := range testdata {
+
+		t.Run(tt.name, func(t *testing.T) {
+			maven, err := New(
+				Spec{}, tt.rootDir, "")
+
+			require.NoError(t, err)
+
+			var pipelines []string
+			rawPipelines, err := maven.DiscoverManifests()
+			require.NoError(t, err)
+
+			assert.Equal(t, len(tt.expectedPipelines), len(rawPipelines))
+
+			for i := range rawPipelines {
+				// We expect manifest generated by the autodiscovery to use the yaml syntax
+				pipelines = append(pipelines, string(rawPipelines[i]))
+				assert.Equal(t, tt.expectedPipelines[i], pipelines[i])
+			}
+		})
+	}
+
+}
--- pkg/plugins/autodiscovery/maven/utils.go
+++ pkg/plugins/autodiscovery/maven/utils.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strings"
 
 	"github.com/beevik/etree"
 	"github.com/sirupsen/logrus"
@@ -164,4 +165,117 @@ func getDependencyManagementsFromPom(doc *etree.Document) []dependency {
 
 	}
 	return dependencies
+}
+
+// getMavenRepositoriesURL returns the URL for all repository while trying to identify potential credentials and mirrors
+// It will look for repositories in the pom.xml and in the settings.xml
+func getMavenRepositoriesURL(pomFile string, doc *etree.Document) (mavenRepositories []string) {
+
+	repositories := getRepositoriesFromPom(doc)
+
+	settingsXMLMap := map[string]Settings{}
+
+	for _, path := range getSettingsXMLPath(filepath.Dir(pomFile)) {
+		settings := readSettingsXML(path)
+		if settings == nil {
+			logrus.Debugf("No settings.xml content found in %q", path)
+			continue
+		}
+
+		settingsXMLRepositories := settings.getRepositoriesFromSettingsXML()
+
+		if len(settingsXMLRepositories) > 0 {
+			logrus.Debugf("Found %d repositories in %q", len(settingsXMLRepositories), path)
+			for _, repo := range settingsXMLRepositories {
+				logrus.Debugf("\t* repository %q %q", repo.ID, repo.URL)
+			}
+
+			repositories = append(repositories, settingsXMLRepositories...)
+		}
+
+		settingsXMLMap[path] = *settings
+	}
+
+	setCredentials := func(settings Settings, url, id string) (string, bool) {
+
+		username, password := settings.getMatchingServerCredentials(id)
+
+		cred := strings.Join([]string{username, password}, ":")
+
+		foundCredentials := username != ""
+		if foundCredentials {
+			if strings.HasPrefix(url, "http://") {
+				return strings.Replace(url, "http://", fmt.Sprintf("http://%s@", cred), 1), foundCredentials
+			} else if strings.HasPrefix(url, "https://") {
+				return strings.Replace(url, "https://", fmt.Sprintf("https://%s@", cred), 1), foundCredentials
+			} else if strings.HasPrefix(url, "file:/") {
+				logrus.Debugf("Skipping credentials for file based repository %q, feel free to open an issue on github.com/updatecli/updatecli", url)
+			} else {
+				return fmt.Sprintf("http://%s@%s", cred, url), foundCredentials
+			}
+		}
+
+		return url, foundCredentials
+	}
+
+	for _, repo := range repositories {
+		// If settings.xml is empty, we add the repository URL to the list
+		// as nothing can modify repository URL from the pom.xml
+		if len(settingsXMLMap) == 0 {
+			mavenRepositories = append(mavenRepositories, repo.URL)
+			continue
+		}
+
+		for path, settings := range settingsXMLMap {
+
+			mirrorURL, mirrorID, foundMirrorOf := settings.getMatchingMirrorOf(repo.ID, repo.URL)
+
+			if mirrorURL != "" && mirrorID == "" {
+				logrus.Warningf("Found proxy URL %q for repository %q in config file %q but no mirror ID", mirrorURL, repo.ID, path)
+			} else if mirrorURL == "" && mirrorID != "" {
+				logrus.Warningf("Found mirror ID %q for repository %q in config file %q but no proxy URL", mirrorID, repo.ID, path)
+			}
+
+			if foundMirrorOf {
+				logrus.Debugf("Found proxy URL %q for repository %q in config file %q", mirrorURL, mirrorID, path)
+				mirrorURL, _ = setCredentials(settings, mirrorURL, mirrorID)
+				mavenRepositories = append(mavenRepositories, mirrorURL)
+				continue
+			}
+
+			if repoURL, foundCredentials := setCredentials(settings, repo.URL, repo.ID); foundCredentials {
+				mavenRepositories = append(mavenRepositories, repoURL)
+				continue
+			}
+
+			if mavenMirrorURLFromEnv != "" {
+				mavenRepositories = append(mavenRepositories, mavenMirrorURLFromEnv)
+				continue
+			}
+
+			mavenRepositories = append(mavenRepositories, repo.URL)
+		}
+	}
+
+	if len(repositories) == 0 {
+		mavenCentralURL := "https://repo.maven.apache.org/maven2"
+		for _, settings := range settingsXMLMap {
+
+			mirrorURL, mirrorID := settings.getCentralMatchingMirrorOf()
+
+			if mirrorURL != "" {
+				if repoURL, foundCredentials := setCredentials(settings, mirrorURL, mirrorID); foundCredentials {
+					mavenRepositories = append(mavenRepositories, repoURL)
+					continue
+				}
+			}
+
+			if repoURL, foundCredentials := setCredentials(settings, mavenCentralURL, "central"); foundCredentials {
+				mavenRepositories = append(mavenRepositories, repoURL)
+				continue
+			}
+		}
+	}
+
+	return mavenRepositories
 }
