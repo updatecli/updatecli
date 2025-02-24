a78239741e683b481c9152c5b0e402c91ba4a59c
Olblak <me@olblak.com>
2025-02-24 21:36:43 +0100

    feat: go changelog handle proxy

--- pkg/plugins/resources/go/module/changelog.go
+++ pkg/plugins/resources/go/module/changelog.go
@@ -1,27 +1,92 @@
 package gomodule
 
 import (
+	"io"
+	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/result"
 	githubChangelog "github.com/updatecli/updatecli/pkg/plugins/changelog/github/v3"
+	"golang.org/x/net/html"
 )
 
 // Changelog returns the changelog for a specific golang module, or an empty string if it couldn't find one
 func (g *GoModule) Changelog(from, to string) *result.Changelogs {
 
-	if from == "" && to == "" {
+	return DetectChangelogSource(g.Spec.Module, from, to, 0)
+
+}
+
+// DetectChangelogSource tries to identify based on the Goland module
+// where the changelog is located. At the moment it only supports
+// GitHub repositories both direct and from a proxy.
+func DetectChangelogSource(module, from, to string, depth int) *result.Changelogs {
+
+	if module == "" {
 		return nil
 	}
 
-	if strings.HasPrefix(g.Spec.Module, "github.com") {
-		return getChangelogFromGitHub(g.Spec.Module, from, to)
+	if strings.HasPrefix(module, "github.com/") {
+		return getChangelogFromGitHub(module, from, to)
+	}
+
+	if depth == 0 {
+		return getChangelogFromProxy(module, from, to)
+	}
+
+	return nil
+
+}
+
+func getChangelogFromProxy(module, from, to string) *result.Changelogs {
+
+	if module == "" {
+		return nil
+	}
+
+	URL, err := url.Parse(module)
+	if err != nil {
+		logrus.Errorf("something went wrong while parsing module %q\n", err)
+		return nil
+	}
+
+	if URL.Scheme == "" {
+		URL.Scheme = "https"
+	}
+
+	query := URL.Query()
+	query.Set("go-get", "1")
+	URL.RawQuery = query.Encode()
+
+	httpClient := http.DefaultClient
+
+	res, err := httpClient.Get(URL.String())
+	if err != nil {
+		logrus.Errorf("something went wrong while getting go module api data %q\n", err)
+		return nil
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode >= 400 {
+		logrus.Errorf("something went wrong while getting go module data %q\n", err)
+		return nil
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil
+	}
+
+	if parsedModule := getGitRepositoryURL(string(data)); parsedModule != "" {
+		return DetectChangelogSource(parsedModule, from, to, 1)
 	}
 
 	return nil
 }
 
+// getChangelogFromGitHub retrieves the releases notes from a GitHub repository
 func getChangelogFromGitHub(module, from, to string) *result.Changelogs {
 	parsedModule := strings.Split(module, "/")
 
@@ -40,4 +105,63 @@ func getChangelogFromGitHub(module, from, to string) *result.Changelogs {
 	}
 
 	return &releases
+}
+
+// getGitRepositoryURL parse a HTML content and return the golang module source git repository URL
+// if it exists.
+// https://go.dev/ref/mod#vcs-find
+// Please note that we didn't use the xml approach as it couldn't parse the html content.
+func getGitRepositoryURL(htmlContent string) string {
+	var result string
+
+	doc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		return ""
+	}
+
+	var parseMeta func(*html.Node)
+	parseMeta = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "meta" {
+			var name, content string
+			for _, attr := range n.Attr {
+				if attr.Key == "name" {
+					name = attr.Val
+				} else if attr.Key == "content" {
+					content = attr.Val
+				}
+			}
+
+			if name == "go-import" {
+				content = strings.ReplaceAll(content, "\n", " ")
+				content = strings.TrimSpace(content)
+				// We only support git repositories
+				urls := strings.Split(content, " git ")
+
+				if len(urls) > 0 {
+					result = urls[1]
+				}
+
+				return
+			}
+		}
+
+		// Recursively parse child nodes
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			parseMeta(c)
+		}
+	}
+
+	parseMeta(doc)
+
+	URL, err := url.Parse(result)
+	if err != nil {
+		logrus.Debugf("failed parsing module %q: %s", result, err)
+		return ""
+	}
+
+	// Remove scheme from URL
+	URL.Scheme = ""
+	result = strings.TrimPrefix(URL.String(), "//")
+
+	return result
 }
--- pkg/plugins/resources/go/module/changelog_test.go
+++ pkg/plugins/resources/go/module/changelog_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/updatecli/updatecli/pkg/core/result"
 )
 
@@ -33,11 +34,95 @@ func TestChangelog(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "Test getting changelog from helm.sh/helm/v3",
+			from: "v3.17.1",
+			to:   "v3.17.1",
+			module: GoModule{
+				Spec: Spec{
+					Module: "helm.sh/helm/v3",
+				},
+			},
+			expectedResult: &result.Changelogs{
+				{
+					Title:       "v3.17.1",
+					URL:         "https://github.com/helm/helm/releases/tag/v3.17.1",
+					PublishedAt: "2025-02-12 21:01:05 +0000 UTC",
+				},
+			},
+		},
+		{
+			name: "Test do not exist module",
+			from: "v1.67.0",
+			to:   "v1.67.0",
+			module: GoModule{
+				Spec: Spec{
+					Module: "donotexit.com/ini.v1",
+				},
+			},
+			expectedResult: nil,
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, tt.expectedResult, tt.module.Changelog(tt.from, tt.to))
+			gotResultPtr := tt.module.Changelog(tt.from, tt.to)
+
+			if tt.expectedResult == nil && gotResultPtr != nil {
+				t.Fail()
+			} else if tt.expectedResult == nil && gotResultPtr == nil {
+				return
+			} else {
+				require.NotNil(t, gotResultPtr)
+			}
+
+			gotResult := *gotResultPtr
+
+			require.Equal(t, len(*tt.expectedResult), len(gotResult))
+			for i := range *tt.expectedResult {
+				expectedResult := *tt.expectedResult
+				assert.Equal(t, expectedResult[i].Title, gotResult[i].Title)
+				assert.Equal(t, expectedResult[i].URL, gotResult[i].URL)
+				assert.Equal(t, expectedResult[i].PublishedAt, gotResult[i].PublishedAt)
+			}
+		})
+	}
+}
+
+func TestGetSourceURL(t *testing.T) {
+
+	testdata := []struct {
+		name        string
+		htmlContent string
+		expectedURL string
+	}{
+		{
+			name: "working scenario",
+			htmlContent: `<html><head>
+      <meta name="go-import"
+            content="sigs.k8s.io/yaml
+                     git https://github.com/kubernetes-sigs/yaml">
+      <meta name="go-source"
+            content="sigs.k8s.io/yaml
+                     https://github.com/kubernetes-sigs/yaml
+                     https://github.com/kubernetes-sigs/yaml/tree/master{/dir}
+                     https://github.com/kubernetes-sigs/yaml/blob/master{/dir}/{file}#L{line}">
+</head></html>
+`,
+			expectedURL: "github.com/kubernetes-sigs/yaml",
+		},
+		{
+			name: "empty html content",
+			htmlContent: `<html><head></head></html>
+`,
+			expectedURL: "",
+		},
+	}
+
+	for _, tt := range testdata {
+		t.Run(tt.name, func(t *testing.T) {
+			gotResult := getGitRepositoryURL(tt.htmlContent)
+			assert.Equal(t, tt.expectedURL, gotResult)
 		})
 	}
 }
