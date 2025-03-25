cc4fddb1b0283b76510af1f99249a06cfc678754
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2025-03-25 14:56:34 +0100

    tests: Add new case on TestSearchReleases when key is `title`
    Signed-off-by: Víctor Cuadrado Juan <vcuadradojuan@suse.de>

--- pkg/plugins/scms/github/release_test.go
+++ pkg/plugins/scms/github/release_test.go
@@ -166,6 +166,79 @@ func TestSearchReleases(t *testing.T) {
 			wantReleases: []string{"44444444", "2222222"},
 		},
 		{
+			name: "Case with a mix of releases (draft, prerelease, release) and default ReleaseType filter and title",
+			spec: Spec{
+				Owner:      "updatecli",
+				Repository: "updatecli",
+				Token:      "superSecretTokenForJoe",
+				Username:   "joe",
+			},
+			searchKey: "title",
+			mockedQuery: &releasesQuery{
+				RateLimit: RateLimit{
+					Cost:      1,
+					Remaining: 5000,
+				},
+				Repository: struct {
+					Releases repositoryRelease `graphql:"releases(last: 100, before: $before, orderBy: $orderBy)"`
+				}{
+					Releases: repositoryRelease{
+						TotalCount: 4,
+						PageInfo: PageInfo{
+							HasNextPage: false,
+							EndCursor:   "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+						},
+						Edges: []releaseEdge{
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: ReleaseNode{
+									Name:    "v0.18.4 🌈",
+									TagName: "v0.18.4",
+									TagCommit: TagCommit{
+										Oid: "11111111",
+									},
+									IsDraft: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMi0wMS0xOFQxOTo0Nzo1MyswMTowMM4Da8vn",
+								Node: ReleaseNode{
+									Name:    "release v0.18.3",
+									TagName: "v0.18.3",
+									TagCommit: TagCommit{
+										Oid: "2222222",
+									},
+									IsLatest: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMS0xMS0xM1QxMjozNzoxMiswMTowMM4DK66W",
+								Node: ReleaseNode{
+									Name:    "v1.0.0",
+									TagName: "v1.0.0",
+									TagCommit: TagCommit{
+										Oid: "33333333",
+									},
+									IsPrerelease: true,
+								},
+							},
+							{
+								Cursor: "Y3Vyc29yOnYyOpK5MjAyMC0wMi0xOVQyMTozMDoxMyswMTowMM4BYOFb",
+								Node: ReleaseNode{
+									Name:    "v0.0.1 🌈",
+									TagName: "v0.0.1",
+									TagCommit: TagCommit{
+										Oid: "44444444",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			wantReleases: []string{"v0.0.1 🌈", "release v0.18.3"},
+		},
+		{
 			name: "Case with a mix of releases (draft, prerelease, release) and a ReleaseType with draft, prerelease and releases",
 			spec: Spec{
 				Owner:      "updatecli",
@@ -418,6 +491,8 @@ func TestSearchReleases(t *testing.T) {
 			var err error
 			if tt.searchKey == "hash" {
 				got, err = sut.SearchReleasesByTagHash(tt.releaseType)
+			} else if tt.searchKey == "title" {
+				got, err = sut.SearchReleasesByTitle(tt.releaseType)
 			} else {
 				got, err = sut.SearchReleasesByTagName(tt.releaseType)
 
