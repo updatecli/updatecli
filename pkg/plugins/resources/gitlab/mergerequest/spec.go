a90ee622cbbec76cd7d11f73891a7fda07ea0072
Alik Khilazhev <a.khilazhev@criteo.com>
2025-05-12 09:41:29 +0200

    Reformat docs in comment

--- pkg/plugins/resources/gitlab/mergerequest/spec.go
+++ pkg/plugins/resources/gitlab/mergerequest/spec.go
@@ -8,134 +8,109 @@ import (
 // It's a mapping of user input from a Updatecli manifest and it shouldn't modified
 type Spec struct {
 	client.Spec
-	/*
-		"sourcebranch" defines the branch name used as a source to create the GitLab mergerequest.
-
-		default:
-			"sourcebranch" inherits the value from the scm branch if a scm of kind "gitlab" is specified by the action.
-
-		remark:
-			unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
-	*/
+	// "sourcebranch" defines the branch name used as a source to create the GitLab mergerequest.
+	//
+	// default:
+	// 		"sourcebranch" inherits the value from the scm branch if a scm of kind "gitlab" is specified by the action.
+	//
+	// remark:
+	// 		unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
 	SourceBranch string `yaml:",omitempty"`
-	/*
-		"targetbranch" defines the branch name used as a target to create the GitLab mergerequest.
-
-		default:
-			"targetbranch" inherits the value from the scm working branch if a scm of kind "gitlab" is specified by the action.
-
-		remark:
-			unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
-			the GitLab scm will create and use a working branch such as updatecli_xxxx
-	*/
+	// "targetbranch" defines the branch name used as a target to create the GitLab mergerequest.
+	//
+	// default:
+	// 		"targetbranch" inherits the value from the scm working branch if a scm of kind "gitlab" is specified by the action.
+	//
+	// remark:
+	// 		unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
+	// 		the GitLab scm will create and use a working branch such as updatecli_xxxx
 	TargetBranch string `yaml:",omitempty"`
-	/*
-		"owner" defines the GitLab repository owner.
-
-		remark:
-			unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
-	*/
+	// "owner" defines the GitLab repository owner.
+	//
+	// remark:
+	// 		unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
 	Owner string `yaml:",omitempty"`
-	/*
-		"repository" defines the GitLab repository for a specific owner
-
-		remark:
-			unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
-	*/
+	// "repository" defines the GitLab repository for a specific owner
+	//
+	// remark:
+	// 		unless you know what you are doing, you shouldn't set this value and rely on the scmid to provide the sane default.
 	Repository string `yaml:",omitempty"`
-	/*
-		"title" defines the GitLab mergerequest title
-
-		default:
-			A GitLab mergerequest title is defined by one of the following location (first match)
-				1. title is defined by the spec such as:
-
-					actions:
-						default:
-							kind: gitlab/mergerequest
-							scmid: default
-							spec:
-								title: This is my awesome title
-
-				2. title is defined by the action such as:
-
-					actions:
-						default:
-							kind: gitlab/mergerequest
-							scmid default
-							title: This is my awesome title
-
-				3. title is defined by the first associated target title
-
-				4. title is defined by the pipeline title
-
-		remark:
-			usually we prefer to go with option 2
-	*/
+	// "title" defines the GitLab mergerequest title
+	//
+	// default:
+	//	 	A GitLab mergerequest title is defined by one of the following location (first match)
+	//	 		1. title is defined by the spec such as:
+	//
+	//	 			actions:
+	//	 				default:
+	//	 					kind: gitlab/mergerequest
+	//	 					scmid: default
+	//	 					spec:
+	//	 						title: This is my awesome title
+	//
+	//	 		2. title is defined by the action such as:
+	//
+	//	 			actions:
+	//	 				default:
+	//	 					kind: gitlab/mergerequest
+	//	 					scmid default
+	//	 					title: This is my awesome title
+	//
+	//	 		3. title is defined by the first associated target title
+	//
+	//	 		4. title is defined by the pipeline title
+	//
+	// remark:
+	// 		usually we prefer to go with option 2
 	Title string `yaml:",omitempty"`
-	/*
-		"body" defines a custom mergerequest body
-
-		default:
-			By default a mergerequest body is generated out of a pipeline execution.
-
-		remark:
-			Unless you know what you are doing, you shouldn't set this value and rely on the sane default.
-			"body" is useful to provide additional information when reviewing mergerequest, such as changelog url.
-	*/
+	// "body" defines a custom mergerequest body
+	//
+	// default:
+	// 	By default a mergerequest body is generated out of a pipeline execution.
+	//
+	// remark:
+	// 	Unless you know what you are doing, you shouldn't set this value and rely on the sane default.
+	// 	"body" is useful to provide additional information when reviewing mergerequest, such as changelog url.
 	Body string `yaml:",omitempty"`
-
-	/*
-		"assignees" contains the list of assignee to add to the merge request
-
-		default: empty
-
-		remark:
-		    assignees only accept GitLab User IDs.
-			To find the user ID:
-				1. Go to the users’ profile page.
-				2. On the profile page, in the upper-right corner, select Actions (or ⋮).
-				3. Select Copy user ID.
-	*/
+	// "assignees" contains the list of assignee to add to the merge request
+	//
+	// default: empty
+	//
+	// remark:
+	//		assignees only accept GitLab User IDs.
+	// 		To find the user ID:
+	// 			1. Go to the users’ profile page.
+	// 			2. On the profile page, in the upper-right corner, select Actions (or ⋮).
+	// 			3. Select Copy user ID.
 	Assignees []int `yaml:",omitempty"`
-	/*
-		"reviewers" contains the list of reviewers to add to the merge request
-
-		default: empty
-
-		remark:
-			assignees only accept GitLab User IDs.
-			To find the user ID:
-				1. Go to the users’ profile page.
-				2. On the profile page, in the upper-right corner, select Actions (or ⋮).
-				3. Select Copy user ID.
-	*/
+	// "reviewers" contains the list of reviewers to add to the merge request
+	//
+	// default: empty
+	//
+	// remark:
+	// 		assignees only accept GitLab User IDs.
+	// 		To find the user ID:
+	// 			1. Go to the users’ profile page.
+	// 			2. On the profile page, in the upper-right corner, select Actions (or ⋮).
+	// 			3. Select Copy user ID.
 	Reviewers []int `yaml:",omitempty"`
-
-	/*
-		"squash" defines if all commits should be squashed into a single commit on merge
-
-		default: false
-
-		remark:
-			project settings might override this value
-	*/
+	// "squash" defines if all commits should be squashed into a single commit on merge
+	//
+	// default: false
+	//
+	// remark:
+	// 		project settings might override this value
 	Squash bool `yaml:",omitempty"`
-
-	/*
-		"removesourcebranch" is a flag indicating if a merge request should remove the source branch when merging
-
-		default: false
-	*/
+	// "removesourcebranch" is a flag indicating if a merge request should remove the source branch when merging
+	//
+	// default: false
 	RemoveSourceBranch bool `yaml:",omitempty"`
-
-	/*
-		"labels" defines labels for the merge request.
-
-		default: empty
-
-		remark:
-			if a label does not already exist, this creates a new project label and assigns it to the merge request
-	*/
+	//
+	// 	"labels" defines labels for the merge request.
+	//
+	// 	default: empty
+	//
+	// 	remark:
+	// 		if a label does not already exist, this creates a new project label and assigns it to the merge request
 	Labels []string `yaml:",omitempty"`
 }
