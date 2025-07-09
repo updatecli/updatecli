4d86ae572522490c8086f2547191e7404ea9f05e
Olblak <me@olblak.com>
2025-07-09 17:45:54 +0200

    fix: ensure a branch name never start with a /

--- pkg/plugins/utils/gitgeneric/main.go
+++ pkg/plugins/utils/gitgeneric/main.go
@@ -695,9 +695,12 @@ func (g GoGit) SanitizeBranchName(branch string) string {
 		branch = strings.ReplaceAll(branch, character, "_")
 	}
 
+	branch = strings.TrimPrefix(branch, "/")
+
 	if len(branch) > 255 {
 		return branch[0:255]
 	}
+
 	return branch
 }
 
