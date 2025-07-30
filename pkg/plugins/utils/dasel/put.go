60454e5d8ebf1742a30f3299dc073963e49d20d3
Olblak <me@olblak.com>
2025-07-30 08:58:14 +0200

    feat: add Dasel v2 support to dasel utils package

--- pkg/plugins/utils/dasel/put.go
+++ pkg/plugins/utils/dasel/put.go
@@ -1,5 +1,11 @@
 package dasel
 
+import (
+	"fmt"
+
+	daselV2 "github.com/tomwright/dasel/v2"
+)
+
 // Put insert value in a Dasel node
 func (f *FileContent) Put(query, value string) error {
 	if f.DaselNode == nil {
@@ -24,4 +30,17 @@ func (f *FileContent) PutMultiple(query, value string) error {
 	}
 
 	return nil
+}
+
+func (f *FileContent) PutV2(query, value string) error {
+	if f.DaselV2Node == nil {
+		return ErrEmptyDaselNode
+	}
+
+	if _, err := daselV2.Put(f.DaselV2Node, query, value); err != nil {
+		return fmt.Errorf("setting value %q with query %q: %w", value, query, err)
+	}
+
+	return nil
+
 }
