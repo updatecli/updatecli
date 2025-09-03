99e86598e612f33813b50a0c9763b62635cfeb13
Olblak <me@olblak.com>
2025-09-03 20:27:20 +0200

    chore: add missing test

--- pkg/plugins/autodiscovery/golang/main_test.go
+++ pkg/plugins/autodiscovery/golang/main_test.go
@@ -1,6 +1,7 @@
 package golang
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +16,124 @@ func TestDiscoverManifests(t *testing.T) {
 		expectedPipelines []string
 	}{
 		{
-			name:              "Golang Replace module",
-			rootDir:           "testdata/replace",
-			expectedPipelines: []string{``},
+			name:    "Golang Replace module",
+			rootDir: "testdata/replace",
+			expectedPipelines: []string{`name: 'deps(go): bump module github.com/crewjam/saml'
+sources:
+  module:
+    name: 'Get latest golang module github.com/crewjam/saml version'
+    kind: 'golang/module'
+    spec:
+      module: 'github.com/crewjam/saml'
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=0.6.0'
+targets:
+  module:
+    name: 'deps(go): bump module github.com/crewjam/saml to {{ source "module" }}'
+    kind: 'golang/gomod'
+    sourceid: 'module'
+    spec:
+      file: 'go.mod'
+      module: 'github.com/crewjam/saml'
+`,
+				`name: 'deps(go): bump module github.com/rancher/saml'
+sources:
+  module:
+    name: 'Get latest golang module github.com/rancher/saml version'
+    kind: 'golang/module'
+    spec:
+      module: 'github.com/rancher/saml'
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=0.3.0'
+targets:
+  module:
+    name: 'deps(go): bump module github.com/rancher/saml to {{ source "module" }}'
+    kind: 'golang/gomod'
+    sourceid: 'module'
+    spec:
+      file: 'go.mod'
+      module: 'github.com/rancher/saml'
+`,
+				`name: 'deps(go): bump module github.com/stretchr/testify'
+sources:
+  module:
+    name: 'Get latest golang module github.com/stretchr/testify version'
+    kind: 'golang/module'
+    spec:
+      module: 'github.com/stretchr/testify'
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=1.8.4'
+targets:
+  module:
+    name: 'deps(go): bump module github.com/stretchr/testify to {{ source "module" }}'
+    kind: 'golang/gomod'
+    sourceid: 'module'
+    spec:
+      file: 'go.mod'
+      module: 'github.com/stretchr/testify'
+`,
+				`name: 'deps(go): bump replaced module github.com/crewjam/saml'
+sources:
+  module:
+    name: 'Get latest golang module github.com/crewjam/saml version'
+    kind: 'golang/module'
+    spec:
+      module: 'github.com/crewjam/saml'
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=0.5.0'
+targets:
+  module:
+    name: 'deps(go): bump module github.com/crewjam/saml to {{ source "module" }}'
+    kind: 'golang/gomod'
+    sourceid: 'module'
+    spec:
+      file: 'go.mod'
+      module: 'github.com/crewjam/saml'
+      replace: true
+      replaceVersion: 'v0.6.0'
+
+`,
+				`name: 'deps(go): bump replaced module github.com/rancher/saml'
+sources:
+  module:
+    name: 'Get latest golang module github.com/rancher/saml version'
+    kind: 'golang/module'
+    spec:
+      module: 'github.com/rancher/saml'
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=0.2.0'
+targets:
+  module:
+    name: 'deps(go): bump module github.com/rancher/saml to {{ source "module" }}'
+    kind: 'golang/gomod'
+    sourceid: 'module'
+    spec:
+      file: 'go.mod'
+      module: 'github.com/rancher/saml'
+      replace: true
+`,
+				`name: 'deps(golang): bump Go version'
+sources:
+  go:
+    name: 'Get latest Go version'
+    kind: 'golang'
+    spec:
+      versionfilter:
+        kind: 'semver'
+        pattern: '>=1.25.0'
+targets:
+  go:
+    name: 'deps(golang): bump Go version to {{ source "go" }}'
+    kind: 'golang/gomod'
+    sourceid: 'go'
+    spec:
+      file: 'go.mod'
+`},
 		},
 		{
 			name:    "Golang Version",
@@ -136,6 +252,12 @@ targets:
 			for i := range bytesPipelines {
 				stringPipelines = append(stringPipelines, string(bytesPipelines[i]))
 			}
+
+			// sort stringsPipelines to ensure that the test result
+			// is always the same
+			sort.Slice(stringPipelines, func(i, j int) bool {
+				return stringPipelines[i] < stringPipelines[j]
+			})
 
 			for i := range stringPipelines {
 				pipelines = append(pipelines, stringPipelines...)
