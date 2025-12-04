06c40e9791898f2a8ac38a5815f542361bed3470
Olblak <me@olblak.com>
2025-12-04 20:55:32 +0100

    fix hostfunction
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/autodiscovery/plugin/hostFunction.go
+++ pkg/plugins/autodiscovery/plugin/hostFunction.go
@@ -3,9 +3,6 @@ package plugin
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-
-	"gopkg.in/yaml.v3"
 
 	extism "github.com/extism/go-sdk"
 	"github.com/updatecli/updatecli/pkg/plugins/resources/dockerimage"
@@ -16,11 +13,19 @@ import (
 // It reads input parameters from the plugin's memory stack, processes them to create the spec,
 // and writes the resulting YAML back to the stack.
 // The input is expected to be a JSON string containing "image" and "tag" fields.
-// The output is a YAML representation of the Docker source spec.
+// The output is a json representation of the Docker source spec versionfilter and tagfilter.
+// Such as:
+//
+//	{
+//	  "versionfilter": {
+//	    "kind": "semver",
+//	    "pattern": "*"
+//	  }
+//	  "tagfilter": "^v[0-9]+\\.[0-9]+\\.[0-9]+$"
+//	}
 func generate_docker_source_spec(ctx context.Context, p *extism.CurrentPlugin, stack []uint64) {
 	input, err := p.ReadString(stack[0])
 	if err != nil {
-		fmt.Printf("Error reading input string: %v\n", err)
 		stack[0] = 0
 		return
 	}
@@ -32,7 +37,6 @@ func generate_docker_source_spec(ctx context.Context, p *extism.CurrentPlugin, s
 
 	err = json.Unmarshal([]byte(input), &inputData)
 	if err != nil {
-		fmt.Printf("Error unmarshaling input JSON: %v\n", err)
 		stack[0] = 0
 		return
 	}
@@ -40,16 +44,19 @@ func generate_docker_source_spec(ctx context.Context, p *extism.CurrentPlugin, s
 	auths := map[string]docker.InlineKeyChain{}
 	sourceSpec := dockerimage.NewDockerImageSpecFromImage(inputData.Image, inputData.Tag, auths)
 
-	outputBytes, err := yaml.Marshal(sourceSpec)
-	if err != nil {
-		fmt.Printf("Error marshaling source spec to YAML: %v\n", err)
+	if sourceSpec == nil {
 		stack[0] = 0
+		return
+	}
+
+	outputBytes, err := json.Marshal(sourceSpec)
+	if err != nil {
+		stack[0], stack[1] = 0, 0
 		return
 	}
 
 	output, err := p.WriteString(string(outputBytes))
 	if err != nil {
-		fmt.Printf("Error writing output bytes: %v\n", err)
 		stack[0] = 0
 		return
 	}
