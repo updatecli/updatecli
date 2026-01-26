1328f60a9b627cb3f3da3e4abdb0e5a274b532f9
josill <josill@taltech.ee>
2026-01-26 17:47:33 +0200

    add e2e tests

--- e2e/scripts/test_deprecated.bash
+++ e2e/scripts/test_deprecated.bash
@@ -8,4 +8,8 @@ set -eux
 ## In order for the Updatecli manifest to work, we must be at the root of the git repository 
 pushd "$VENOM_VAR_rootpath"
 
+# Test deprecated diff command (should show deprecation warning but still work)
 "$VENOM_VAR_binpath/updatecli" diff --config  e2e/updatecli.d/deprecated.d
+
+# Test deprecated apply command (should show deprecation warning but still work)
+"$VENOM_VAR_binpath/updatecli" apply --config  e2e/updatecli.d/deprecated.d --dry-run
