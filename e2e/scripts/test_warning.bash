d8de657634641b1fe9d8fcd1d6cc22ad2cc34ad7
Olblak <me@olblak.com>
2026-01-29 09:05:55 +0100

    fix: e2e script
    Signed-off-by: Olblak <me@olblak.com>

--- e2e/scripts/test_warning.bash
+++ e2e/scripts/test_warning.bash
@@ -5,7 +5,7 @@ set -eux
 : "${VENOM_VAR_binpath:? Please set VENOM_VAR_binpath to updatecli binary dirname}"
 : "${VENOM_VAR_rootpath:=../..}"
 
-## In order for the Updatecli manifest to work, we must be at the root of the git repository 
+## In order for the Updatecli manifest to work, we must be at the root of the git repository
 pushd "$VENOM_VAR_rootpath"
 
-"$VENOM_VAR_binpath/updatecli" diff --config  e2e/updatecli.d/warning.d/
+"$VENOM_VAR_binpath/updatecli" pipeline diff --config e2e/updatecli.d/warning.d/
