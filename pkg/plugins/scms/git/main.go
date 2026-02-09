638c9582a7cc84a57b993e59461443c9f47afafc
Olblak <me@olblak.com>
2026-02-09 21:03:18 +0100

    fix: comment
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/scms/git/main.go
+++ pkg/plugins/scms/git/main.go
@@ -102,7 +102,7 @@ type Spec struct {
 	// Default: disabled (full clone)
 	//
 	// Remark:
-	//   When using a depth greater than 1, Updatecli is not able to retrieve the full git history.
+	//   When using a shallow clone (depth greater than 0), Updatecli is not able to retrieve the full git history.
 	//   This may cause some issues when Updatecli tries to push changes to the remote repository.
 	//   In that case, you may need to set the force option to true to force push changes to the remote repository.
 	Depth *int `yaml:",omitempty"`
