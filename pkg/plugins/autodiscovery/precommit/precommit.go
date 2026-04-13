3b90fbf573036383a02e2e7637d4ee36f6f3a429
nickzerjeski <nick@zerjeski.de>
2026-04-13 10:35:13 +0200

    feat(autodiscovery/precommit): detect rev version from comment
    Signed-off-by: nickzerjeski <nick@zerjeski.de>

--- pkg/plugins/autodiscovery/precommit/precommit.go
+++ pkg/plugins/autodiscovery/precommit/precommit.go
@@ -2,14 +2,16 @@ package precommit
 
 import (
 	"os"
+	"strings"
 
 	goyaml "go.yaml.in/yaml/v3"
 )
 
 // precommitRepo represent a precommit repo
 type precommitRepo struct {
-	Repo string `yaml:"repo"`
-	Rev  string `yaml:"rev"`
+	Repo       string `yaml:"repo"`
+	Rev        string `yaml:"rev"`
+	RevComment string
 }
 
 // precommitData represent the useful struct content of .pre-commit-config.yaml
@@ -25,6 +27,54 @@ func loadPrecommitData(filename string) (*precommitData, error) {
 	err := goyaml.Unmarshal(rawFileContent, &data)
 	if err != nil {
 		return nil, err
+	}
+
+	var rootNode goyaml.Node
+	err = goyaml.Unmarshal(rawFileContent, &rootNode)
+	if err != nil {
+		return &data, nil
+	}
+
+	if len(rootNode.Content) == 0 {
+		return &data, nil
+	}
+
+	reposByURL := map[string]*precommitRepo{}
+	for i := range data.Repos {
+		repo := &data.Repos[i]
+		reposByURL[repo.Repo] = repo
+	}
+
+	document := rootNode.Content[0]
+	for i := 0; i+1 < len(document.Content); i += 2 {
+		keyNode := document.Content[i]
+		valueNode := document.Content[i+1]
+		if keyNode.Value != "repos" || valueNode.Kind != goyaml.SequenceNode {
+			continue
+		}
+
+		for _, repoNode := range valueNode.Content {
+			if repoNode.Kind != goyaml.MappingNode {
+				continue
+			}
+
+			var repoURL string
+			var revComment string
+			for j := 0; j+1 < len(repoNode.Content); j += 2 {
+				repoKey := repoNode.Content[j]
+				repoValue := repoNode.Content[j+1]
+				switch repoKey.Value {
+				case "repo":
+					repoURL = repoValue.Value
+				case "rev":
+					revComment = strings.TrimSpace(repoValue.LineComment)
+				}
+			}
+
+			if repo, ok := reposByURL[repoURL]; ok {
+				repo.RevComment = revComment
+			}
+		}
 	}
 
 	return &data, nil
