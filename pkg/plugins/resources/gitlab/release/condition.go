abd1d6842282f33e8a1f3487d541b95f6b0eddaf
Olivier Vernin <olivier@vernin.me>
2026-09-07 18:41:13 +0200

    feat(gitlab/release): add age support (#10280)
    * feat(gitlab/release): add age support
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * chore: add age e2e test
    
    Signed-off-by: Olblak <me@olblak.com>
    
    * fix(gitlab/release): latest order
    
    Signed-off-by: Olblak <me@olblak.com>
    
    ---------
    
    Signed-off-by: Olblak <me@olblak.com>

--- pkg/plugins/resources/gitlab/release/condition.go
+++ pkg/plugins/resources/gitlab/release/condition.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/updatecli/updatecli/pkg/core/pipeline/scm"
+	"github.com/updatecli/updatecli/pkg/plugins/utils/age"
 )
 
 func (g *Gitlab) Condition(_ context.Context, source string, scm scm.ScmHandler) (pass bool, message string, err error) {
@@ -13,7 +14,9 @@ func (g *Gitlab) Condition(_ context.Context, source string, scm scm.ScmHandler)
 		logrus.Warningf("Condition not supported for the plugin GitLab release")
 	}
 
-	releases, err := g.SearchReleases()
+	// A condition checks whether a specific release exists, so the age filter, which
+	// only narrows down which release to pick, doesn't apply here.
+	releases, err := g.SearchReleases(age.Spec{})
 	if err != nil {
 		return false, "", fmt.Errorf("looking for GitLab release: %w", err)
 	}
